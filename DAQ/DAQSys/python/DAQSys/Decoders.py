de6d0b6a0e24b11f6934e57c2dfb6f471187d584
Rosen Matev <r.matev@gmail.com>
2016-04-18 03:32:31 +0200

    Take persist reco locations for decoder from GaudiConf.PersistRecoConf

--- DAQ/DAQSys/python/DAQSys/Decoders.py
+++ DAQ/DAQSys/python/DAQSys/Decoders.py
@@ -412,26 +412,13 @@ Decoder("HltLumiSummaryDecoder",
         outputs={"OutputContainerName":None},
         conf=DecoderDB)
 
+from GaudiConf.PersistRecoConf import PersistRecoPacking
+__packing = PersistRecoPacking()
 Decoder("HltPackedDataDecoder/Hlt2PackedDataDecoder",
         active=True, banks=["DstData"],
         inputs={"RawEventLocations": None},
-        outputs=[
-            "Hlt2/pRec/long/Protos",
-            "Hlt2/pRec/down/Protos",
-            "Hlt2/pRec/long/RichPIDs",
-            "Hlt2/pRec/down/RichPIDs",
-            "Hlt2/pRec/long/MuonIDs",
-            "Hlt2/pRec/long/Tracks",
-            "Hlt2/pRec/down/Tracks",
-            "Hlt2/pRec/down/PID/MuonSegments",
-            "Hlt2/pRec/neutral/Protos",
-            "Hlt2/pRec/neutral/CaloClusters",
-            "Hlt2/pRec/neutral/EcalSplitClusters",
-            "Hlt2/pRec/neutral/Electrons",
-            "Hlt2/pRec/neutral/Photons",
-            "Hlt2/pRec/neutral/MergedPi0s",
-            "Hlt2/pRec/neutral/SplitPhotons",
-        ],
+        outputs=__packing.packedLocations(),
+        properties={"ContainerMap": __packing.packedToOutputLocationMap()},
         conf=DecoderDB)
 
 #UPGRADE ===========UT===========
