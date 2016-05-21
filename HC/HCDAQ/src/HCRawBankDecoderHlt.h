59948a2c075f7a82cc44de8170f897c94893a16d
Daniel Johnson <daniel.johnson@cern.ch>
2016-05-21 08:32:50 +0200

    New Raw bank decoder for Hlt

---
+++ HC/HCDAQ/src/HCRawBankDecoderHlt.h
@@ -0,0 +1,47 @@
+#ifndef HCRAWBANKDECODER_HLT_H
+#define HCRAWBANKDECODER_HLT_H 1
+
+// LHCb
+#include "DAQKernel/DecoderHistoAlgBase.h"
+
+class Condition;
+
+/** @class HCRawBankDecoderHlt.h
+ *
+ * Specialised decoder for Herschel raw banks for use in HLT1.
+ *
+ */
+
+class HCRawBankDecoderHlt : public Decoder::HistoAlgBase {
+ public:
+  /// Standard constructor
+  HCRawBankDecoderHlt(const std::string& name, ISvcLocator* pSvcLocator);
+  /// Destructor
+  virtual ~HCRawBankDecoderHlt();
+
+  virtual StatusCode initialize();  ///< Algorithm initialization
+  virtual StatusCode execute();     ///< Algorithm execution
+
+ private:
+  Condition* m_cond = nullptr;
+
+  /// TES location of output digits.
+  std::string m_digitLocation;
+
+  /// Crate number for B side stations.
+  unsigned int m_crateB = 0;
+  /// Crate number for F side stations.
+  unsigned int m_crateF = 1;
+
+  /// Masking flag for each channel.
+  std::vector<bool> m_masked;
+  /// Station number for each channel.
+  std::vector<unsigned int> m_station;
+
+  /// Do the decoding.
+  bool decode(LHCb::RawBank* bank, std::vector<int>& sums);
+  /// Retrieve the mapping from the conditions database.
+  StatusCode cacheMapping();
+};
+
+#endif
