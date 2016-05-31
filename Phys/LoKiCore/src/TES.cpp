ddae6b37dc49ce260b2e17793bd7f246e783da9f
Daniel Johnson <daniel.johnson@cern.ch>
2016-05-31 12:56:07 +0200

    Update TES.cpp

--- Phys/LoKiCore/src/TES.cpp
+++ Phys/LoKiCore/src/TES.cpp
@@ -241,7 +241,7 @@ LoKi::TES::HrcSumAdc::operator() ( /* LoKi::TES::HrcSumAdc::argument */ ) const
   // Compute ADC sum
   LHCb::HCCellID id( stationId ) ;
   const auto digit = digits->object(id);
-  return digit ? digit->adc() : -1; 
+  return digit ? digit->adc() : 99999; 
 }
 // ============================================================================
 // OPTIONAL: nice printout
