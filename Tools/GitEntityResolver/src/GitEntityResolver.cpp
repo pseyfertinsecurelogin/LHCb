592dd6793ee849ffec8293bcd3dc39fa2cb5337f
Marco Clemencic <marco.clemencic@cern.ch>
2016-07-22 18:27:14 +0200

    added GitEntityResolver tool
    Allow Xerces-C++ to access XML files from a Git repository database.

---
+++ Tools/GitEntityResolver/src/GitEntityResolver.cpp
@@ -0,0 +1,154 @@
+#include "GitEntityResolver.h"
+
+#include <sstream>
+
+#include <xercesc/util/XMLString.hpp>
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/framework/MemoryManager.hpp>
+#include <xercesc/framework/MemBufInputSource.hpp>
+
+#include <boost/utility/string_ref.hpp>
+
+DECLARE_COMPONENT( GitEntityResolver )
+
+namespace {
+
+  template<class RET, class FUNC, class ...ARGS>
+  RET git_call(boost::string_ref name, boost::string_ref err_msg, boost::string_ref key,
+               FUNC func, ARGS&&... args) {
+    typename RET::element_type * tmp = nullptr;
+    if ( UNLIKELY( func(&tmp, std::forward<ARGS>(args)...) ) )
+      throw GaudiException(std::string(err_msg) + " " + std::string(key) + ": " + giterr_last()->message, std::string(name), StatusCode::FAILURE);
+    return RET{ tmp };
+  }
+
+}
+
+GitEntityResolver::Blob::Blob( const git_object_ptr& obj ) {
+  auto blob = reinterpret_cast<const git_blob*>( obj.get() );
+  m_size = git_blob_rawsize( blob );
+  auto buff = xercesc::XMLPlatformUtils::fgMemoryManager->allocate( m_size );
+  std::memcpy(buff, git_blob_rawcontent( blob ), m_size );
+  m_buff = reinterpret_cast<const XMLByte*>( buff );
+}
+
+GitEntityResolver::Blob::~Blob() {
+  auto tmp = static_cast<const void*>( adopt() );
+  if ( tmp ) xercesc::XMLPlatformUtils::fgMemoryManager->deallocate( const_cast<void*>( tmp ) );
+}
+
+
+#define ON_DEBUG if ( UNLIKELY( msgLevel( MSG::DEBUG ) ) )
+#define DEBUG_MSG ON_DEBUG debug()
+#define ON_VERBOSE if ( UNLIKELY( msgLevel( MSG::VERBOSE ) ) )
+#define VERBOSE_MSG ON_VERBOSE verbose()
+
+GitEntityResolver::GitEntityResolver( const std::string& type, const std::string& name, const IInterface* parent ):
+  base_class(type, name, parent)
+{
+  // Initialize Git library
+  git_libgit2_init();
+
+  declareProperty( "PathToRepository", m_pathToRepository = "", "path to the git repository to get data from");
+  m_pathToRepository.declareUpdateHandler([this](Property&) -> void {
+    DEBUG_MSG << "opening Git repository " << m_pathToRepository.value() << endmsg;
+    m_repository = git_call<git_repository_ptr>( this->name(), "cannot open repository", m_pathToRepository.value(),
+                                                 git_repository_open, m_pathToRepository.value().c_str() );
+  });
+
+  declareProperty( "Commit", m_commit = "HEAD", "commit id (or tag, or branch) of the version to use");
+}
+
+GitEntityResolver::~GitEntityResolver() {
+  // Finalize Git library
+  git_libgit2_shutdown();
+}
+
+StatusCode GitEntityResolver::initialize ( ) {
+  StatusCode sc = AlgTool::initialize();
+  if ( ! sc.isSuccess() ) return sc;
+
+  DEBUG_MSG << "Initializing..." << endmsg;
+
+  // Initialize the Xerces-C++ XML subsystem
+  try {
+
+    xercesc::XMLPlatformUtils::Initialize();
+
+    DEBUG_MSG << "Successfully initialized." << endmsg;
+
+  } catch(const xercesc::XMLException& toCatch) {
+    char *message = xercesc::XMLString::transcode(toCatch.getMessage());
+    fatal() << "Error during Xerces-C Initialization." << endmsg;
+    fatal() << "  Exception message:" << message << endmsg;
+    xercesc::XMLString::release(&message);
+    sc = StatusCode::FAILURE;  // allow return value optimization
+    return sc;
+  }
+
+  return sc;
+}
+
+StatusCode GitEntityResolver::finalize ( ) {
+
+  m_repository.reset();
+
+  // Finalize the Xerces-C++ XML subsystem
+  xercesc::XMLPlatformUtils::Terminate();
+
+  return AlgTool::finalize();
+}
+
+
+const std::vector<std::string>& GitEntityResolver::protocols() const {
+  /// Vector of supported protocols. (for IFileAccess)
+  static const std::vector<std::string> s_protocols = { { "git" } };
+  return s_protocols;
+}
+
+GitEntityResolver::open_result_t GitEntityResolver::open(const std::string &url) {
+  auto obj = i_getData(url);
+  auto blob = reinterpret_cast<const git_blob*>( obj.get() );
+  std::string str{ reinterpret_cast<const char*>( git_blob_rawcontent( blob ) ), static_cast<std::size_t>( git_blob_rawsize( blob ) ) };
+  return open_result_t( new std::istringstream( str ) );
+}
+
+git_object_ptr GitEntityResolver::i_getData( const std::string &url ) const {
+  boost::string_ref path{url};
+  if ( url.compare( 0, 4, "git:") == 0 ) {
+    if ( url.compare( 4, 2, "//" ) == 0 )
+      path = path.substr(6);
+    else
+      path = path.substr(4);
+  }
+  while ( path[0] == '/' ) path = path.substr(1);
+
+  std::string rev = m_commit.value() + ":" + std::string(path);
+  return git_call<git_object_ptr>(name(), "cannot resolve object", rev,
+                                  git_revparse_single, m_repository.get(), rev.c_str());
+}
+
+xercesc::InputSource* GitEntityResolver::resolveEntity(const XMLCh *const, const XMLCh *const systemId) {
+
+  // copy Xerces string into std::string
+  std::string systemIdString;
+  char *cString = xercesc::XMLString::transcode(systemId);
+  if (cString) {
+    systemIdString = cString;
+    xercesc::XMLString::release(&cString);
+  }
+
+  DEBUG_MSG << "systemId = \"" << systemIdString << "\"" << endmsg;
+
+  if ( systemIdString.compare( 0, 4, "git:" ) != 0 ) {
+    // the string does not start with "git:", so I cannot handle it
+    VERBOSE_MSG << "Not a Git URL" << endmsg;
+    // tell XercesC to use the default action
+    return nullptr;
+  }
+
+  Blob data{ i_getData( systemIdString ) };
+
+  auto buff_size = data.size();  // must be done here because "adopt" set the size to 0
+  return new xercesc::MemBufInputSource(data.adopt(), buff_size, systemId, true);
+}
---
+++ Tools/GitEntityResolver/src/GitEntityResolver.h
@@ -0,0 +1,117 @@
+#ifndef GITENTITYRESOLVER_H
+#define GITENTITYRESOLVER_H
+
+// Include files
+#include <type_traits>
+#include "boost/optional.hpp"
+#include "GaudiKernel/AlgTool.h"
+#include "GaudiKernel/IFileAccess.h"
+#include "XmlTools/IXmlEntityResolver.h"
+
+#include <git2.h>
+
+namespace std {
+  template <>
+  struct default_delete<git_object> {
+    void operator()(git_object* ptr) const {
+      git_object_free(ptr);
+    }
+  };
+  template <>
+  struct default_delete<git_repository> {
+    void operator()(git_repository* ptr) const {
+      git_repository_free(ptr);
+    }
+  };
+}
+
+using git_object_ptr = std::unique_ptr<git_object>;
+using git_repository_ptr = std::unique_ptr<git_repository>;
+
+/** Allow use of a Git repository as a source of XML files for XercesC.
+ *
+ *  An instance of this tool can be used to allow inclusion of XML DTDs and documents
+ *  from a Git repository. It works implementing the EntityResolver interface defined in Xerces-C++,
+ *  which allows to intercept the requests of external XML data.
+ *
+ *  Examples of CondDB external references:
+ *  \li \<!DOCTYPE DDDB SYSTEM "git:/DTD/structure"\>
+ *  \li \<!ENTITY Parameters SYSTEM "git:/Parameters/SpecialParamters"\>
+ *
+ *  This tool implements the Gaudi IFileAccess interface, so that it can be used
+ *  to retrieve files from a Git repository.
+ *
+ *  @author Marco Clemencic
+ *  @date   2016-07-21
+ */
+class GitEntityResolver: public extends<AlgTool, IXmlEntityResolver, IFileAccess>,
+                         virtual public xercesc::EntityResolver {
+public:
+
+  GitEntityResolver( const std::string& type, const std::string& name, const IInterface* parent );
+
+  ~GitEntityResolver() override;
+
+  /// Initialize the tool
+  StatusCode initialize() override;
+
+  /// Finalize the tool
+  StatusCode finalize() override;
+
+
+  /// Return a pointer to the actual implementation of a xercesc::EntityResolver.
+  xercesc::EntityResolver *resolver() override { return this; }
+
+  /// Create a Xerces-C input source based on the given systemId (publicId is ignored).
+  /// If the systemId does not begin with "conddb:", it returns NULL, so the parser go on with the default action.
+  xercesc::InputSource *resolveEntity(const XMLCh *const publicId, const XMLCh *const systemId) override;
+
+
+  /// Find the URL and returns an auto_ptr to an input stream interface of an
+  /// object that can be used to read from the file the URL is pointing to.
+  /// Returns an empty pointer if the URL cannot be resolved.
+  using open_result_t = typename std::result_of<decltype(&IFileAccess::open)(IFileAccess&,std::string)>::type;
+  open_result_t open(const std::string &url) override;
+
+  /// @see IFileAccess::protocols
+  const std::vector<std::string> &protocols() const override;
+
+private:
+
+  /// path to the git repository to get data from
+  StringProperty m_pathToRepository;
+  /// commit id (or tag, or branch) of the version to use
+  StringProperty m_commit;
+
+  /// Helper class to manage Xerces-C++ buffers
+  struct Blob {
+    Blob( const git_object_ptr& obj );
+    Blob( Blob&& other ) {
+      m_size = other.m_size;
+      m_buff = other.adopt();
+    }
+    Blob( const Blob& ) = delete;
+
+    ~Blob();
+    const XMLByte* get() { return m_buff; }
+    XMLSize_t size() { return m_size; }
+    const XMLByte* adopt() {
+      const XMLByte* tmp = m_buff;
+      m_buff = nullptr;
+      m_size = 0;
+      return tmp;
+    }
+
+  private:
+    const XMLByte* m_buff = nullptr;
+    XMLSize_t m_size = 0;
+  };
+
+  /// Return a string containing the data at a given path in the repository.
+  git_object_ptr i_getData(const std::string &url) const;
+
+  git_repository_ptr m_repository;
+
+};
+
+#endif // GITENTITYRESOLVER_H
