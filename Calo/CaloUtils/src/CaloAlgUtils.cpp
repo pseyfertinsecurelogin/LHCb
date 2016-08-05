4817b4bcb82d5c6a5bfea14db30facbfc75acf33
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2016-08-05 21:46:48 +0200

    Modernize / streamline CaloUtils
    * avoid redundant string construction
    * prefer STL algorithms
    * factor out common code,

--- Calo/CaloUtils/src/CaloAlgUtils.cpp
+++ Calo/CaloUtils/src/CaloAlgUtils.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "Event/ProtoParticle.h"
 #include "CaloDet/DeCalorimeter.h"
 #include "Event/CaloDigit.h"
@@ -6,11 +7,22 @@
 #include "GaudiKernel/IRegistry.h"
 
 
+using LHCb::CaloAlgUtils::details::contains_ci;
+namespace {
+   std::string pathFromContext(const std::initializer_list<std::pair<boost::string_ref,boost::string_ref>>& table, boost::string_ref val, boost::string_ref context)
+   {
+       auto i = std::find_if( std::begin(table), std::end(table),
+                        [&](const std::pair<boost::string_ref,boost::string_ref>& item) { return contains_ci( val, item.first); } );
+       return i!=std::end(table) ? LHCb::CaloAlgUtils::PathFromContext(context, i->second) : std::string{};
+   }
+}
+
+
 // Default location for CaloObject as function of detector and possibly context
 
 // DetectorElement location from name
-std::string LHCb::CaloAlgUtils::DeCaloLocation( std::string name ){
-  std::string det = LHCb::CaloAlgUtils::CaloNameFromAlg( name );
+std::string LHCb::CaloAlgUtils::DeCaloLocation( const std::string& name ){
+  std::string det = CaloNameFromAlg( name );
   if ( det == "Ecal" ) return  DeCalorimeterLocation::Ecal;
   else if ( det == "Hcal")return  DeCalorimeterLocation::Hcal;
   else if ( det == "Prs")return  DeCalorimeterLocation::Prs;
@@ -19,15 +31,9 @@ std::string LHCb::CaloAlgUtils::DeCaloLocation( std::string name ){
 }
 
 // Digit location from name (CaloDigits are context independent so far)
-std::string  LHCb::CaloAlgUtils::CaloDigitLocation( std::string name  , std::string context ){
-  std::string det = LHCb::CaloAlgUtils::CaloNameFromAlg( name );
-
+std::string  LHCb::CaloAlgUtils::CaloDigitLocation( const std::string& name  , const std::string&){
+  std::string det = CaloNameFromAlg( name );
   using namespace LHCb::CaloDigitLocation;
-
-  std::string cont = context;
-  if(cont != "")cont = ""; // just to avoid a compilation warning
-
-
   if ( det == "Ecal" ) return Ecal;
   else if ( det == "Hcal")return Hcal;
   else if ( det == "Prs")return  Prs;
@@ -38,94 +44,87 @@ std::string  LHCb::CaloAlgUtils::CaloDigitLocation( std::string name  , std::str
 
 
 // Cluster location from context
-std::string  LHCb::CaloAlgUtils::CaloSplitClusterLocation( std::string context ){
-  using namespace LHCb::CaloClusterLocation;
-  return LHCb::CaloAlgUtils::PathFromContext( context , EcalSplit ) ;
+std::string  LHCb::CaloAlgUtils::CaloSplitClusterLocation( const std::string& context ){
+  return PathFromContext( context , LHCb::CaloClusterLocation::EcalSplit ) ;
 }
 
 
-std::string  LHCb::CaloAlgUtils::CaloClusterLocation( std::string name , std::string context){
-
+std::string  LHCb::CaloAlgUtils::CaloClusterLocation( const std::string& name , const std::string& context) {
   //##  splitClusters
-  std::string uName = toUpper( name );
-  if( std::string::npos != uName.find( "SPLITCLUSTER" ) )
-    return CaloSplitClusterLocation( context );
+  if( contains_ci(name, "SPLITCLUSTER" ) ) return CaloSplitClusterLocation(context);
 
   // ## standard clusters
-  std::string det = LHCb::CaloAlgUtils::CaloNameFromAlg( name );
+  std::string det = CaloNameFromAlg( name );
 
   // caloClusters are almost context-independent : either Offline type or HLT-type!
-  // MUST be synchronous with CaloRecoConf 
-  std::string uCont = toUpper( context );
-  if( std::string::npos != uCont.find( "HLT" ) )
-    context = "Hlt";
-  else
-    context = "";
-  
+  // MUST be synchronous with CaloRecoConf
   using namespace LHCb::CaloClusterLocation;
-  if ( det == "Hcal" )return LHCb::CaloAlgUtils::PathFromContext( context ,Hcal );
-  return LHCb::CaloAlgUtils::PathFromContext( context , Ecal  ); // default is Ecal 
+  return PathFromContext(
+                contains_ci( context, "HLT" )  ? "Hlt" : "",
+                det == "Hcal" ? Hcal : Ecal // default is Ecal
+  );
 }
 
 // Hypo location from type/context
-std::string  LHCb::CaloAlgUtils::CaloHypoLocation( std::string type , std::string context){
+std::string  LHCb::CaloAlgUtils::CaloHypoLocation( const std::string& type , const std::string& context){
   using namespace LHCb::CaloHypoLocation;
-  std::string uType = toUpper(type);
-  if ( std::string::npos != uType.find("ELECTRON") ) return LHCb::CaloAlgUtils::PathFromContext( context ,Electrons );
-  else if ( std::string::npos != uType.find("SPLITPHOTON") )return LHCb::CaloAlgUtils::PathFromContext( context , SplitPhotons );
-  else if ( std::string::npos != uType.find("BREM") )return LHCb::CaloAlgUtils::PathFromContext( context ,Photons );//Brem=photon
-  else if ( std::string::npos != uType.find("PHOTON"))return LHCb::CaloAlgUtils::PathFromContext( context ,Photons);//after split 
-  else if ( std::string::npos != uType.find("MERGED") )return LHCb::CaloAlgUtils::PathFromContext( context ,MergedPi0s );
-  return "";
+  static const std::initializer_list<std::pair<boost::string_ref, boost::string_ref>> table =
+                            { { "ELECTRON",    Electrons },
+                              { "SPLITPHOTON", SplitPhotons },
+                              { "BREM",        Photons }, //Brem=photon
+                              { "PHOTON",      Photons },//after split
+                              { "MERGED",      MergedPi0s } } ;
+  return pathFromContext(  table, type, context );
 }
 
 //CaloId location from type/context
-std::string LHCb::CaloAlgUtils::CaloIdLocation( std::string type, std::string context){
+std::string LHCb::CaloAlgUtils::CaloIdLocation( const std::string& type, const std::string& context){
   using namespace LHCb::CaloIdLocation;
-  std::string uType = toUpper(type);
-  if( std::string::npos != uType.find("BREMMATCH" ))return PathFromContext( context , BremMatch ) ;
-  else if( std::string::npos != uType.find("ELECTRONMATCH" ))return PathFromContext( context , ElectronMatch   ) ;
-  else if( std::string::npos != uType.find("PHOTONMATCH"  ))return PathFromContext( context , ClusterMatch     ) ;//=ClusterMatch
-  else if( std::string::npos != uType.find("CLUSTERMATCH"  ))return PathFromContext( context , ClusterMatch    ) ;
-  else if( std::string::npos != uType.find("BREMCHI2"  ))return PathFromContext( context , BremChi2   ) ;
-  else if( std::string::npos != uType.find("ECALCHI2"  ))return PathFromContext( context , EcalChi2   ) ;
-  else if( std::string::npos != uType.find("CLUSCHI2"  ))return PathFromContext( context , ClusChi2   ) ; 
-  else if( std::string::npos != uType.find("ECALE"     ))return PathFromContext( context , EcalE      ) ;
-  else if( std::string::npos != uType.find("HCALE"     ))return PathFromContext( context , HcalE      ) ;
-  else if( std::string::npos != uType.find("PRSE"      ))return PathFromContext( context , PrsE       ) ;
-  else if( std::string::npos != uType.find("SPDE"      ))return PathFromContext( context , SpdE       ) ;
-  else if( std::string::npos != uType.find("PRSPIDE"   ))return PathFromContext( context , PrsPIDe    ) ; 
-  else if( std::string::npos != uType.find("BREMPIDE"  ))return PathFromContext( context , BremPIDe   ) ; 
-  else if( std::string::npos != uType.find("ECALPIDE"  ))return PathFromContext( context , EcalPIDe   ) ;
-  else if( std::string::npos != uType.find("HCALPIDE"  ))return PathFromContext( context , HcalPIDe   ) ;
-  else if( std::string::npos != uType.find("ECALPIDMU" ))return PathFromContext( context , EcalPIDmu  ) ;
-  else if( std::string::npos != uType.find("HCALPIDMU" ))return PathFromContext( context , HcalPIDmu  ) ;
-  else if( std::string::npos != uType.find("INBREM" ))return PathFromContext( context , InBrem        ) ;
-  else if( std::string::npos != uType.find("INECAL" ))return PathFromContext( context , InEcal        ) ;
-  else if( std::string::npos != uType.find("INHCAL" ))return PathFromContext( context , InHcal        ) ;
-  else if( std::string::npos != uType.find("INPRS"  ))return PathFromContext( context , InPrs         ) ;
-  else if( std::string::npos != uType.find("INSPD"  ))return PathFromContext( context , InSpd         ) ;
-  else if( std::string::npos != uType.find("PHOTONFROMMERGEDID"  ))return PathFromContext( context , PhotonFromMergedID   );
-  else if( std::string::npos != uType.find("SPLITPHOTONID"  ))return PathFromContext( context , PhotonFromMergedID   ); //same
-  else if( std::string::npos != uType.find("PHOTONID"  ))return PathFromContext( context , PhotonID   ) ;// after SplitPhotonID
-  else if( std::string::npos != uType.find("MERGEDID"  ))return PathFromContext( context , MergedID   ) ;// after PhotonFromMerged
-  return "";
+  static const std::initializer_list<std::pair<boost::string_ref, boost::string_ref>> table =
+                            { { "BREMMATCH"     , BremMatch },
+                              { "ELECTRONMATCH" , ElectronMatch},
+                              { "PHOTONMATCH"   , ClusterMatch  }, //=ClusterMatch
+                              { "CLUSTERMATCH"  , ClusterMatch  },
+                              { "BREMCHI2"      , BremChi2   },
+                              { "ECALCHI2"      , EcalChi2   },
+                              { "CLUSCHI2"      , ClusChi2   },
+                              { "ECALE"         , EcalE      },
+                              { "HCALE"         , HcalE      },
+                              { "PRSE"          , PrsE       },
+                              { "SPDE"          , SpdE       },
+                              { "PRSPIDE"       , PrsPIDe    },
+                              { "BREMPIDE"      , BremPIDe   },
+                              { "ECALPIDE"      , EcalPIDe   },
+                              { "HCALPIDE"      , HcalPIDe   },
+                              { "ECALPIDMU"     , EcalPIDmu  },
+                              { "HCALPIDMU"     , HcalPIDmu  },
+                              { "INBREM"        , InBrem     },
+                              { "INECAL"        , InEcal     },
+                              { "INHCAL"        , InHcal     },
+                              { "INPRS"         , InPrs      },
+                              { "INSPD"         , InSpd      },
+                              { "PHOTONFROMMERGEDID", PhotonFromMergedID   },
+                              { "SPLITPHOTONID" , PhotonFromMergedID }, //same
+                              { "PHOTONID"      , PhotonID   },// after SplitPhotonID
+                              { "MERGEDID"      , MergedID   },// after PhotonFromMerged
+                            };
+  return pathFromContext(  table, type, context );
 }
 
-    
+
 
 
 // Track location from context - specific path
-std::vector<std::string>  LHCb::CaloAlgUtils::TrackLocations( std::string context){
+std::vector<std::string>  LHCb::CaloAlgUtils::TrackLocations(const std::string& context){
   std::vector<std::string> locs;
   using namespace LHCb::TrackLocation;
 
-  
+
   // ----  push first the predefined contexts
-  // if( toUpper(context) == "HLT1UNFITTED" || toUpper(context) == "HLT1")locs.push_back("/HLT1/Track/Unfitted/Default"); 
-  // else if( toUpper(context) == "HLT1FITTED")locs.push_back( "/HLT1/Track/Fitted/Default"); 
-  // else if( toUpper(context) == "HLT2UNFITTED" || toUpper(context) == "HLT2")locs.push_back("/HLT2/Track/Unfitted/Default"); 
-  // else if( toUpper(context) == "HLT2FITTED")locs.push_back("/HLT2/Track/Fitted/Default"); 
+  // if( toUpper(context) == "HLT1UNFITTED" || toUpper(context) == "HLT1")locs.push_back("/HLT1/Track/Unfitted/Default");
+  // else if( toUpper(context) == "HLT1FITTED")locs.push_back( "/HLT1/Track/Fitted/Default");
+  // else if( toUpper(context) == "HLT2UNFITTED" || toUpper(context) == "HLT2")locs.push_back("/HLT2/Track/Unfitted/Default");
+  // else if( toUpper(context) == "HLT2FITTED")locs.push_back("/HLT2/Track/Fitted/Default");
   if( hltContext( context ) )locs.push_back("Hlt2/Track/Forward");
   else if ( toUpper(context) == "OFFLINE" || context == "" )locs.push_back(Default);  // Rec/Track/Best
   else locs.push_back(Default); // default is offline
@@ -136,104 +135,73 @@ std::vector<std::string>  LHCb::CaloAlgUtils::TrackLocations( std::string contex
 }
 
 const LHCb::CaloCluster*  LHCb::CaloAlgUtils::ClusterFromHypo(const LHCb::CaloHypo* hypo,bool split){
-  if( NULL == hypo)return NULL;
+  if( !hypo ) return nullptr;
 
-  if( 1 == hypo->clusters().size() )return hypo->clusters().front(); // single hypo
+  if( 1 == hypo->clusters().size() ) return hypo->clusters().front(); // single hypo
 
-  const SmartRefVector<LHCb::CaloCluster>& clusters = hypo->clusters();
+  const auto& clusters = hypo->clusters();
   // split hypo
-  if( NULL != hypo->parent() && NULL != hypo->parent()->registry() ){
-    std::string hcont = hypo->parent()->registry()->identifier();
-    std::string uHcont = toUpper( hcont );
-    bool hsplit = ( std::string::npos != uHcont.find( "SPLIT" ) && 1 < hypo->clusters().size() ); // split hypo
-    if( !hsplit )return hypo->clusters().front(); // SHOULD NEVER OCCUR
-    SmartRef<LHCb::CaloCluster> cluster ;
-    for(LHCb::CaloHypo::Clusters::const_iterator icl =  clusters.begin();icl!= clusters.end();++icl){
-      if( 0 == *icl)continue;
-      std::string ccont = (*icl)->parent()->registry()->identifier();
-      std::string uCcont = toUpper( ccont );
-      if( split  &&  std::string::npos != uCcont.find( "SPLIT" ) )cluster = *icl;   // return splitCluster
-      else if( !split &&  std::string::npos == uCcont.find( "SPLIT" ) )cluster = *icl; // return mainCluster
+  if ( hypo->parent() && hypo->parent()->registry() ) {
+    bool hsplit = ( contains_ci(  hypo->parent()->registry()->identifier(), "SPLIT" ) && 1 < clusters.size() ); // split hypo
+    if( !hsplit ) return clusters.front(); // SHOULD NEVER OCCUR
+    const LHCb::CaloCluster* cluster = nullptr;
+    for(const auto& cl :  clusters) {
+      bool split_container = contains_ci( cl->parent()->registry()->identifier(), "SPLIT" );
+      if     (  split &&  split_container ) cluster = cl;   // return splitCluster
+      else if( !split && !split_container ) cluster = cl; // return mainCluster
     }
     return cluster;
-    
-  }else{ // unregistered caloHypo -> return the smallest/largest cluster according to getsplit
-    SmartRef<LHCb::CaloCluster> minCl ;
-    SmartRef<LHCb::CaloCluster> maxCl ;
-    unsigned int min = 999999;
-    unsigned int max = 0;
-    for(LHCb::CaloHypo::Clusters::const_iterator icl = clusters.begin();icl!=clusters.end();++icl){
-      if( 0 == *icl)continue;
-      if( (*icl)->entries().size() < min){
-        minCl = *icl;
-        min = (*icl)->entries().size();
-      }
-      if( (*icl)->entries().size() > max){
-        maxCl = *icl;
-        max = (*icl)->entries().size();
-      }
-    }
-    return split ? minCl : maxCl;
   }
+  // unregistered caloHypo -> return the smallest/largest cluster according to getsplit
+  constexpr auto smallest = [](const LHCb::CaloCluster* lhs, const LHCb::CaloCluster* rhs)
+             { return lhs->entries().size() < rhs->entries().size(); };
+  constexpr auto largest  = [](const LHCb::CaloCluster* lhs, const LHCb::CaloCluster* rhs)
+             { return lhs->entries().size() > rhs->entries().size(); };
+  auto i = std::min_element( clusters.begin(), clusters.end(),
+                             split ? smallest : largest ); // only works because non-capturing lambda can be converted to pointer to function
+  return i!=clusters.end() ? i->target() : nullptr;
 }
 
-  
-
-bool LHCb::CaloAlgUtils::StringMatcher(std::vector<std::string> refs, std::string name){
+bool LHCb::CaloAlgUtils::StringMatcher(const std::vector<std::string>& refs, const std::string& name){
 
   bool ok=false;
   bool ko=true;
 
-  if( refs.empty())return true;
-  for( std::vector<std::string>::iterator iref=refs.begin() ; refs.end() != iref; ++iref){
-    std::string ref = *iref;
-    if( toUpper(ref) == "ALL")ok=true;
-    if( toUpper(ref) == "NONE")return false;
+  if( refs.empty()) return true;
+  for( const auto& ref : refs ) {
+    if( toUpper(ref) == "ALL") ok=true;
+    if( toUpper(ref) == "NONE") return false;
     bool match = StringMatcher( ref , name );
-    if( ref.find("!") != std::string::npos && !match)ko=false;
-    if( ref.find("!") == std::string::npos && match )ok=true;
+    if( contains_ci( ref, "!") && !match)ko=false;
+    if( !contains_ci( ref, "!") && match )ok=true;
   }
   if( !ko )return  false; // rejection wins
-  if(  ok )return  true;
-  return false;
+  return ok;
 }
 
-
-bool LHCb::CaloAlgUtils::StringMatcher(std::string ref, std::string name){
-
-  std::string uref=ref;
-  if( toUpper(uref) == "ALL")return true;
-  if( toUpper(uref) == "NONE")return false;
-  std::string uname=name;
+bool LHCb::CaloAlgUtils::StringMatcher(const std::string& ref, const std::string& name){
+  std::string uref= toUpper(ref);
+  if( uref == "ALL") return true;
+  if( uref == "NONE")return false;
   bool rev=false;
-  if( std::string::npos != uref.find("!")  ){
+  if( contains_ci( uref, "!")  ){
     rev=true;
     int ind=uref.find_first_of("!");
     uref =  uref.erase(ind,1);
   }
   bool exact=true;
-  if( std::string::npos !=  uref.find("*") ){
+  if( contains_ci( uref, "*") ){
     exact=false;
     if( uref.find_first_of("*") != std::string::npos){
-    int i1=uref.find_first_of("*");
-    uref.erase(i1,1);
+      int i1=uref.find_first_of("*");
+      uref.erase(i1,1);
     }
     if( uref.find_last_of("*") != std::string::npos){
       int i2=uref.find_last_of("*");
       uref.erase(i2,1);
     }
   }
-  
-  uname = toUpper(uname);
-  uref  = toUpper(uref);
-  
-  bool match;
-  if( exact )
-    match = ( uref == uname );
-  else
-    match = (uname.find(uref) != std::string::npos);
-
+  std::string uname=toUpper(name);
+  bool match = ( exact ?  uref == uname : contains_ci(uname, uref) );
   return rev ? !match : match;
-
 }
-
