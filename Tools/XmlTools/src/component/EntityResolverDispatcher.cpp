b336eb5e919183fac2a8bccb22c337312f96f4ad
Marco Clemencic <marco.clemencic@cern.ch>
2016-08-17 16:50:37 +0200

    enhanced EntityResolverDispatcher to redirect/remap URLs

--- Tools/XmlTools/src/component/EntityResolverDispatcher.cpp
+++ Tools/XmlTools/src/component/EntityResolverDispatcher.cpp
@@ -1,15 +1,31 @@
+#include "GaudiKernel/ParsersFactory.h"
+
 #include "GaudiKernel/AlgTool.h"
 #include "Kernel/ICondDBInfo.h"
 #include "XmlTools/IXmlEntityResolver.h"
+
+#include <regex>
+
+#include <xercesc/util/XMLString.hpp>
+
+namespace
+{
+  struct XMLStrDeleter {
+    void operator()( char* c ) { xercesc::XMLString::release( &c ); }
+    void operator()( XMLCh* c ) { xercesc::XMLString::release( &c ); }
+  };
+}
 
 /// Simple xercesc::EntityResolver that can delegate to other resolvers
 class EntityResolverDispatcher : public extends<AlgTool, IXmlEntityResolver, ICondDBInfo>,
                                  virtual public xercesc::EntityResolver
 {
 
-  /// implementations of IXmlEntityResolver.
   StringArrayProperty m_resolversNames{"EntityResolvers", {}};
+  SimpleProperty<std::vector<std::pair<std::string, std::string>>> m_mappingsDesc{"Mappings", {}};
+
   std::vector<SmartIF<IXmlEntityResolver>> m_resolvers;
+  std::vector<std::pair<std::regex, const std::string&>> m_mappings;
 
 public:
   EntityResolverDispatcher( const std::string& type, const std::string& name, const IInterface* parent )
@@ -23,6 +39,17 @@ public:
         if ( !toolSvc()->retrieveTool( name, tmp ) )
           throw GaudiException( "failed to retrieve tool " + name, this->name(), StatusCode::FAILURE );
         m_resolvers.emplace_back( tmp );
+      }
+    } );
+
+    declareProperty( m_mappingsDesc.name(), m_mappingsDesc,
+                     "list of pairs of regex and substitution patterns to redirect requests" );
+    m_mappingsDesc.declareUpdateHandler( [this]( Property& ) {
+      m_mappings.clear();
+      for ( const auto& mapping : m_mappingsDesc.value() ) {
+        if ( UNLIKELY( msgLevel( MSG::DEBUG ) ) )
+          debug() << "defined mapping \"" << mapping.first << "\" -> \"" << mapping.second << "\"" << endmsg;
+        m_mappings.emplace_back( std::regex{mapping.first}, mapping.second );
       }
     } );
   }
@@ -40,6 +67,18 @@ public:
   /// If the systemId does not begin with "conddb:", it returns NULL, so the parser go on with the default action.
   xercesc::InputSource* resolveEntity( const XMLCh* const publicId, const XMLCh* const systemId ) override
   {
+    std::unique_ptr<XMLCh, XMLStrDeleter> tmpSysId;
+    if ( !m_mappings.empty() ) {
+      std::unique_ptr<char, XMLStrDeleter> tmp1{xercesc::XMLString::transcode( systemId )};
+      std::string tmp2{tmp1.get()};
+      for ( const auto& mapping : m_mappings ) {
+        tmp2 = std::regex_replace( tmp2, mapping.first, mapping.second );
+      }
+      tmpSysId.reset( xercesc::XMLString::transcode( tmp2.c_str() ) );
+      *const_cast<XMLCh**>( &systemId ) = tmpSysId.get();
+      if ( UNLIKELY( msgLevel( MSG::DEBUG ) && tmp2.compare( tmp1.get() ) ) )
+        debug() << "mapped " << tmp1.get() << " to " << tmp2 << endmsg;
+    }
     xercesc::InputSource* out = nullptr;
     for ( auto& resolver : m_resolvers ) {
       out = resolver->resolver()->resolveEntity( publicId, systemId );
