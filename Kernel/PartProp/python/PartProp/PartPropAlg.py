d6e1915b70b03a911d754c59a57bbf3e1df6c75e
ibelyaev <Ivan.Belyaev@cern.ch>
2016-08-17 09:53:49 +0200

    Kernel/PartProp: ! 2016-08-17 - Vanya Belyaev  - update a bit DumpParticleProperties    - allow to define the explicit DDDB and CONDDB tags, DataType and Simulation flag    - make ose of ArgumentParser    - define alias    - fix doxygen

--- Kernel/PartProp/python/PartProp/PartPropAlg.py
+++ Kernel/PartProp/python/PartProp/PartPropAlg.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env gaudirun.py
 # =============================================================================
-# $Id$
-# =============================================================================
 ## @file
 #  The configurationi file to run QMTest for the package Kernel/PartProp
 #  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
 #  @date   2008-12-02
 # =============================================================================
-"""
-The configurationi file to run QMTest for the package Kernel/PartProp
+""" The configuration file to run QMTest for the package Kernel/PartProp
 """
 # =============================================================================
 __author__  =  "Vanya BELYAEV Ivan.Belyaev@nikhef.nl"
-__version__ =  "CVS tag $Name: not supported by cvs2svn $, version $Revision$"
 # =============================================================================
 
 from Gaudi.Configuration import *
@@ -23,8 +19,6 @@ from Configurables import LHCb__Example__PartPropAlg as PPA
 ## needed for the proper access to Particle table  from DDDB  
 from Configurables import DDDBConf 
 DDDBConf       ()  # use the default Configuration of DDDB
-
-
 
 ApplicationMgr ( TopAlg = [ PPA() ] ,
                  EvtSel = "NONE"    ,
