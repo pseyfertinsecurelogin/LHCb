cb9bd53c9e41ba1e48ec55105582fb855abffbe4
Gerhard Raven <gerhard.raven@nikhef.nl>
2016-10-03 22:36:02 +0200

    Modernize Muon & L0
    * prefer STL algorithms over raw loops
    * prefer range-based for loops
    * keep MuonSystemLayout as static const instead of member data
    * prefer direct member initialization
    * prefer assignment of vector over clear() followed by push_back()
    * prefer std::unique_ptr over raw pointer with explicit delete
    * remove obsolete SVN keywords
    * add const
    * add final
    * remove unnecessary virtual destructors
    * prefer std::array over C-style array
    * remove use of deprececated std::binary_function
    * prefer override over virtual

--- Muon/MuonDAQ/Event/MuonPPEventInfo.h
+++ Muon/MuonDAQ/Event/MuonPPEventInfo.h
@@ -1,55 +1,31 @@
-// $Id: MuonPPEventInfo.h,v 1.1 2008-04-02 13:25:52 asatta Exp $
-#ifndef EVENT_MUONPPEVENTINFO_H 
+#ifndef EVENT_MUONPPEVENTINFO_H
 #define EVENT_MUONPPEVENTINFO_H 1
 
 // Include files
 
 /** @class MuonPPEventInfo MuonPPEventInfo.h Event/MuonPPEventInfo.h
- *  
  *
- *  @author 
+ *
+ *  @author
  *  @date   2008-01-07
  */
-class MuonPPEventInfo {
-public: 
-  /// Standard constructor
-  MuonPPEventInfo( ){
-    for (int i=0;i<8;i++)
-    {      
-      m_data[i]=0;
-    }
-    
-  }; 
+class MuonPPEventInfo final {
+public:
 
-  virtual ~MuonPPEventInfo( ){}; ///< Destructor
+  /// Standard constructor
+  MuonPPEventInfo( ){ m_data.fill(0); };
+
   void setWord(unsigned int i, unsigned int word)
   {
     m_data[i]=word;
-    
   };
-  
-  unsigned int getBCN()
-  {
-    return m_data[0]&0x0FFF;
-    
-  };
-  
-  unsigned int getDetID()
-  {
-    return m_data[0]&0xF000;
-    
-  }
-   unsigned int getL0EvtID()
-  {
-    return m_data[1];
-    
-  };
-  
 
-protected:
+  unsigned int getBCN() const { return m_data[0]&0x0FFF; };
+  unsigned int getDetID() const { return m_data[0]&0xF000; }
+  unsigned int getL0EvtID() const { return m_data[1]; };
 
 private:
-  unsigned int m_data[8];
-  
+   std::array<unsigned int,8> m_data;
+
 };
 #endif // EVENT_MUONPPEVENTINFO_H
--- Muon/MuonDAQ/MuonDAQ/SortDigitInODE.h
+++ Muon/MuonDAQ/MuonDAQ/SortDigitInODE.h
@@ -1,32 +1,17 @@
-// $Id: SortDigitInODE.h,v 1.2 2008-04-02 11:52:05 asatta Exp $
 #ifndef SORTDIGITINODE_H 
 #define SORTDIGITINODE_H 1
 
-
-
-#include <iostream>
-#include <functional>
 #include "MuonDAQ/MuonHLTDigitFormat.h"
 #include "MuonHLTBase.h"
 
-class SortDigitInODE :
-  public std::binary_function<unsigned int,unsigned int,bool>{
-public :
+struct SortDigitInODE {
   bool operator()(  const unsigned int first, 
-                    const unsigned int second )
-    const;
-};
+                    const unsigned int second ) const {
+     return (( first  & MuonHLTBaseDC06::MaskAddress ) >> MuonHLTBaseDC06::ShiftAddress) 
+          < (( second & MuonHLTBaseDC06::MaskAddress ) >> MuonHLTBaseDC06::ShiftAddress);
 
-
-inline bool SortDigitInODE::operator()(const unsigned int first,
-const unsigned int second )const
-{
-  return (( first & MuonHLTBaseDC06::MaskAddress ) >> 
-MuonHLTBaseDC06::ShiftAddress) 
-    <(( second & MuonHLTBaseDC06::MaskAddress ) 
->>MuonHLTBaseDC06::ShiftAddress);
   }
-
+};
 
 
 #endif // SORTDIGITINODE_H
