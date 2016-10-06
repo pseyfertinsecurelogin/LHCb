7f2a520f25d1c9170b1b9cd0167aa2dc1924083b
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2016-10-06 16:10:51 +0100

    Add RichFutureUtils package

---
+++ Rich/RichFutureUtils/dict/RichFutureUtilsDict.h
@@ -0,0 +1,16 @@
+#ifndef DICT_RICHUTILSDICT_H 
+#define DICT_RICHUTILSDICT_H 1
+
+#include "RichFutureUtils/RichHypoData.h"
+
+// instanciate templated classes
+namespace 
+{
+  struct _Instantiations 
+  {
+    Rich::Future::HypoData<float>         obj_1;
+    Rich::Future::HypoData<double>        obj_2;
+  };
+}
+
+#endif // DICT_RICHUTILSDICT_H
