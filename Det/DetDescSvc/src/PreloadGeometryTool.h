9309407d26c5a59a534d68e5b2f773c20efcbf3c
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2016-10-12 15:03:12 +0100

    Fix untested statuscode, prefer std::unqiue_ptr to raw pointer and remove uneccessary destructor

--- Det/DetDescSvc/src/PreloadGeometryTool.h
+++ Det/DetDescSvc/src/PreloadGeometryTool.h
@@ -2,11 +2,12 @@
 #ifndef PRELOADGEOMETRYTOOL_H 
 #define PRELOADGEOMETRYTOOL_H 1
 
-// Include files
+// STL
+#include <memory>
+
 // from Gaudi
 #include "GaudiAlg/GaudiTool.h"
 #include "GaudiAlg/IGenericTool.h"            // Interface
-
 
 /** @class PreloadGeometryTool PreloadGeometryTool.h
  *  Tool to traverse the full geometry tree and load it
@@ -14,19 +15,18 @@
  *  @author Marco Cattaneo
  *  @date   2006-07-26
  */
-class PreloadGeometryTool : public GaudiTool, virtual public IGenericTool {
+class PreloadGeometryTool : public GaudiTool, virtual public IGenericTool
+{
+
 public: 
+
   /// Standard constructor
   PreloadGeometryTool( const std::string& type, 
                        const std::string& name,
-                       const IInterface* parent);
-
-  virtual ~PreloadGeometryTool( ); ///< Destructor
+                       const IInterface* parent );
 
   void execute(); ///< Load the geometry tree
-protected:
-
-private:
 
 };
+
 #endif // PRELOADGEOMETRYTOOL_H
