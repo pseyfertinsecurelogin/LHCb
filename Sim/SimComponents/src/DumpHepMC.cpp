e641af683713dc3082ebbcfd433f13df4addf595
Gerhard Raven <gerhard.raven@nikhef.nl>
2016-10-19 00:06:23 +0200

    Fix two buffer overruns, and protect against others

--- Sim/SimComponents/src/DumpHepMC.cpp
+++ Sim/SimComponents/src/DumpHepMC.cpp
@@ -1,5 +1,5 @@
 // ============================================================================
-// Include files 
+// Include files
 // ============================================================================
 // GaudiKernel
 // ============================================================================
@@ -16,7 +16,7 @@
 
 #include "DumpHepMC.h"
 
-/** @file 
+/** @file
  *  implementation file for the class DumpHepMC.
  *  Just a renaming of the DumpMC class that was in Gen/Generators package
  */
@@ -26,48 +26,48 @@
 DECLARE_ALGORITHM_FACTORY( DumpHepMC )
 
 // ============================================================================
-/** standard constructor 
+/** standard constructor
  *  @see GaudiAlgorithm
  *  @see      Algorithm
  *  @see      AlgFactory
- *  @see     IAlgFactory 
- *  @param name algorithm instance's name 
- *  @param iscv pointer to Service Locator 
+ *  @see     IAlgFactory
+ *  @param name algorithm instance's name
+ *  @param iscv pointer to Service Locator
  */
 // ============================================================================
-DumpHepMC::DumpHepMC ( const std::string& name , 
-                       ISvcLocator*       isvc ) 
-  : GaudiAlgorithm( name , isvc ) 
+DumpHepMC::DumpHepMC ( const std::string& name ,
+                       ISvcLocator*       isvc )
+  : GaudiAlgorithm( name , isvc )
   , m_addresses()
 {
   m_addresses.push_back( LHCb::HepMCEventLocation::Default ) ;  // default!
-  // define the property 
+  // define the property
   declareProperty ("Addresses" , m_addresses ) ;
 }
 // ============================================================================
 
 // ============================================================================
-// Destructor 
+// Destructor
 // ============================================================================
 DumpHepMC::~DumpHepMC() {}
 // ============================================================================
 
 // ============================================================================
 /** execution of the algoritm
- *  @see IAlgorithm 
- *  @return status code 
+ *  @see IAlgorithm
+ *  @return status code
  */
 // ============================================================================
-StatusCode DumpHepMC::execute() 
+StatusCode DumpHepMC::execute()
 {
-  // no printout at all 
+  // no printout at all
   if( !msgLevel( MSG::INFO ) ) { return StatusCode::SUCCESS ; }
-  
+
   // get the stream
   MsgStream& log = info() ;
-  
-  for( Addresses::const_iterator ia = m_addresses.begin() ; 
-       m_addresses.end() != ia ; ++ia ) 
+
+  for( Addresses::const_iterator ia = m_addresses.begin() ;
+       m_addresses.end() != ia ; ++ia )
   {
     //
     if( UNLIKELY( msgLevel(MSG::DEBUG) ) )
@@ -75,27 +75,27 @@ StatusCode DumpHepMC::execute()
     LHCb::HepMCEvents* events = get<LHCb::HepMCEvents>( *ia ) ;
     if( 0 == events ) { continue ; }
     //
-    info  () << " HepMC container '" << *ia << "' \t has " 
+    info  () << " HepMC container '" << *ia << "' \t has "
              << events->size() << " event(s) " << endmsg ;
-    for ( LHCb::HepMCEvents::const_iterator ie = events->begin() ; 
-          events->end() != ie ; ++ie ) 
+    for ( LHCb::HepMCEvents::const_iterator ie = events->begin() ;
+          events->end() != ie ; ++ie )
     {
       const LHCb::HepMCEvent* event = *ie ;
       if ( 0 == event ) { continue ; }
       log << "  Generator '" << event->generatorName() << "'" << std::endl ;
-      if ( log.isActive() ) 
+      if ( log.isActive() )
         { orderedPrint( event->pGenEvt() , log.stream() ) ; }
       log << endmsg ;
     }
   };
-  
+
   return StatusCode::SUCCESS;
 }
 // ============================================================================
 // Print ordered HepMC
 //=============================================================================
-void DumpHepMC::orderedPrint( const HepMC::GenEvent * theEvent , 
-                           std::ostream & ostr ) 
+void DumpHepMC::orderedPrint( const HepMC::GenEvent * theEvent ,
+                           std::ostream & ostr )
   const {
   // dumps the content of this event to ostr
   //   to dump to cout use: event.print();
@@ -103,43 +103,43 @@ void DumpHepMC::orderedPrint( const HepMC::GenEvent * theEvent ,
   //      std::ofstream outfile("outfile.txt"); event.print( outfile );
   ostr << "________________________________________"
        << "________________________________________\n";
-  ostr << "GenEvent: #" << theEvent -> event_number() 
-       << " ID=" << theEvent -> signal_process_id() 
-       << " SignalProcessGenVertex Barcode: " 
-       << ( theEvent -> signal_process_vertex() ? 
+  ostr << "GenEvent: #" << theEvent -> event_number()
+       << " ID=" << theEvent -> signal_process_id()
+       << " SignalProcessGenVertex Barcode: "
+       << ( theEvent -> signal_process_vertex() ?
             theEvent -> signal_process_vertex()->barcode()
             : 0 )
        << "\n";
-  ostr << " Entries this event: " 
+  ostr << " Entries this event: "
        << theEvent -> vertices_size() << " vertices, "
-       << theEvent -> particles_size() << " particles.\n"; 
+       << theEvent -> particles_size() << " particles.\n";
   // print a legend to describe the particle info
   char particle_legend[80];
-  sprintf( particle_legend,"      %9s %8s %9s,%9s,%9s,%8s %4s %9s",
+  snprintf( particle_legend,80,"      %9s %8s %9s,%9s,%9s,%8s %4s %9s",
            "Barcode","PDG ID","( Px","Py","Pz","E )","Stat","DecayVtx");
   ostr << "                                    GenParticle Legend\n"
        << particle_legend << "\n";
   ostr << "________________________________________"
        << "________________________________________\n";
   // Print all Vertices
-  for ( HepMC::GenEvent::vertex_const_iterator 
+  for ( HepMC::GenEvent::vertex_const_iterator
           vtx = theEvent -> vertices_begin();
         vtx != theEvent -> vertices_end(); ++vtx ) {
-    orderedVertexPrint(*vtx , ostr); 
+    orderedVertexPrint(*vtx , ostr);
   }
   ostr << "________________________________________"
        << "________________________________________" << std::endl;
 }
 
 //=============================================================================
-// Print vertices ordered 
+// Print vertices ordered
 //=============================================================================
-void DumpHepMC::orderedVertexPrint( HepMC::GenVertex * theVertex , 
+void DumpHepMC::orderedVertexPrint( HepMC::GenVertex * theVertex ,
                                     std::ostream & ostr ) const {
   char outline[80];
   if ( theVertex -> barcode()!=0 ) {
     if ( theVertex -> position() != HepMC::FourVector(0,0,0,0) ) {
-      sprintf( outline,
+      snprintf( outline, 80,
                "Vertex:%9d ID:%5d (X,cT)=%+9.2e,%+9.2e,%+9.2e,%+9.2e"
                ,theVertex -> barcode(), theVertex -> id(),
                theVertex -> position().x(),
@@ -147,7 +147,7 @@ void DumpHepMC::orderedVertexPrint( HepMC::GenVertex * theVertex ,
                theVertex -> position().z(),
                theVertex -> position().t() );
     } else {
-      sprintf( outline, "GenVertex:%9d ID:%5d (X,cT):0", 
+      snprintf( outline, 80, "GenVertex:%9d ID:%5d (X,cT):0",
                theVertex -> barcode(), theVertex -> id() );
     }
   } else {
@@ -155,7 +155,7 @@ void DumpHepMC::orderedVertexPrint( HepMC::GenVertex * theVertex ,
     //  we print its memory address instead... so that the
     //  print out gives us a unique tag for the particle.
     if ( theVertex -> position() != HepMC::FourVector(0,0,0,0) ) {
-      sprintf( outline,
+      snprintf( outline,80,
                "Vertex:%9p ID:%5d (X,cT)=%+9.2e,%+9.2e,%+9.2e,%+9.2e"
                ,(void*)theVertex, theVertex -> id(),
                theVertex -> position().x(),
@@ -163,16 +163,16 @@ void DumpHepMC::orderedVertexPrint( HepMC::GenVertex * theVertex ,
                theVertex -> position().z(),
                theVertex -> position().t() );
     } else {
-      sprintf( outline, "GenVertex:%9p ID:%5d (X,cT):0", 
+      snprintf( outline, 80, "GenVertex:%9p ID:%5d (X,cT):0",
                (void*)theVertex, theVertex -> id() );
     }
   }
-  
+
   ostr << outline << std::endl;
   // print the weights if there are any
   if ( ! theVertex -> weights().empty() ) {
     ostr << " Wgts(" << theVertex -> weights().size() << ")=";
-    for ( HepMC::WeightContainer::const_iterator wgt = 
+    for ( HepMC::WeightContainer::const_iterator wgt =
             theVertex -> weights().begin();
           wgt != theVertex -> weights().end(); wgt++ ) { ostr << *wgt << " "; }
     ostr << std::endl;
@@ -183,28 +183,28 @@ void DumpHepMC::orderedVertexPrint( HepMC::GenVertex * theVertex ,
   std::sort(lP.begin(), lP.end(),  HepMCUtils::compareHepMCParticles ) ;
   for (auto it = lP.begin() ; it != lP.end() ; ++it ) {
     if ( it == lP.begin() ) {
-      char label[5];
-      sprintf( label," I:%2d", theVertex -> particles_in_size() );
-      ostr << label; 
+      char label[6];
+      snprintf( label,6," I:%2d", theVertex -> particles_in_size() );
+      ostr << label;
     } else { ostr << "     "; }
     //(*part1)->print( ostr );  //uncomment for long debugging printout
     ostr << **it << std::endl;
   }
-  
+
   std::vector< const HepMC::GenParticle * > lP2{ theVertex -> particles_out_const_begin(),
                                                  theVertex -> particles_out_const_end() };
   std::sort(lP2.begin(), lP2.end(), HepMCUtils::compareHepMCParticles ) ;
-  
+
   for ( auto it2 = lP2.begin() ; it2 != lP2.end() ; ++it2 ) {
-    if ( it2 == lP2.begin() ) { 
-      char label[5];
-      sprintf( label," O:%2d", theVertex -> particles_out_size() );
-      ostr << label; 
+    if ( it2 == lP2.begin() ) {
+      char label[6];
+      snprintf( label,6," O:%2d", theVertex -> particles_out_size() );
+      ostr << label;
     } else { ostr << "     "; }
     //(*part2)->print( ostr ); // uncomment for long debugging printout
     ostr << **it2 << std::endl;
   }
 }
 // ============================================================================
-// The END 
+// The END
 // ============================================================================
