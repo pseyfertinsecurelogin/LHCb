a6f260ea0b8cb1dff6daf881bddb41008bbeb929
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2016-10-21 16:58:33 +0100

    Use range utilities from GaudiAlg

--- Rich/RichUtils/RichUtils/ZipRange.h
+++ Rich/RichUtils/RichUtils/ZipRange.h
@@ -8,9 +8,11 @@
 #include <type_traits>
 #include <tuple>
 
+// Gaudi
+#include "GaudiAlg/FunctionalDetails.h"
+
 // Range V3
 #include <range/v3/all.hpp>
-//#include "range/v3/view/zip.hpp"
 
 namespace Rich
 {
@@ -28,45 +30,19 @@ namespace Rich
   namespace Ranges
   {
 
-    /// Implementation details
-    namespace
-    {
-      /// Compare sizes of two containers
-      template < typename A, typename B >
-      inline bool check_sizes( const A& a, const B& b ) noexcept
-      {
-        return a.size() == b.size();
-      }
-      
-      /// Compare sizes of 3 or more containers
-      template < typename A, typename B, typename... C >
-      inline bool check_sizes( const A& a, const B& b, const C& ... c ) noexcept
-      {
-        return ( check_sizes(a,b) && check_sizes(b,c...) );
-      }
-    }
-
     /// Zips multiple containers together to form a single range
     template< typename... Args >
     inline decltype(auto) Zip( const Args&... args ) noexcept
     {
-      assert( check_sizes( args... ) );
-      return ranges::view::zip( args... );
+      return Gaudi::Functional::details::zip::range( args... );
     }
 
     /// Zips multiple containers together to form a single const range
     template< typename... Args >
     inline decltype(auto) ConstZip( const Args&... args ) noexcept
     {
-      return ranges::view::const_( Zip(args...) );
+      return Gaudi::Functional::details::zip::const_range( args... );
     }
-
-    /// Access the element for the given container type from the given zipped tuple
-    //template < typename CONT, typename TUPLE >
-    //inline decltype(auto) unzip( const TUPLE & data )
-    //{
-    //  return std::get<const typename std::remove_reference<CONT>::type ::value_type&>(data);
-    //}
 
   }
 }
