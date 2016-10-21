2020c217c842e626e57da5dee7d0b8993da61086
Marco Clemencic <marco.clemencic@cern.ch>
2016-10-21 23:49:03 +0200

    added missing 'override' in Calo/CaloDAQ and Calo/CaloKernel

--- Calo/CaloKernel/CaloKernel/CaloException.h
+++ Calo/CaloKernel/CaloKernel/CaloException.h
@@ -9,10 +9,10 @@
 //
 // Revision 1.3  2001/06/23 14:39:31  ibelyaev
 //  fix CVS-keywords and remove long lines
-// 
+//
 // ===========================================================================
-#ifndef      CALOKERNEL_CALOEXCEPTION_H  
-#define      CALOKERNEL_CALOEXCEPTION_H 1 
+#ifndef      CALOKERNEL_CALOEXCEPTION_H
+#define      CALOKERNEL_CALOEXCEPTION_H 1
 /// ===========================================================================
 // GaudiKernel
 #include "GaudiKernel/Kernel.h"
@@ -21,42 +21,42 @@
 
 /** @class CaloException CaloException.h CaloKernel/CaloException.h
  *
- *  It is just a base class for all exceptions in the Calorimeter system     
- *  
- *  @author  Vanya Belyaev Ivan.Belyaev@itep.ru 
+ *  It is just a base class for all exceptions in the Calorimeter system
+ *
+ *  @author  Vanya Belyaev Ivan.Belyaev@itep.ru
  *  @date    26/11/1999
  */
 
 class CaloException : public GaudiException
-{  
+{
 public:
 
-  /** constructor 
-   *  @param ex  exception message 
+  /** constructor
+   *  @param ex  exception message
    *  @param sc  status code assiciated with exception
-   */ 
-  CaloException( const std::string& ex   = "unspecified exception", 
+   */
+  CaloException( const std::string& ex   = "unspecified exception",
                  const StatusCode&  sc   =  StatusCode::FAILURE )
-    : GaudiException( ex , "*CaloException*" , sc )  {}; 
+    : GaudiException( ex , "*CaloException*" , sc )  {};
 
   /** constructor
-   *  @param ex          exception message 
-   *  @param Exception   previous exception 
+   *  @param ex          exception message
+   *  @param Exception   previous exception
    *  @param sc          status code associated with the exception
-   */  
-  CaloException( const std::string   & ex                        , 
-                 const GaudiException& Ex                        , 
-                 const StatusCode&     sc =  StatusCode::FAILURE ) 
+   */
+  CaloException( const std::string   & ex                        ,
+                 const GaudiException& Ex                        ,
+                 const StatusCode&     sc =  StatusCode::FAILURE )
     : GaudiException( ex, "*CaloException*" , sc , Ex ) {};
-  
+
   /** destructor
    */
-  virtual ~CaloException() throw() {};
-  
+  virtual ~CaloException() throw() {}
+
   /** clone (virtual constructor)
    */
-  virtual GaudiException* clone() const { return new CaloException(*this); }
-  
+  GaudiException* clone() const override { return new CaloException(*this); }
+
 };
 
 // ===========================================================================
