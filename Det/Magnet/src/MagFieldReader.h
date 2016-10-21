a459745e9172fa0e48a02adab2283978407da16e
Marco Clemencic <marco.clemencic@cern.ch>
2016-10-21 23:57:35 +0200

    added missing 'override' in Det/Magnet

--- Det/Magnet/src/MagFieldReader.h
+++ Det/Magnet/src/MagFieldReader.h
@@ -29,9 +29,9 @@ public:
 
   virtual ~MagFieldReader( ){ }; ///< Destructor
 
-  virtual StatusCode initialize();    ///< Algorithm initialization
-  virtual StatusCode execute   ();    ///< Algorithm execution
-  virtual StatusCode finalize   ();    ///< Algorithm finalization
+  StatusCode initialize() override;    ///< Algorithm initialization
+  StatusCode execute   () override;    ///< Algorithm execution
+  StatusCode finalize   () override;    ///< Algorithm finalization
   void TestBdl();
 
 protected:
