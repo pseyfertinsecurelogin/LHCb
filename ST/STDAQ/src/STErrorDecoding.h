b9b1cf8d46f572b33e8f6ec266b98780719bf750
Marco Clemencic <marco.clemencic@cern.ch>
2016-10-21 23:57:36 +0200

    added missing 'override' in ST/STDAQ

--- ST/STDAQ/src/STErrorDecoding.h
+++ ST/STDAQ/src/STErrorDecoding.h
@@ -1,5 +1,5 @@
 // $Id: STErrorDecoding.h,v 1.3 2008-08-15 08:21:44 mneedham Exp $
-#ifndef STERRORDECODING_H 
+#ifndef STERRORDECODING_H
 #define STERRORDECODING_H 1
 
 //===========================================
@@ -9,25 +9,25 @@
 
 
 /** @class STErrorDecoding STErrorDecoding.h public/STErrorDecoding.h
- *   
+ *
  *
  *  @author Mathias Knecht, M Needham
  *  @date   2007-09-11 (2008-06)
  */
 
 class STErrorDecoding : public STDecodingBaseAlg {
-public: 
+public:
   /// Standard constructor
   STErrorDecoding( const std::string& name, ISvcLocator* pSvcLocator );
 
   virtual ~STErrorDecoding( ); ///< Destructor
 
-  virtual StatusCode execute   ();    ///< Algorithm execution
- 
+  StatusCode execute   () override;    ///< Algorithm execution
+
 private:
 
 
-  
+
   bool m_PrintErrorInfo;
 
 };
