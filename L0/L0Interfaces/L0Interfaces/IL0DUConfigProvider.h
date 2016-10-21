6330bce36a8e5c97e59c1b6fe39d8e1bf8774e3d
Marco Clemencic <marco.clemencic@cern.ch>
2016-10-21 23:57:34 +0200

    added missing 'override' in L0/L0Interfaces

--- L0/L0Interfaces/L0Interfaces/IL0DUConfigProvider.h
+++ L0/L0Interfaces/L0Interfaces/IL0DUConfigProvider.h
@@ -1,5 +1,5 @@
 // $Id: IL0DUConfigProvider.h,v 1.2 2010-01-20 16:06:46 odescham Exp $
-#ifndef IL0DUCONFIGPROVIDER_H 
+#ifndef IL0DUCONFIGPROVIDER_H
 #define IL0DUCONFIGPROVIDER_H 1
 
 // Include files
@@ -10,25 +10,23 @@
 //from Event
 #include "Event/L0DUConfig.h"
 
-static const InterfaceID IID_IL0DUConfigProvider ( "IL0DUConfigProvider", 2, 0 );
+static const InterfaceID IID_IL0DUConfigProvider ( "IL0DUConfigProvider", 3, 0 );
 
 /** @class IL0DUConfigProvider IL0DUConfigProvider.h
- *  
+ *
  *
  *  @author Olivier Deschamps
  *  @date   2007-09-19
  */
 class IL0DUConfigProvider : virtual public IAlgTool {
-public: 
+public:
 
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IL0DUConfigProvider; }
 
-  virtual StatusCode initialize()=0; 
-  virtual StatusCode finalize()=0; 
   virtual LHCb::L0DUConfig* config(long tck,std::string slot="T0")=0;
   virtual LHCb::L0DUConfig::Container* configs(std::string slot="T0")=0;
-  
+
 
 protected:
 
--- L0/L0Interfaces/L0Interfaces/IL0MuonModifyInputTool.h
+++ L0/L0Interfaces/L0Interfaces/IL0MuonModifyInputTool.h
@@ -1,5 +1,5 @@
 // $Id: $
-#ifndef L0INTERFACES_IL0MUONMODIFYINPUTTOOL_H 
+#ifndef L0INTERFACES_IL0MUONMODIFYINPUTTOOL_H
 #define L0INTERFACES_IL0MUONMODIFYINPUTTOOL_H 1
 
 // Include files
@@ -10,24 +10,22 @@
 #include "GaudiKernel/IAlgTool.h"
 #include "Kernel/MuonTileID.h"
 
-static const InterfaceID IID_IL0MuonModifyInputTool ( "IL0MuonModifyInputTool", 1, 0 );
+static const InterfaceID IID_IL0MuonModifyInputTool ( "IL0MuonModifyInputTool", 2, 0 );
 
 /** @class IL0MuonModifyInputTool IL0MuonModifyInputTool.h L0Interfaces/IL0MuonModifyInputTool.h
- *  
+ *
  *
  *  @author Julien Cogan
  *  @date   2013-01-21
  */
 class IL0MuonModifyInputTool : virtual public IAlgTool {
-public: 
+public:
 
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IL0MuonModifyInputTool; }
 
   virtual StatusCode modifyInput(std::vector<LHCb::MuonTileID> &digits)=0;
-  virtual StatusCode initialize()=0;
-  virtual StatusCode finalize()=0;
-  
+
 protected:
 
 private:
