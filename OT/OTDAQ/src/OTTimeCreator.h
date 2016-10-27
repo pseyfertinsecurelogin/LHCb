8ce43f19a5079cfe9d7265be5fb3799b92d02cef
Gerhard Raven <gerhard.raven@nikhef.nl>
2016-10-27 16:42:44 +0200

    Modernize OTDAQ
    * remove SVN/CVS keywords
    * prefer direct member initialization
    * prefer DeclareInterfaceID
    * prefer struct in case all member functions/data are public
    * prefer std::array over C-style array
    * add final
    * prefer STL algorithms over raw loops
    * do not cast if not required
    * prefer implicit destructors over trivial explicit destructors
    * prefer range-based for loops
    * prefer inheritance from extends<> over explicit virtual inheritance
    * add const

--- OT/OTDAQ/src/OTTimeCreator.h
+++ OT/OTDAQ/src/OTTimeCreator.h
@@ -1,4 +1,4 @@
-#ifndef OTDAQ_OTTIMECREATOR_H 
+#ifndef OTDAQ_OTTIMECREATOR_H
 #define OTDAQ_OTTIMECREATOR_H 1
 
 // from Gaudi
@@ -13,28 +13,27 @@ class IOTRawBankDecoder;
 class DeOTDetector;
 
 /** @class OTTimeCreator OTTimeCreator.h
- *  
+ *
  *
  *  @author Jacopo Nardulli
  *  @date   2004-06-06
  */
 
 class OTTimeCreator : public GaudiAlgorithm {
-public: 
+public:
 
   /// Standard constructor
   OTTimeCreator( const std::string& name, ISvcLocator* pSvcLocator );
 
-  ~OTTimeCreator( ) override; ///< Destructor
   StatusCode initialize() override;    ///< Algorithm initialization
   StatusCode finalize() override;
   StatusCode execute() override;    ///< Algorithm execution
- 
+
 private:
-  
+
   // job options
   std::string m_timeLocation;
-  DeOTDetector* m_tracker = nullptr;                  ///< Pointer to XML geometry 
+  const DeOTDetector* m_tracker = nullptr;                  ///< Pointer to XML geometry
   ToolHandle<IOTRawBankDecoder> m_decoder = { "OTRawBankDecoder" };
 };
 
