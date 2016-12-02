654045d44bf6a919e20b3027e18b1ded251b06a4
Marco Clemencic <marco.clemencic@cern.ch>
2016-12-02 14:07:26 +0100

    reorganized LoadDDDB test options
    improved code reuse

--- Det/DetDescChecks/options/LoadDDDB.py
+++ Det/DetDescChecks/options/LoadDDDB.py
@@ -1,33 +1,3 @@
-# Options to load CondDB with default tag as defined in LHCbApp() configurable
-# To use another tag, provide options like e.g. $DDDBROOT/options/LHCb-2008.py
-from Gaudi.Configuration import *
-
-from Configurables import LoadDDDB, DDDBConf, CondDB
-
-DDDBConf() # Configure the detector description
-#DDDBConf().DataType = "DC06"
-
-# We do not care if the database is not up-to-date
-CondDB(IgnoreHeartBeat = True)
-
-# ---------- option to use Oracle CondDB instead of SQLDDDB
-#from Configurables import CondDB
-#CondDB(UseOracle = True)
-
-ApplicationMgr().EvtSel = "NONE"
-ApplicationMgr().EvtMax = 1
-
-ApplicationMgr().TopAlg  = [ GaudiSequencer(Members = [ LoadDDDB() ],
-                                            MeasureTime = True) ]
-
-# ---------- option to select only a subtree
-#LoadDDDB().Node = "/dd/Geometry*"
-
-# Ensure that the first event time is the one used at initialize
-# (based on the datatype).
-def bindFakeEventTime():
-    from Configurables import EventClockSvc, FakeEventTime
-    ecs = EventClockSvc()
-    ecs.addTool(FakeEventTime, "FakeEventTime")
-    ecs.FakeEventTime.StartTime = ecs.getProp("InitialTime")
-appendPostConfigAction(bindFakeEventTime)
+# Options to load CondDB with a default tag
+from DetDescChecks.Options import LoadDDDBTest
+LoadDDDBTest("2016")
