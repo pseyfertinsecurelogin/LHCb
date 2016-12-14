9426c7f29f4082b162552be0b34c7355fa09cdd9
Gerhard Raven <gerhard.raven@nikhef.nl>
2016-12-14 21:04:39 +0100

    Make DetDesc threadsafe
    * either make static variables atomic
    * or change their lifetime to the local scope

--- Det/DetDesc/DetDesc/IntersectionErrors.h
+++ Det/DetDesc/DetDesc/IntersectionErrors.h
@@ -1,99 +1,102 @@
-#ifndef DETDESC_INTERSECTIONERRORS_H 
+#ifndef DETDESC_INTERSECTIONERRORS_H
 #define DETDESC_INTERSECTIONERRORS_H 1
 // ============================================================================
 // Include files
 // ============================================================================
-// STD & STL 
+// STD & STL
 // ============================================================================
 #include <map>
+#include <atomic>
 // ============================================================================
 // GaudiKernel
 // ============================================================================
 #include "GaudiKernel/StatusCode.h"
 // ============================================================================
-// DetDesc 
+// DetDesc
 // ============================================================================
 #include "DetDesc/ILVolume.h"
 // ============================================================================
 namespace DetDesc
 {
-  // forward declarations 
+  // forward declarations
   struct IGeometryErrorSvc ;
   // ==========================================================================
   /** @class IntersectionErrors DetDesc/IntersectionErrors.h
-   *  
+   *
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2007-12-12
    */
   class IntersectionErrors final
   {
   public:
-    /// get the total number of uncatched errors 
+    /// get the total number of uncatched errors
     static unsigned long errors () ;
   public:
-    /// set the static error code 
-    static void              setCode 
+    /// set the static error code
+    static void              setCode
     ( const StatusCode& sc , const ILVolume*   volume ) ;
-    /// get the static error code 
-    static const StatusCode& code    ()  ;
+    /// get the static error code
+    // [[deprecated("can not be guaranteed to give the expected results in a multithreaded environment")]]
+    static StatusCode code    ()  ;
   public:
-    /// set the static pointer to Geoemtry Error Service 
+    /// set the static pointer to Geoemtry Error Service
     static void setService ( DetDesc::IGeometryErrorSvc* svc) ;
-    /// get the static pointer to Geoemtry Error Service 
+    /// get the static pointer to Geoemtry Error Service
     static DetDesc::IGeometryErrorSvc* service() ;
   public:
     /// set the static flag for recovery
     static void setRecovery ( const bool value ) ;
-    /// get teh static flag for recovery
+    /// get the static flag for recovery
+    // [[deprecated("can not be guaranteed to give the expected results in a multithreaded environment")]]
     static bool    recovery () ;
   public:
     // ========================================================================
-    /** report the recovered action in intersections 
-     *  @param  volume    the problematic volume 
-     *  @param  material1 the affected material 
-     *  @param  material2 the affected material 
+    /** report the recovered action in intersections
+     *  @param  volume    the problematic volume
+     *  @param  material1 the affected material
+     *  @param  material2 the affected material
      *  @param  delta    the problematic delta  (non-negative!)
      *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
      *  @date 2007-12-14
      */
     static void recovered
-    ( const ILVolume* volume    , 
+    ( const ILVolume* volume    ,
       const Material* material1 ,
       const Material* material2 ,
       const double    delta     ) ;
     // ========================================================================
-    /** report the skipped intersection 
-     *  @param  volume   the problematic volume 
-     *  @param  material the affected material 
+    /** report the skipped intersection
+     *  @param  volume   the problematic volume
+     *  @param  material the affected material
      *  @param  delta    the problematic delta  (non-negative!)
      *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
      *  @date 2007-12-14
      */
     static void skip
-    ( const ILVolume* volume   , 
+    ( const ILVolume* volume   ,
       const Material* material ,
       const double    delta    ) ;
     // ========================================================================
   public:
     /// inspect the potential error in intersections
     static void  inspect
-    ( const ILVolume*                volume , 
-      const Gaudi::XYZPoint&         pnt    , 
-      const Gaudi::XYZVector&        vect   , 
+    ( const ILVolume*                volume ,
+      const Gaudi::XYZPoint&         pnt    ,
+      const Gaudi::XYZVector&        vect   ,
       const ILVolume::Intersections& cnt    ) ;
-  private:  
-    // static code 
-    static StatusCode                  s_code     ; /// the static error code 
-    // static pointer to IGeometryErrorSvc 
-    static DetDesc::IGeometryErrorSvc* s_service  ; /// pointer to IGeometryErrorSvc 
-    // static counter of unreported errors 
-    static unsigned long               s_errors   ;
-    // static flag to perform the error ecovery 
-    static bool                        s_recovery ;
+  private:
+    // static code
+    static std::atomic<unsigned long> s_code     ; /// the static error code
+    // static pointer to IGeometryErrorSvc
+    static std::atomic<DetDesc::IGeometryErrorSvc*> s_service  ; /// pointer to IGeometryErrorSvc
+    // static counter of unreported errors
+    static std::atomic<unsigned long>  s_errors   ;
+    // static flag to perform the error ecovery
+    static std::atomic<bool>           s_recovery ;
   } ;
 } // end of namespace DetDesc
-// ============================================================================ 
-// The END 
-// ============================================================================ 
+// ============================================================================
+// The END
+// ============================================================================
 #endif // DETDESC_INTERSECTIONERRORS_H
-// ============================================================================ 
+// ============================================================================
