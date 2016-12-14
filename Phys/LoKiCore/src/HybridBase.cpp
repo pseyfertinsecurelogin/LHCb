b0d4cda2774e9f08a114273134bea8f0f8aa2778
Rosen Matev <rosen.matev@cern.ch>
2016-12-14 16:19:25 +0100

    Switch warnings for disabled python/cache functors to always

--- Phys/LoKiCore/src/HybridBase.cpp
+++ Phys/LoKiCore/src/HybridBase.cpp
@@ -147,8 +147,8 @@ StatusCode LoKi::Hybrid::Base::initialize ()
   // force the loading/initialization of  LoKi Service
   svc<LoKi::ILoKiSvc>( "LoKiSvc" , true ) ;
   // Messages
-  if ( !m_use_python ) Warning( "Python Functors are DISABLED",    sc ).ignore();
-  if ( !m_use_cache  ) Warning( "C++ Cache Functors are DISABLED", sc ).ignore();
+  if ( !m_use_python ) always() << "Python Functors are DISABLED" << endmsg ;
+  if ( !m_use_cache  ) always() << "C++ Cache Functors are DISABLED" << endmsg ;
   // return
   return ( m_use_python || m_use_cache ? sc : Error( "No Functors enabled" ) );
 }
