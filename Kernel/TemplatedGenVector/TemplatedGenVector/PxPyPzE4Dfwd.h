2621e20c88545246a3e268332b6727649e92ad46
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2017-03-10 16:33:41 +0000

    Add new packages

---
+++ Kernel/TemplatedGenVector/TemplatedGenVector/PxPyPzE4Dfwd.h
@@ -0,0 +1,20 @@
+// @(#)root/mathcore:$Id$
+// Authors: W. Brown, M. Fischler, L. Moneta    2005
+
+#ifndef LHCbROOT_Math_GenVector_PxPyPzE4Dfwd
+#define LHCbROOT_Math_GenVector_PxPyPzE4Dfwd  1
+
+
+namespace LHCbROOT {
+
+  namespace Math {
+
+template <class Scalar>
+class PxPyPzE4D;
+
+  } // end namespace Math
+
+} // end namespace LHCbROOT
+
+
+#endif /* LHCbROOT_Math_GenVector_PxPyPzE4Dfwd  */
