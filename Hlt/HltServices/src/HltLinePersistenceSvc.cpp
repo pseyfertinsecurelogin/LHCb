e7e9c6ca88da961ad4e8225701fb0632d041224b
Rosen Matev <rosen.matev@cern.ch>
2017-04-20 14:39:40 +0200

    Implement TCK service and factor out common code with Hlt one

--- Hlt/HltServices/src/HltLinePersistenceSvc.cpp
+++ Hlt/HltServices/src/HltLinePersistenceSvc.cpp
@@ -3,9 +3,11 @@
 #include "GaudiKernel/Service.h"
 #include "GaudiKernel/Property.h"
 #include "Kernel/ILinePersistenceSvc.h"
+#include "LinePersistenceSvcCommon.h"
 
 
-class HltLinePersistenceSvc : public extends1<Service, ILinePersistenceSvc> {
+class HltLinePersistenceSvc : public extends<Service, ILinePersistenceSvc>,
+                              private LinePersistenceSvcCommon {
 public:
   HltLinePersistenceSvc(const std::string& name, ISvcLocator* svcLocator);
 
@@ -13,18 +15,12 @@ public:
   RawBanks rawBanksToPersist(const LHCb::HltDecReports& hdr) const override;
 
 private:
-  using NameListPerLine = std::map<std::string, std::vector<std::string> >;
-  using RawBanksPerLine = std::map<std::string, RawBanks>;
-
   Gaudi::Property<NameListPerLine> m_locations{this, "Locations", {},
     "Map of line decisison name to list of output locations."};
   Gaudi::PropertyWithReadHandler<NameListPerLine> m_rawBankTypes{
     this, "RawBankTypes", {},
     "Map of line decisison name to list of rawbank type names."};
   RawBanksPerLine m_rawBanks;
-
-  RawBanksPerLine typeNamesToBitset(const NameListPerLine& map) const;
-
 };
 
 DECLARE_COMPONENT(HltLinePersistenceSvc)
@@ -41,58 +37,11 @@ HltLinePersistenceSvc::HltLinePersistenceSvc(const std::string& name, ISvcLocato
 
 ILinePersistenceSvc::Locations
 HltLinePersistenceSvc::locationsToPersist(const LHCb::HltDecReports& hdr) const {
-  Locations locations;
-  for (const auto& pair : hdr) {
-    if (pair.second.decision()) {
-      auto lineLocations = m_locations.find(pair.first);
-      if (lineLocations == std::end(m_locations)) {
-        throw GaudiException("Decision name not in Locations map.",
-                             this->name(), StatusCode::FAILURE);
-      }
-      locations.push_back(lineLocations->second);
-    }
-  }
-  return locations;
+  return locationsToPersistImpl(hdr, m_locations);
 }
 
 
 ILinePersistenceSvc::RawBanks
 HltLinePersistenceSvc::rawBanksToPersist(const LHCb::HltDecReports& hdr) const {
-  RawBanks rawBanks;
-  for (const auto& pair : hdr) {
-    if (pair.second.decision()) {
-      auto lineRawBanks = m_rawBanks.find(pair.first);
-      if (lineRawBanks == std::end(m_rawBanks)) {
-        throw GaudiException("Decision name not in RawBankTypes map.",
-                             this->name(), StatusCode::FAILURE);
-      }
-      rawBanks |= lineRawBanks->second;
-    }
-  }
-  return rawBanks;
-}
-
-
-HltLinePersistenceSvc::RawBanksPerLine
-HltLinePersistenceSvc::typeNamesToBitset(const NameListPerLine& map) const {
-  RawBanksPerLine result;
-  for (const auto& pair : map) {
-    RawBanks rawBanks{};
-    for (const auto& typeName : pair.second) {
-      int bankid = -1;
-      for (auto i = 0; i < LHCb::RawBank::LastType; ++i) {
-        if (typeName == LHCb::RawBank::typeName(static_cast<LHCb::RawBank::BankType>(i))) {
-          bankid = i;
-          break;
-        }
-      }
-      if (bankid < 0) {
-        throw GaudiException("Rawbank type name does not exist.",
-                             this->name(), StatusCode::FAILURE);
-      }
-      rawBanks.set(bankid);
-    }
-    result[pair.first] = rawBanks;
-  }
-  return result;
+  return rawBanksToPersistImpl(hdr, m_rawBanks);
 }
