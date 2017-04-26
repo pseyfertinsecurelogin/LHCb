8be5f5e7cd61c0859ed370af8ac79a5386358cd0
Marco Clemencic <marco.clemencic@cern.ch>
2017-04-26 12:23:15 +0200

    Fixed size of array returned by LHCb::ODINCodec::encode

--- Kernel/LHCbAlgs/src/ODINCodec.h
+++ Kernel/LHCbAlgs/src/ODINCodec.h
@@ -3,14 +3,16 @@
 
 #include "Event/ODIN.h"
 #include "Event/RawBank.h"
+#include <array>
 
 namespace LHCb { namespace ODINCodec {
   namespace Bank {
     constexpr const unsigned int VERSION = 6;
-    const size_t SIZE = 10 * sizeof(int);
+    using array = std::array<unsigned int, 10>;
+    const size_t SIZE = sizeof(array);
   }
   LHCb::ODIN decode( const LHCb::RawBank& bank, const bool ignoreBankVersion = false );
-  std::array<unsigned int, Bank::SIZE> encode( const LHCb::ODIN& odin );
+  Bank::array encode( const LHCb::ODIN& odin );
 } } // LHCb::ODINCodec
 
 #endif // ODIN_CODEC_H
