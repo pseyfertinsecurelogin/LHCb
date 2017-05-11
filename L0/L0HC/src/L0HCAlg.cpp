34347146cb580492274ec469e3ac1d0936ad288e
djohnson <djohnson@192.168.0.130>
2017-05-11 21:25:54 +0200

    Improvements by Heinrich

--- L0/L0HC/src/L0HCAlg.cpp
+++ L0/L0HC/src/L0HCAlg.cpp
@@ -1,47 +1,38 @@
-/// local
-#include "L0HCAlg.h"
-
-/// STL
-#include <fstream>
-#include <numeric>
-
-// From Event
+#include "Event/HCDigit.h"
 #include "Event/L0ProcessorData.h"
 
-//
-//  Level-0 Herschel trigger emulation algorithm
-//
+// Local
+#include "L0HCAlg.h"
 
-DECLARE_ALGORITHM_FACTORY( L0HCAlg )
-
+DECLARE_ALGORITHM_FACTORY(L0HCAlg)
 
 //=============================================================================
 // Constructor
 //=============================================================================
-L0HCAlg::L0HCAlg( const std::string & name , ISvcLocator * pSvcLocator)
-  : L0AlgBase( name , pSvcLocator )
-{
+L0HCAlg::L0HCAlg(const std::string& name, ISvcLocator* pSvcLocator)
+    : L0AlgBase(name, pSvcLocator) {
+
   declareProperty("DigitLocation",
-		  	  	  m_digitLocation = LHCb::HCDigitLocation::Default);
-  m_HCMult_B = std::vector< int >( 12 , 0 ) ;
-  m_HCMult_F = std::vector< int >(  8 , 0 ) ;
+                  m_digitLocation = LHCb::HCDigitLocation::Default);
+  declareProperty("ChannelsB0", m_channelsB0);
+  declareProperty("ChannelsB1", m_channelsB1);
+  declareProperty("ChannelsB2", m_channelsB2);
+  declareProperty("ChannelsF1", m_channelsF1);
+  declareProperty("ChannelsF2", m_channelsF2);
 }
 
 //=============================================================================
 // Destructor
 //=============================================================================
-L0HCAlg::~L0HCAlg() { }
+L0HCAlg::~L0HCAlg() {}
 
 //=============================================================================
-// Initialization: Retrieve the Herschel digits from the TES where
-//				   they should have been placed by HCRawBankDecoder
+// Initialization
 //=============================================================================
 StatusCode L0HCAlg::initialize() {
-  StatusCode sc = L0AlgBase::initialize(); // must be executed first
-  if ( sc.isFailure() ) return sc; 
+  StatusCode sc = L0AlgBase::initialize();
+  if (sc.isFailure()) return sc;
 
-  if( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
-  
   // Check if the mapping is available in the conditions database.
   const std::string location = "Conditions/ReadoutConf/HC/Mapping";
   if (existDet<Condition>(location)) {
@@ -59,135 +50,77 @@ StatusCode L0HCAlg::initialize() {
     mapChannels(m_channelsF1, 1, false);
     mapChannels(m_channelsF2, 2, false);
   }
-  
-  // Temporary warning
-  warning() << "No default HCDigits location has yet been defined -- beware" << endmsg;
-  
+
   return StatusCode::SUCCESS;
 }
 
 //=============================================================================
-// Execute: Compute the Herschel trigger information
+// Execute
 //=============================================================================
 StatusCode L0HCAlg::execute() {
-
   // Retrieve Herschel digits
-  digits = getIfExists<LHCb::HCDigits>(m_digitLocation);
-  if ( !digits ) {
-    // should always be available ...
-	return Error( "Cannot load the HCDigits data object", StatusCode::SUCCESS );
+  LHCb::HCDigits* digits = getIfExists<LHCb::HCDigits>(m_digitLocation);
+  if (!digits) {
+    return Error("Cannot retrieve HC digits from " + m_digitLocation,
+                 StatusCode::SUCCESS);
   }
 
-  // Get the Herschel information. Adds it to the m_ecalFe[] objects
-  addHCData( ) ;
-  
-  // Save the candidates in HCProcessor data location (for L0DU) 
-  LHCb::L0ProcessorDatas* L0HC = new LHCb::L0ProcessorDatas() ;
-  std::string procName =  LHCb::L0ProcessorDataLocation::HC;
-  put( L0HC, procName ) ;
+  unsigned int multB = 0;
+  // Construct B-side sum
+  for (unsigned int i = 0; i < 3; ++i) {
+    // Loop over all quadrants on this station
+    for (unsigned int j = 0; j < 4; ++j) {
+      // Find the cell ID for this quadrant
+      LHCb::HCCellID id(m_channels[i][j]);
+      // Retrieve the digit
+      const LHCb::HCDigit* digit = digits->object(id);
+      if (!digit) {
+        const std::string ch = "B" + std::to_string(i) + std::to_string(j);
+        Warning("Cannot retrieve digit for " + ch).ignore();
+        continue;
+      }
+      if (digit->adc() > 300) ++multB;
+    }
+  }
 
-  // Herschel B-side multiplicity counter
-  int totHCMult_B = std::accumulate( m_HCMult_B.begin() , m_HCMult_B.end() , 0 ) ;
-  unsigned int code = 0x10000 + ( totHCMult_B << L0DUBase::Calo::Sum::Shift ) ; // what bit-shift should be employed here?
-  LHCb::L0ProcessorData* HCMult_B = 
-    new LHCb::L0ProcessorData ( L0DUBase::Fiber::CaloPi0Global, code ) ;
-  L0HC -> add( HCMult_B ) ;
+  unsigned int multF = 0;
+  // Construct F-side sum
+  for (unsigned int i = 3; i < 5; ++i) {
+    // Loop over all quadrants on this station
+    for (unsigned int j = 0; j < 4; ++j) {
+      // Find the cell ID for this quadrant
+      LHCb::HCCellID id(m_channels[i][j]);
+      // Retrieve the digit
+      const LHCb::HCDigit* digit = digits->object(id);
+      if (!digit) {
+        const std::string ch = "F" + std::to_string(i - 2) + std::to_string(j);
+        Warning("Cannot retrieve digit for " + ch).ignore();
+        continue;
+      }
+      if (digit->adc() > 300) ++multF;
+    }
+  }
 
-  // Herschel F-side multiplicity counter
-  int totHCMult_F = std::accumulate( m_HCMult_F.begin() , m_HCMult_F.end() , 0 ) ;
-  code = 0x10000 + ( totHCMult_F << L0DUBase::Calo::Sum::Shift ) ;
-  LHCb::L0ProcessorData* HCMult_F = 
-    new LHCb::L0ProcessorData ( L0DUBase::Fiber::CaloPi0Local, code ) ;
-  L0HC -> add( HCMult_F ) ;
-  
+  // Save the candidates in HCProcessor data location (for L0DU)
+  LHCb::L0ProcessorDatas* L0HC = new LHCb::L0ProcessorDatas();
+  put(L0HC, LHCb::L0ProcessorDataLocation::HC);
+
+  // What bit-shift should be employed here?
+  const unsigned int codeB = 0x10000 + (multB << L0DUBase::Calo::Sum::Shift);
+  L0HC->add(new LHCb::L0ProcessorData(L0DUBase::Fiber::CaloPi0Global, codeB));
+
+  const unsigned int codeF = 0x10000 + (multF << L0DUBase::Calo::Sum::Shift);
+  L0HC->add(new LHCb::L0ProcessorData(L0DUBase::Fiber::CaloPi0Local, codeF));
+
   // Debug now the L0 candidates
-  if ( msgLevel( MSG::DEBUG ) ) {
-    LHCb::L0ProcessorDatas::const_iterator item;
-    debug() << "== L0ProcessorData Summary: " << L0HC->size() 
-            << " entries." << endmsg ;
-    for( item = L0HC->begin() ; L0HC->end() != item ; ++item ) {
-      LHCb::L0ProcessorData* cand = (*item);
-      debug() << format( "Key %2d Word %8x", cand->key(), cand->word() ) 
-              << endmsg ;
+  if (msgLevel(MSG::DEBUG)) {
+    for (LHCb::L0ProcessorData* cand : *L0HC) {
+      debug() << format("Key %2d Word %8x", cand->key(), cand->word())
+              << endmsg;
     }
   }
 
   return StatusCode::SUCCESS;
-}
-
-//=============================================================================
-// Finalize.
-//=============================================================================
-StatusCode L0HCAlg::finalize() {
-  if ( 0 != m_nbEvents ) {
-    info() << format( "Average bank size : %7.1f words RAW.", 
-                      m_totRawSize/m_nbEvents )
-           << endmsg ;
-  }
-  return L0AlgBase::finalize(); 
-}
-
-//=============================================================================
-// Add the Herschel information 
-//=============================================================================
-void L0HCAlg::addHCData( ) {
-
-  // Assign zeros to m_HCMult_B and m_HCMult_F
-  for ( unsigned int i = 0 ; i < m_HCMult_B.size() ; ++i )  
-    m_HCMult_B[ i ] = 0 ;
-  for ( unsigned int i = 0 ; i < m_HCMult_F.size() ; ++i )  
-    m_HCMult_F[ i ] = 0 ;
-  
-  // Construct F-side sum
-  for (unsigned int i = 0; i < 3; ++i) {
-  	// Loop over all quadrants on this station
-    for (unsigned int j = 0; j < 4; ++j) {
-      // Find CellID for this quadrant
-      LHCb::HCCellID id(m_channels[i][j]);
-      
-      // Retrieve the digit
-      const LHCb::HCDigit* digit = digits->object(id);
-      const std::string ch = "Counter B" + std::to_string(i) + std::to_string(j);
-       
-      if (!digit) {
-        warning() << "Cannot retrieve digit for " << ch << endmsg;
-        continue;
-      }
-      
-      // Extract data ADC
-      int adc = digit->adc();
-
-      // Compute trigger bit
-      int trig_bit = (adc>300); // TODO: Fake threshold at 300
-      m_HCMult_B[ i*4 + j ] = trig_bit;
-    }
-  }
-      
-  // Construct F-side sum
-  for (unsigned int i = 3; i < 5; ++i) {
-  	// Loop over all quadrants on this station
-    for (unsigned int j = 0; j < 4; ++j) {
-      // Find CellID for this quadrant
-      LHCb::HCCellID id(m_channels[i][j]);
-      
-      // Retrieve the digit
-      const LHCb::HCDigit* digit = digits->object(id);
-      const std::string ch = "Counter F" + std::to_string(i-2) + std::to_string(j);
-      
-      if (!digit) {
-        warning() << "Cannot retrieve digit for " << ch << endmsg;
-        continue;
-      }
-      
-      // Extract data ADC
-      int adc = digit->adc();
-
-      // Compute trigger bit
-      int trig_bit = (adc>300); // TODO: Fake threshold at 300
-
-      m_HCMult_F[ (i-3)*4 + j ] = trig_bit;
-    }
-  }
 }
 
 //=============================================================================
@@ -214,11 +147,10 @@ StatusCode L0HCAlg::cacheMapping() {
 // Setup the channel map for a given station.
 //=============================================================================
 bool L0HCAlg::mapChannels(const std::vector<int>& channels,
-                                    const unsigned int station,
-                                    const bool bwd) {
+                          const unsigned int station, const bool bwd) {
   // Indices 0,1,2 are B-side; 3,4 are F-side
   const unsigned int offset = bwd ? 0 : 2;
-  
+
   // Check if the input is valid and if not return 0 mapping
   if (channels.size()) {
     std::string s = bwd ? "B" : "F";
@@ -227,10 +159,10 @@ bool L0HCAlg::mapChannels(const std::vector<int>& channels,
     m_channels[station + offset].assign(4, 0);
     return false;
   }
-  
+
   // Determine whether dealing with F-side or B-side crate
   const unsigned int crate = bwd ? m_crateB : m_crateF;
-  
+
   // Resize mapping vector for 4 quadrants
   m_channels[station + offset].resize(4);
 
@@ -239,8 +171,8 @@ bool L0HCAlg::mapChannels(const std::vector<int>& channels,
     if (channels[i] < 0) {
       std::string s = bwd ? "B" : "F";
       s += std::to_string(station);
-      warning() << "Invalid channel number " << channels[i]
-                << " for quadrant " << s << i << endmsg;
+      warning() << "Invalid channel number " << channels[i] << " for quadrant "
+                << s << i << endmsg;
       continue;
     }
     m_channels[station + offset][i] = (crate << 6) | channels[i];
--- L0/L0HC/src/L0HCAlg.h
+++ L0/L0HC/src/L0HCAlg.h
@@ -1,14 +1,8 @@
-#ifndef   L0HC_L0HCALG_H
-#define   L0HC_L0HCALG_H  1
+#ifndef L0HC_L0HCALG_H
+#define L0HC_L0HCALG_H 1
 
-// from Gaudi
 #include "L0Base/L0AlgBase.h"
-
-// Local classes
-// from LHCb
-//#include "Kernel/IEventTupleTool.h"
 #include "DetDesc/Condition.h"
-#include "Event/HCDigit.h"
 
 /** @class L0HCAlg L0HCAlg.h
  *
@@ -19,48 +13,39 @@
  */
 
 class L0HCAlg : public L0AlgBase {
-public:
-  /// standard algorithm Constructor
-  L0HCAlg( const std::string& name , ISvcLocator* pSvcLocator ) ;
+ public:
+  L0HCAlg(const std::string& name, ISvcLocator* pSvcLocator);
+  virtual ~L0HCAlg();
 
-  virtual ~L0HCAlg() ; ///< Standard destructor
+  StatusCode initialize() override;  ///< Algorithm initialization
+  StatusCode execute() override;     ///< Algorithm execution
 
-  StatusCode initialize() override; ///< Algorithm initialization
-  StatusCode execute   () override; ///< Algorithm execution
-  StatusCode finalize  () override; ///< Algorithm finalization
+ private:
+  /// Pointer to conditions DB
+  Condition* m_cond = nullptr;
+  /// Herschel digits location
+  std::string m_digitLocation;
 
-protected:
-  void addHCData(  );   ///< process the Herschel information
+  /// Number of B-side crate
+  unsigned int m_crateB;
+  /// Number of F-side crate
+  unsigned int m_crateF;
 
-private:
-  // Local variables
-  Condition *    m_cond = nullptr         	; ///< Pointer to conditions DB
-  std::string    m_digitLocation            ; ///< Herschel digits location
-  
-  unsigned int m_crateB					  	; /// Number of B-side crate
-  unsigned int m_crateF					  	; /// Number of F-side crate
-  
-  std::vector<int> m_channelsB0			  	; /// Channel mappings
-  std::vector<int> m_channelsB1			  	;
-  std::vector<int> m_channelsB2			  	;
-  std::vector<int> m_channelsF1			  	;
-  std::vector<int> m_channelsF2			  	;
+  /// Channel mappings
+  std::vector<int> m_channelsB0;
+  std::vector<int> m_channelsB1;
+  std::vector<int> m_channelsB2;
+  std::vector<int> m_channelsF1;
+  std::vector<int> m_channelsF2;
+
   /// Channel numbers for each quadrant
   std::vector<std::vector<unsigned int> > m_channels;
 
-  LHCb::HCDigits* digits  			  	    ; /// Herschel digits
-
-  double m_totRawSize = 0.0 ;
-  int    m_nbEvents   = 0   ;
-  
-  std::vector< int > m_HCMult_B, m_HCMult_F ; ///< Multiplicity of Herschel B- or F-side
-
-  /// Cache database content  
+  /// Cache database content
   StatusCode cacheMapping();
   /// Setup the mapping for a given station.
-  bool mapChannels(const std::vector<int>& channels,
-                   const unsigned int station, const bool bwd);
-
+  bool mapChannels(const std::vector<int>& channels, const unsigned int station,
+                   const bool bwd);
 };
 
-#endif //   L0HC_L0HCALG_H
+#endif  // L0HC_L0HCALG_H
