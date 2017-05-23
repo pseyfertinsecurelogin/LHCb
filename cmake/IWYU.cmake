cf9e16121d44bbdd91ccb6bc17d3f27f13622a68
Paul Seyfert <pseyfert@cern.ch>
2017-05-23 11:46:10 +0200

    run iwyu from cmake

--- cmake/IWYU.cmake
+++ cmake/IWYU.cmake
@@ -2,18 +2,55 @@ find_program(iwyu_tool NAMES iwyu_tool.py)
 set(IWYU_MAPPING_FILE ${CMAKE_SOURCE_DIR}/cmake/include_rules.imp)
 set(IWYU_WORK_DIR ${CMAKE_BINARY_DIR}/iwyu)
 file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/iwyu)
-add_custom_target(run-iwyu
-  COMMAND ${iwyu_tool} -p ${IWYU_WORK_DIR}
-  -- mapping_file=${IWYU_MAPPING_FILE} > ${IWYU_WORK_DIR}
-  WORKING_DIR=${IWYU_WORK_DIR}
-  DEPENDS ${GODHeaders}
-  COMMENT "run iwyu"
-  )
+if(iwyu_tool)
+  message(FATAL "FOUND IWYU w/o PATH SETUP???")
+  add_custom_target(run-iwyu
+    COMMAND ${iwyu_tool} -p ${IWYU_WORK_DIR}
+    -- --mapping_file=${IWYU_MAPPING_FILE} > ${IWYU_WORK_DIR}/iwyu.log
+    WORKING_DIR=${IWYU_WORK_DIR}
+    DEPENDS ${GODHeaders}
+    COMMENT "run iwyu"
+    )
+else()
+  find_program(iwyu_tool NAMES iwyu_tool.py
+    HINTS "/tmp/pseyfert/tmp.iCzgEqwMfJ/install/bin/")
+  set(gcc_config_version 4.9.3)
+  set(clang_config_version 3.9)
+  set(LCGPLAT x86_64-centos7)
+  set(LCG_lib_name lib64)
+  set(LCG_contdir /afs/cern.ch/sw/lcg/external)
+  set(LCG_gcc_home ${LCG_contdir}/gcc/${gcc_config_version}/${LCGPLAT})
+  set(LCG_clang_home ${LCG_contdir}/llvm/${clang_config_version}/${LCGPLAT})
+  if(DEFINED ENV{PATH})
+    message(STATUS "extending PATH")
+    set(iwyu_path ${LCG_clang_home}/bin:${LCG_gcc_home}/bin:/tmp/pseyfert/tmp.iCzgEqwMfJ/install/bin/:$ENV{PATH})
+  else()
+    message(FATAL "not extending PATH")
+    set(iwyu_path ${LCG_clang_home}/bin:${LCG_gcc_home}/bin:/tmp/pseyfert/tmp.iCzgEqwMfJ/install/bin/)
+  endif()
+  if(DEFINED ENV{LD_LIBRARY_PATH})
+    message(STATUS "extending LD_LIBRARY_PATH")
+    set(iwyu_ld_path ${LCG_gcc_home}/${LCG_lib_name}:${LCG_clang_home}/lib:$ENV{LD_LIBRARY_PATH})
+  else()
+    message(FATAL "not extending LD_LIBRARY_PATH")
+    set(iwyu_ld_path ${LCG_gcc_home}/${LCG_lib_name}:${LCG_clang_home}/lib)
+  endif()
+
+  add_custom_target(run-iwyu
+    ${CMAKE_COMMAND} -E env
+    PATH=${iwyu_path}
+    LD_LIBRARY_PATH=${iwyu_ld_path}
+    ${iwyu_tool} -p ${IWYU_WORK_DIR}
+    -- --mapping_file=${IWYU_MAPPING_FILE} > ${IWYU_WORK_DIR}/iwyu.log
+    WORKING_DIR=${IWYU_WORK_DIR}
+    DEPENDS ${GODHeaders}
+    COMMENT "run iwyu"
+    )
+endif()
 
 file(WRITE ${CMAKE_BINARY_DIR}/strip-json.py "#!/bin/python
 godfiles=[]\n")
 get_property(god_gen_file GLOBAL PROPERTY GOD_GENERATED_FILES)
-message(STATUS "GOD_GENERATED_FILES is ${god_gen_file}")
 foreach(genfile ${god_gen_file})
   file(APPEND ${CMAKE_BINARY_DIR}/strip-json.py
     "godfiles+=['${genfile}']\n")
