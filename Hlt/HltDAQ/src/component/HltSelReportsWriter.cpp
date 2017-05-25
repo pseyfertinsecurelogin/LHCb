7bd3a5c332a7956932bde990339f7248b377f061
Rosen Matev <rosen.matev@cern.ch>
2017-05-25 18:34:07 +0200

    Check LHCbID ordering in HltSelReportsWriter and sort IDs in Decoder

--- Hlt/HltDAQ/src/component/HltSelReportsWriter.cpp
+++ Hlt/HltDAQ/src/component/HltSelReportsWriter.cpp
@@ -194,6 +194,11 @@ StatusCode HltSelReportsWriter::execute() {
     std::transform( std::begin(ids), std::end(ids),
                     std::inserter(thisIDset, std::end(thisIDset)),
                     [](const LHCb::LHCbID& id) { return id.lhcbID(); } );
+    // Make sure the LHCbIDs are sorted or else the binary_search called
+    // later does not work! (when connecting substructures with ids)
+    if (!std::is_sorted(std::begin(thisIDset), std::end(thisIDset))) {
+      Error("LHCbIDs in HltObjectSummary are not sorted!");
+    }
     addToSequences( std::move(thisIDset), lhcbidSequences );
   }
 
