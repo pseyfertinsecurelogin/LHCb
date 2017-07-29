8e65e8fc5e49a72bf79df783afda7e1b4adb144b
Gerhard Raven <gerhard.raven@nikhef.nl>
2017-07-29 11:44:37 +0200

    Modernize RateFromTCK
    - prefer inheritance from extends<...>
    - make control flow more uniform
    - add an explicit bool conversion to MD5 to unify checking for
      'bad' values
    - prefer Gaudi::Property
    - prefer implicit destructors

--- Kernel/LHCbAlgs/src/RateFromTCK.cpp
+++ Kernel/LHCbAlgs/src/RateFromTCK.cpp
@@ -15,23 +15,11 @@
 DECLARE_TOOL_FACTORY( RateFromTCK )
 
 //=============================================================================
-// Standard constructor, initializes variables
-//=============================================================================
-RateFromTCK::RateFromTCK( const std::string& type,
-                          const std::string& name,
-                          const IInterface* parent )
-: GaudiTool ( type, name , parent )
-{
-  declareInterface<IRateFromTCK>(this);
-  declareProperty( "IPropertyConfigSvcInstance", m_propertyConfigSvcName = "PropertyConfigSvc");
-}
-
-//=============================================================================
 // Initialize
 //=============================================================================
 StatusCode RateFromTCK::initialize()
 {
-  StatusCode sc = GaudiTool::initialize();
+  StatusCode sc = base_class::initialize();
   if (!sc) return sc;
   if (UNLIKELY( msgLevel(MSG::VERBOSE))) verbose() << "RateFromTCK::initialize()" << endmsg;
   if (!service( m_propertyConfigSvcName, m_propertyConfigSvc).isSuccess()) {
@@ -47,7 +35,6 @@ StatusCode RateFromTCK::initialize()
     warning() << "Conditions/Online/LHCb/RunInfo/Trigger not found." << endmsg;
   }
   return StatusCode::SUCCESS ;
-
 }
 //=============================================================================
 // get rate - Moved from Jaap Panman's RateFromTCK
@@ -72,46 +59,41 @@ double RateFromTCK::parameterFromTCK( const std::string& instanceName,
       verbose() << "No TCK was found, we will get the rate from the CondDB" << endmsg;
     return rate;
   }
-  else {
-    TCK _tck(tck);
-    _tck.normalize();
-    ConfigTreeNodeAlias::alias_type alias( std::string("TCK/") +  _tck.str()  );
-    // grab properties of child from config database...
-    const ConfigTreeNode* tree = m_propertyConfigSvc->resolveConfigTreeNode(alias);
-    if (!tree) {
-      // if we could not resolve the (non-zero) TCK we have a problem
-      if (UNLIKELY( msgLevel(MSG::VERBOSE)))
-        verbose() << "Obtained TCK " << std::hex
-                  << _tck << " from the database which could not be resolved" << endmsg;
-      return rate;
-    } else {
-      auto child = m_propertyConfigSvc->findInTree(tree->digest(), instanceName);
-      if (child.invalid()) {
-        warning() << "could not find configuration for " << instanceName << endmsg;
-        return rate;
+  TCK _tck(tck);
+  _tck.normalize();
+  ConfigTreeNodeAlias::alias_type alias( std::string("TCK/") +  _tck.str()  );
+  // grab properties of child from config database...
+  const ConfigTreeNode* tree = m_propertyConfigSvc->resolveConfigTreeNode(alias);
+  if ( !tree ) {
+    // if we could not resolve the (non-zero) TCK we have a problem
+    error() << "Obtained TCK " << std::hex
+            << _tck << " from the database which could not be resolved" << endmsg;
+    return rate;
+  }
+  auto child = m_propertyConfigSvc->findInTree(tree->digest(), instanceName);
+  if ( !child ) {
+    warning() << "could not find configuration for " << instanceName << endmsg;
+    return rate;
+  }
+  const PropertyConfig *config = m_propertyConfigSvc->resolvePropertyConfig(child);
+  if ( !config ) {
+    error() << "could not obtain PropertyConfig " << instanceName << endmsg;
+    return rate;
+  }
+  if (UNLIKELY( msgLevel(MSG::VERBOSE)))
+    verbose() << "TCK data found: " << tck << " for instance " << instanceName
+              << " " << config->properties().size() << " properties " << endmsg;
+  // list properties
+  for ( const auto& i : config->properties() ) {
+    if (UNLIKELY( msgLevel(MSG::VERBOSE))) verbose() << i.first << " : " << i.second << endmsg;
+    if ( i.first == code ) {
+      try {
+        const auto pos = i.second.find(sub);
+        const auto str2 = i.second.substr(pos + sub.size());
+        rate = std::strtod(str2.c_str(), 0);
       }
-      const PropertyConfig *config = m_propertyConfigSvc->resolvePropertyConfig(child);
-      if ( !config ) {
-        error() << "could not obtain PropertyConfig " << instanceName << endmsg;
-        return rate;
-      }
-      if (UNLIKELY( msgLevel(MSG::VERBOSE)))
-        verbose() << "TCK data found: " << tck << " for instance " << instanceName
-                  << " " << config->properties().size() << " properties " << endmsg;
-      // list properties
-      for ( const auto & i : config->properties() )
-      {
-        if (UNLIKELY( msgLevel(MSG::VERBOSE))) verbose() << i.first << " : " << i.second << endmsg;
-        if ( i.first == code ) {
-          try {
-            const auto pos = (i.second).find(sub);
-            const auto str2 = (i.second).substr(pos + sub.size());
-            rate = std::strtod(str2.c_str(), 0);
-          }
-          catch (const std::exception&) {
-            warning() << "could not find substring " << sub << " in " << i.first << endmsg;
-          }
-        }
+      catch (const std::exception&) {
+        warning() << "could not find substring " << sub << " in " << i.first << endmsg;
       }
     }
   }
@@ -121,8 +103,8 @@ double RateFromTCK::parameterFromTCK( const std::string& instanceName,
 //  Extract data from Trigger
 //=========================================================================
 StatusCode RateFromTCK::i_cacheTriggerData() {
-  if (UNLIKELY( msgLevel(MSG::DEBUG))) debug() << "callback Trigger:" << endmsg;
-  m_triggerTCK = (unsigned int) m_condTrigger->param<int>("TCK");
+  if (msgLevel(MSG::DEBUG)) debug() << "callback Trigger:" << endmsg;
+  m_triggerTCK = m_condTrigger->param<int>("TCK");
   return StatusCode::SUCCESS;
 }
 
