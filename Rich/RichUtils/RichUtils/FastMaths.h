b6fd6555dc99c6a50a5eb86f78dc1d69c3145312
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2017-11-01 08:49:37 +0000

    Add VDT inspired atans2 method for SIMD types.

--- Rich/RichUtils/RichUtils/FastMaths.h
+++ Rich/RichUtils/RichUtils/FastMaths.h
@@ -11,67 +11,74 @@
 
 #pragma once
 
+// STL
 #include <cmath>
+
+// Local
+#include "RichUtils/RichSIMDTypes.h"
 
 namespace Rich
 {
-  namespace Maths
+  /** @namespace Rich::SIMD
+   *
+   *  Namespace for RICH SIMD types and utilities
+   *
+   *  @author Chris Jones  Christopher.Rob.Jones@cern.ch
+   *  @date   17/10/2017
+   */
+  namespace SIMD
   {
 
-    /** Fast (approx) implementation of atan2(x,y) using
-     *  power series expansions
-     *  @param y
-     *  @param x
-     *  @return Approximate calculation of atan2(y,x)
-     */
-    inline double atan2_f( const double y, const double x )
+    namespace Maths
     {
-      double z(0);
-      if ( fabs(x) < 1e-10 )
-      {
-        z = ( y<0 ? -M_PI_2 : y>0 ? M_PI_2 : 0 );
-      }
-      else
-      {
-        z = y/x;
-        if ( z>=-1 && z<=1 )
-        {
-          z = z*(15.0+4.0*z*z)/(15.0+9.0*z*z);
-          if ( x<=0 ) z = ( y>=0 ? M_PI+z : z-M_PI );
-        }
-        else
-        {
-          z = M_PI_2-(15.0+4.0/(z*z))/(z*(15.0+9.0/(z*z)));
-          if ( y<=0 ) z -= M_PI;
-        }
-      }
-      return z;
-    }
 
+      /** fast atan2 for SIMD types
+       *  Based on VDT fast_atan2f */
+      template < typename TYPE,
+                 typename std::enable_if<!std::is_arithmetic<TYPE>::value>::type * = nullptr >
+      inline TYPE fast_atan2f( const TYPE y, const TYPE x )
+      {
+
+        // move in first octant
+        auto xx = abs(x);
+        auto yy = abs(y);
+        auto tmp ( TYPE::Zero() );
+        auto m = ( yy > xx );
+        if ( any_of(m) )
+        {
+          tmp(m) = yy;
+          yy(m)  = xx; 
+          xx(m)  = tmp;
+          tmp(m) = TYPE::One();
+        }
+
+        auto t = yy / xx;
+        auto z = t;
+        m      = t > TYPE(0.4142135623730950);
+        if ( any_of(m) ) { z(m) = ( t - TYPE::One() ) / ( t + TYPE::One() ); }
+     
+        const auto z2 = z * z;
+
+        auto ret =((((    TYPE(8.05374449538e-2f)  * z2
+                       -  TYPE(1.38776856032E-1f)) * z2
+                       +  TYPE(1.99777106478E-1f)) * z2
+                       -  TYPE(3.33329491539E-1f)) * z2 * z
+                       + z );
+
+        // Here we put the result to 0 if xx was 0, if not nothing happens!
+        m = ( xx == TYPE::Zero() );
+        if ( any_of(m) ) { ret(m) = TYPE::Zero(); }
+
+        // move back in place
+        ret( y == TYPE::Zero() ) = TYPE::Zero();
+        ret( t > TYPE(0.4142135623730950f) ) += TYPE(M_PI_4);
+        ret( tmp != TYPE::Zero() ) = TYPE(M_PI_2) - ret;
+        ret( x    < TYPE::Zero() ) = TYPE(M_PI)   - ret;
+        ret( y    < TYPE::Zero() ) = - ret;
+
+        return ret;
+      }
+      
+    }
   }
 }
-
-/*
-  #define INT_TO_FIXED(x)     ((x) << 16)
-  #define FIXED_ONE       INT_TO_FIXED(1)
-  #define FIXED_PIDIV2            102944L
-
-  int32 fpatan(int32 x)
-  {
-  register int32 x2;
-  register int32 numerator;
-  register int32 denominator;
-  if (x > FIXED_ONE)
-  {
-  return FIXED_PIDIV2 - fpatan(fpdiv(FIXED_ONE, x));
-  }
-  if (x < -FIXED_ONE)
-  {
-  return -FIXED_PIDIV2 - fpatan(fpdiv(FIXED_ONE, x));
-  }
-  x2 = fpmul(x, x);
-  numerator = fpmul(x, INT_TO_FIXED(15) + fpmul(INT_TO_FIXED(4), x2));
-  denominator = INT_TO_FIXED(15) + fpmul(INT_TO_FIXED(9), x2);
-  return fpdiv(numerator, denominator);
-  }
-*/
