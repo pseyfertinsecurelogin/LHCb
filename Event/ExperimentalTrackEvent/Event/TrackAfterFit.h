559057b00b4e888fc4cc3bc08cf3860c1b146f9f
Paul Seyfert <pseyfert.mathphys@gmail.com>
2018-06-27 17:11:17 +0200

    additional states constructor

--- Event/ExperimentalTrackEvent/Event/TrackAfterFit.h
+++ Event/ExperimentalTrackEvent/Event/TrackAfterFit.h
@@ -1,47 +1,59 @@
 #pragma once
 #include "Event/State.h"
 #include "Kernel/LHCbID.h"
-#include <boost/container/small_vector.hpp>
 #include "Kernel/STLExtensions.h"
+#include <boost/container/small_vector.hpp>
+#include <vector>
 
 namespace experimental
 {
+  class TrackAfterFit;
+  using TrackAfterFits = std::vector<TrackAfterFit>;
+
   class TrackAfterFit
   {
   public:
     // FIXME: very initial
-    // FIXME: separate c++ types for different track types?
-    enum class Type {
-      Velo,
-      Long,
-      Downstream,
-      Upstream,
-      TTrack,
-    };
 
-  private:
-    LHCb::State m_physicsstate;
-    Type        m_type;
+    TrackAfterFit();
+
+    friend LHCb::Converters::TrackAfterFit::experimental;
+
+    struct fromVectorLHCbTrack private : LHCb::State m_physicsstate;
+    LHCb::Track::Type                                m_type;
+    float                                            m_chisqperdof;
+    int                                              m_dof;
   };
   namespace TrackAfterFitExtension
   {
+    class Hits;
+    class AdditionalStates;
+
+    using AdditionalStatess = std::vector<AdditionalStates>;
+    using Hitss             = std::vector<Hits>;
+
     class Hits
     {
     private:
       // FIXME: separate containers for different containers?
       // FIXME: proper size
       boost::container::small_vector<LHCb::LHCbID, 30> m_hits;
+
     public:
-      Hits(const std::vector<LHCb::LHCbID>&);
-      Hits(const LHCb::span<const LHCb::LHCbID>);
+      Hits( const std::vector<LHCb::LHCbID>& );
+      Hits( const LHCb::span<const LHCb::LHCbID> );
     };
 
     class AdditionalStates
     {
+    private:
       LHCb::State              m_BegRich1;
       LHCb::State              m_EndRich1;
       LHCb::State              m_BegRich2;
       std::vector<LHCb::State> m_moreStates;
+
+    public:
+      AdditionalStates( LHCb::States::const_iterator it1, LHCb::States::const_iterator it2 );
     };
   } // namespace TrackAfterFitExtension
 } // namespace experimental
--- Event/ExperimentalTrackEvent/src/TrackAfterFit.cpp
+++ Event/ExperimentalTrackEvent/src/TrackAfterFit.cpp
@@ -1,8 +1,31 @@
 #include "Event/TrackAfterFit.h"
 
-experimental::TrackAfterFitExtension::Hits::Hits(
-    const std::vector<LHCb::LHCbID> &container)
-    : m_hits(container.begin(), container.end()) {}
-experimental::TrackAfterFitExtension::Hits::Hits(
-    const LHCb::span<const LHCb::LHCbID> container)
-    : m_hits(container.begin(), container.end()) {}
+experimental::TrackAfterFitExtension::Hits::Hits( const std::vector<LHCb::LHCbID>& container )
+    : m_hits( container.begin(), container.end() )
+{
+}
+experimental::TrackAfterFitExtension::Hits::Hits( const LHCb::span<const LHCb::LHCbID> container )
+    : m_hits( container.begin(), container.end() )
+{
+}
+
+experimental::TrackAfterFitExtension::AdditionalStates::AdditionalStates( LHCb::States::const_iterator it1,
+                                                                          LHCb::States::const_iterator it2 )
+    : m_moreStates( it1, it2 )
+{
+
+  auto rearrange =
+      [this]( LHCb::State::Location location, LHCb::State& output ) {
+        auto iter = std::find_if( this->m_moreStates.begin(), this->m_moreStates.end(),
+                                  [&]( const LHCb::State* s ) { return s->checkLocation( location ); } );
+        if ( iter != m_states.end() ) {
+          auto retval = std::move( *iter );
+          this->m_moreStates->erase( iter );
+        }
+      }
+
+  rearrange( LHCb::State::BegRich1, m_BegRich1 );
+  rearrange( LHCb::State::EndRich1, m_EndRich1 );
+  rearrange( LHCb::State::BegRich2, m_BegRich2 );
+}
+
