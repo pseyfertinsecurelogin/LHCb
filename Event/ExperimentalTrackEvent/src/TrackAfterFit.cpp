29ce909269d14bbd4f573518a39d23533cbe838d
Paul Seyfert <pseyfert@cern.ch>
2018-06-28 15:35:05 +0200

    ironing build out

--- Event/ExperimentalTrackEvent/src/TrackAfterFit.cpp
+++ Event/ExperimentalTrackEvent/src/TrackAfterFit.cpp
@@ -9,20 +9,20 @@ experimental::TrackAfterFitExtension::Hits::Hits( const LHCb::span<const LHCb::L
 {
 }
 
-      experimental::TrackAfterFitExtension::AdditionalStates::AdditionalStates( decltype(std::declval<LHCb::Track>().states().begin()) it1,
-                        decltype(std::declval<LHCb::Track>().states().begin()) it2)
+experimental::TrackAfterFitExtension::AdditionalStates::AdditionalStates(
+    decltype( std::declval<const LHCb::Track>().states().begin() ) it1,
+    decltype( std::declval<const LHCb::Track>().states().begin() ) it2 )
     : m_moreStates( it1, it2 )
 {
 
-  auto rearrange =
-      [this]( LHCb::State::Location location, LHCb::State& output ) {
-        auto iter = std::find_if( this->m_moreStates.begin(), this->m_moreStates.end(),
-                                  [&]( const LHCb::State* s ) { return s->checkLocation( location ); } );
-        if ( iter != m_moreStates.end() ) {
-          auto retval = std::move( *iter );
-          this->m_moreStates->erase( iter );
-        }
-      }
+  auto rearrange = [this]( LHCb::State::Location location, LHCb::State& output ) {
+    auto iter = std::find_if( this->m_moreStates.begin(), this->m_moreStates.end(),
+                              [&]( const LHCb::State s ) { return s.checkLocation( location ); } );
+    if ( iter != m_moreStates.end() ) {
+      output = std::move( *iter );
+      this->m_moreStates.erase( iter );
+    }
+  };
 
   rearrange( LHCb::State::BegRich1, m_BegRich1 );
   rearrange( LHCb::State::EndRich1, m_EndRich1 );
