d65ad04e85cbc979761d8a4542d1cfe3dd249a8d
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2018-07-02 17:00:47 +0100

    Update AdditionalStates constructor to not rely on old Track

--- Event/ExperimentalTrackEvent/src/TrackAfterFit.cpp
+++ Event/ExperimentalTrackEvent/src/TrackAfterFit.cpp
@@ -4,28 +4,29 @@ experimental::TrackAfterFitExtension::Hits::Hits( const std::vector<LHCb::LHCbID
     : m_hits( container.begin(), container.end() )
 {
 }
+
 experimental::TrackAfterFitExtension::Hits::Hits( const LHCb::span<const LHCb::LHCbID> container )
     : m_hits( container.begin(), container.end() )
 {
 }
 
-experimental::TrackAfterFitExtension::AdditionalStates::AdditionalStates(
-    decltype( std::declval<const LHCb::Track>().states().begin() ) it1,
-    decltype( std::declval<const LHCb::Track>().states().begin() ) it2 )
-    : m_moreStates( it1, it2 )
+experimental::TrackAfterFitExtension::AdditionalStates::
+AdditionalStates( std::vector<LHCb::State> && states )
+  : m_moreStates( std::forward< std::vector<LHCb::State> > ( states ) )
 {
 
-  auto rearrange = [this]( LHCb::State::Location location, LHCb::State& output ) {
-    auto iter = std::find_if( this->m_moreStates.begin(), this->m_moreStates.end(),
-                              [&]( const LHCb::State s ) { return s.checkLocation( location ); } );
-    if ( iter != m_moreStates.end() ) {
-      output = std::move( *iter );
-      this->m_moreStates.erase( iter );
-    }
-  };
-
+  auto rearrange = [this]( const LHCb::State::Location location, LHCb::State& output )
+    {
+      auto iter = std::find_if( this->m_moreStates.begin(), this->m_moreStates.end(),
+                                [&]( const auto & s ) { return s.checkLocation( location ); } );
+      if ( iter != m_moreStates.end() )
+      {
+        output = std::move( *iter );
+        this->m_moreStates.erase( iter );
+      }
+    };
+  
   rearrange( LHCb::State::BegRich1, m_BegRich1 );
   rearrange( LHCb::State::EndRich1, m_EndRich1 );
   rearrange( LHCb::State::BegRich2, m_BegRich2 );
 }
-
