65d3611933c72a8aeee8006557f9672a1c8d3629
Sebastien Ponce <sebastien.ponce@cern.ch>
2018-10-09 18:03:28 +0200

    Extracted generic code from PrVeloUT concerning UT geometry
    This will in particular be reused in PrAddUTHitsTool

--- UT/UTDAQ/UTDAQ/UTDAQHelper.h
+++ UT/UTDAQ/UTDAQ/UTDAQHelper.h
@@ -3,6 +3,10 @@
 
 #include <optional>
 #include "Kernel/STLExtensions.h"
+#include <array>
+#include <boost/container/small_vector.hpp>
+#include "STDet/DeUTDetector.h"
+#include "STDet/DeUTSector.h"
 
 namespace LHCb {
 
@@ -17,6 +21,48 @@ namespace UTDAQ {
  */
   std::optional<unsigned int> nbUTClusters(LHCb::span<const RawBank *> banks,
                                            unsigned int maxNbClusters);
+
+  struct LayerInfo final {
+    float z;
+    unsigned int nColsPerSide;
+    unsigned int nRowsPerSide;
+    float invHalfSectorYSize;
+    float invHalfSectorXSize;
+    float dxDy;
+  };
+  using SectorsInRegionZ = std::array<float,98>;
+  using SectorsInLayerZ = std::array<SectorsInRegionZ,3>;
+  using SectorsInStationZ = std::array<SectorsInLayerZ,2>;
+
+  constexpr static const auto mapQuarterSectorToSectorCentralRegion = std::array {
+    6,6,9,9,10,10,13,13,
+    7,7,8,8,11,11,12,12,
+    25,25,26,28,31,33,34,34,
+    24,24,27,29,30,32,35,35,
+    46,46,49,51,52,54,57,57,
+    47,47,48,50,53,55,56,56,
+    69,69,70,70,73,73,74,74,
+    68,68,71,71,72,72,75,75
+  };
+
+  constexpr static const auto  mapSectorToSector = std::array {
+    1,2,3,4,5,0,0,0,0,14,15,16,17,18,
+    19,20,21,22,23,0,0,0,0,36,37,38,39,40,
+    41,42,43,44,45,0,0,0,0,58,59,60,61,62,
+    63,64,65,66,67,0,0,0,0,76,77,78,79,80
+  };
+
+  /**
+   * fills container of (region, sector) pairs with all sectors concerned by
+   * a hit at given layer and coordinates and with given x tolerance
+   */
+  void findSectors(unsigned int layer, float x, float y, float xTol, float yTol,
+                   boost::container::small_vector<std::pair<int, int>,9>& sectors,
+                   const LayerInfo& info);
+
+  void computeGeometry(const DeSTDetector &utDet,
+                       std::array<LayerInfo,4> &layers,
+                       std::array<SectorsInStationZ,2> &sectorsZ);
 
 } // namespace UTDAQ
 
--- UT/UTDAQ/src/Lib/UTDAQHelper.cpp
+++ UT/UTDAQ/src/Lib/UTDAQHelper.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <limits>
 #include "UTDAQ/UTDAQHelper.h"
 #include "Event/RawBank.h"
 #include "SiDAQ/SiHeaderWord.h"
@@ -15,4 +17,127 @@ LHCb::UTDAQ::nbUTClusters(LHCb::span<const RawBank *> banks,
     }
   }
   return nbClusters;
+}
+
+void LHCb::UTDAQ::findSectors(unsigned int layer, float x, float y, float xTol, float yTol,
+                              boost::container::small_vector<std::pair<int, int>,9>& sectors,
+                              const LHCb::UTDAQ::LayerInfo& info) {
+  auto localX = x - info.dxDy * y;
+  // deal with sector overlaps and geometry imprecision
+  xTol += 1; //mm
+  auto localXmin = localX - xTol;
+  auto localXmax = localX + xTol;
+  int subcolmin = std::round(localXmin * info.invHalfSectorXSize-0.5)+2*info.nColsPerSide;
+  int subcolmax = std::round(localXmax * info.invHalfSectorXSize-0.5)+2*info.nColsPerSide;
+  if (subcolmax < 0 || subcolmin >= (int)(4*info.nColsPerSide)) {
+    // out of acceptance, return empty result
+    return;
+  }
+  // on the acceptance limit
+  if (subcolmax >= (int)(4*info.nColsPerSide)) subcolmax = (int)(4*info.nColsPerSide) - 1;
+  if (subcolmin < 0) subcolmin = 0;
+  // deal with sector shifts in tilted layers and overlaps in regular ones
+  yTol += (layer == 1 || layer == 2) ? 8 : 1; //  mm
+  auto localYmin = y-yTol;
+  auto localYmax = y+yTol;
+  int subrowmin = std::round(localYmin * info.invHalfSectorYSize-0.5)+2*info.nRowsPerSide;
+  int subrowmax = std::round(localYmax * info.invHalfSectorYSize-0.5)+2*info.nRowsPerSide;
+  if (subrowmax < 0 || subrowmin >= (int)(4*info.nRowsPerSide)) {
+    // out of acceptance, return empty result
+    return;
+  }
+  // on the acceptance limit
+  if (subrowmax >= (int)(4*info.nRowsPerSide)) subrowmax = (int)(4*info.nRowsPerSide) - 1;
+  if (subrowmin <0) subrowmin = 0;
+  for (int subcol=subcolmin; subcol <= subcolmax; subcol++) {
+    int region = subcol < (int)(2*info.nColsPerSide-4) ? 1 : subcol >= (int)(2*info.nColsPerSide+4) ? 3 : 2;
+    for (int subrow=subrowmin; subrow <= subrowmax; subrow++) {
+      if (region == 1) {
+        sectors.emplace_back(1, (subcol/2)*info.nRowsPerSide*2+subrow/2+1);
+      } else if (region == 2) {
+        int subcolInReg = subcol - 2*info.nColsPerSide + 4;
+        if (subrow < (int)(2*info.nRowsPerSide-4) || subrow >= (int)(2*info.nRowsPerSide+4)) {
+          // no in central Region
+          sectors.emplace_back(2, mapSectorToSector[(subcolInReg/2)*14+(subrow/2)]);
+        } else {
+          // central region
+          sectors.emplace_back(2, mapQuarterSectorToSectorCentralRegion[subcolInReg*8+subrow-2*info.nRowsPerSide+4]);
+        }
+      } else {
+        sectors.emplace_back(3, (subcol/2-info.nColsPerSide-2)*info.nRowsPerSide*2+subrow/2+1);
+      }
+    }
+  }
+}
+
+void LHCb::UTDAQ::computeGeometry(const DeSTDetector &utDet,
+                                  std::array<LHCb::UTDAQ::LayerInfo,4> &layers,
+                                  std::array<SectorsInStationZ,2> &sectorsZ) {
+  for(int iStation = 0; iStation < 2; ++iStation){
+    for(int iLayer = 0; iLayer < 2; ++iLayer){
+      // get layer
+      unsigned int layerIndex = 2*iStation+iLayer;
+      DeSTLayer* layer = utDet.layers()[layerIndex];
+      // get the z position of the layer and store it
+      layers[layerIndex].z = layer->sectors()[0]->sensors()[0]->plane().ProjectOntoPlane({0,0,0}).z();
+      // get the layer size and sector sizes. Go through all sectors, do not rely on ordering
+      float YFirstRow = std::numeric_limits<float>::max();
+      float YLastRow = std::numeric_limits<float>::lowest();
+      float smallestXLastCol = std::numeric_limits<float>::max();
+      float smallestXFirstcol = std::numeric_limits<float>::max();
+      float biggestXFirstCol = std::numeric_limits<float>::lowest();
+      unsigned int biggestColumn = 0;
+      unsigned int smallestColumn = 999;
+      unsigned int topMostRow = 0;
+      unsigned int bottomMostRow = 999;
+      // First pass
+      // deal with col/row numbers, we need a UTSector for that
+      // Note that rows/cols cannot be relied on the middle columns/rows
+      // as they are not anymore "rows/cols" but "number of sectors
+      // in the column/row". So we use only first column and row
+      for (const auto& sector : layer->sectors()) {
+        DeUTSector* utSector = dynamic_cast<DeUTSector*>(sector);
+        auto column = utSector->column();
+        auto row = utSector->row();
+        smallestColumn = std::min<float>(smallestColumn, column);
+        if (utSector->column() == smallestColumn) {
+          topMostRow = std::max<float>(topMostRow, row);
+          bottomMostRow = std::min<float>(bottomMostRow, row);
+        }
+        if (utSector->row() == bottomMostRow) {
+          biggestColumn = std::max<float>(biggestColumn, column);
+        }
+      }
+      // Second pass
+      // find x and y values in the corners to deduce the geometry of the layer
+      for (const auto& sector : layer->sectors()) {
+        // deal with x,y coordinates. Remember the corner coordinates
+        DeUTSector* utSector = dynamic_cast<DeUTSector*>(sector);
+        auto column = utSector->column();
+        auto row = utSector->row();
+        auto center = sector->geometry()->toGlobal(Gaudi::XYZPoint{0,0,0});
+        if (column == smallestColumn) {
+          if (row == bottomMostRow) {
+            smallestXFirstcol = center.x();
+            YFirstRow = center.y();
+          } else if (row == topMostRow) {
+            biggestXFirstCol = center.x();
+            YLastRow = center.y();
+          }
+        }
+        if (column == biggestColumn && row == bottomMostRow) {
+          smallestXLastCol = center.x();
+        }
+        sectorsZ[iStation][iLayer][sector->elementID().detRegion()-1][sector->elementID().sector()-1] = center.z();
+      }
+      // gather all information into the corresponding LayerInfo object
+      auto ncols = biggestColumn - smallestColumn + 1;
+      auto nrows = topMostRow - bottomMostRow + 1;
+      layers[layerIndex].nColsPerSide = ncols / 2;
+      layers[layerIndex].nRowsPerSide = nrows / 2;
+      layers[layerIndex].invHalfSectorYSize = 2*(nrows-1)/(YLastRow-YFirstRow);
+      layers[layerIndex].invHalfSectorXSize = 2*(ncols-1)/(smallestXLastCol-smallestXFirstcol);
+      layers[layerIndex].dxDy = (biggestXFirstCol-smallestXFirstcol)/(YLastRow-YFirstRow);
+    }
+  }
 }
