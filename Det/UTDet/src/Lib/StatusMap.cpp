357dc51a7f7b27ab66042c40d73716a923c5e01b
Marco Cattaneo <marco.cattaneo@cern.ch>
2018-10-23 16:02:25 +0200

    Add Copyright, remove unnecessary release.notes

--- Det/UTDet/src/Lib/StatusMap.cpp
+++ Det/UTDet/src/Lib/StatusMap.cpp
@@ -1,3 +1,13 @@
+/*****************************************************************************\
+* (c) Copyright 2018 CERN for the benefit of the LHCb Collaboration           *
+*                                                                             *
+* This software is distributed under the terms of the GNU General Public      *
+* Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   *
+*                                                                             *
+* In applying this licence, CERN does not waive the privileges and immunities *
+* granted to it by virtue of its status as an Intergovernmental Organization  *
+* or submit itself to any jurisdiction.                                       *
+\*****************************************************************************/
 #include "UTDet/StatusMap.h"
 #if !(defined(__GXX_EXPERIMENTAL_CXX0X__) || __cplusplus >= 201103L)
 #include <boost/assign/list_of.hpp>
