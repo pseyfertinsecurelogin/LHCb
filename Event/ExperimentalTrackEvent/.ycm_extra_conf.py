e3b1f14a6b801dd6072f9e577556dc54429aef69
Paul Seyfert <pseyfert.mathphys@gmail.com>
2018-10-24 12:49:56 +0200

    adding copyright notice

--- Event/ExperimentalTrackEvent/.ycm_extra_conf.py
+++ Event/ExperimentalTrackEvent/.ycm_extra_conf.py
@@ -1,3 +1,13 @@
+###############################################################################
+# (c) Copyright 2018 CERN for the benefit of the LHCb Collaboration           #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
 # ycm configuration to pick up include directories from CMake
 # requires SET( CMAKE_EXPORT_COMPILE_COMMANDS ON ) in CMakeLists.txt
 # mostly from http://bastian.rieck.ru/blog/posts/2015/ycm_cmake/
