886f67ad1e67eaeb341640287de2bedcef3dd115
Paul Seyfert <pseyfert@cern.ch>
2018-11-13 20:10:45 +0100

    seems the input wasn't sorted ... strange but fine, whatever, hope it's not too expensive

--- Event/TrackEvent/src/PrPixelOutput.cpp
+++ Event/TrackEvent/src/PrPixelOutput.cpp
@@ -6,7 +6,7 @@ foo::PrPixelOutputForTracking::operator LHCb::Event::v2::Track() const {
 
   // actually it almost seems like the parameterized kalman doesn't even use that.
   // retval.addToStates(this->state); // yes, this can be left out, but prefer for readability. readability is in the eye of the reader
-  retval.setLhcbIDs( this->lhcbIDs, LHCb::Tag::Sorted_tag{} );
+  retval.setLhcbIDs( this->lhcbIDs, LHCb::Tag::Unordered_tag{});
   retval.setFlags(this->m_flags);
   // skip chi2perdof, should get done by whoever copies for their purpuse
   // skip likelihood, ghostprob, extrainfos, history
