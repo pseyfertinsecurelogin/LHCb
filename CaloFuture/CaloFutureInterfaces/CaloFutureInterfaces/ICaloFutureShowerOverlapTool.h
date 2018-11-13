5f2dbced60fa31589f5024b2e21e749033d0d159
cmarinbe <noreply-lhcb-upgrade-hackathon@cern.ch>
2018-11-13 09:03:44 +0100

    pass by ref instead of pointer in CaloFutureShoweOverlapTool

--- CaloFuture/CaloFutureInterfaces/CaloFutureInterfaces/ICaloFutureShowerOverlapTool.h
+++ CaloFuture/CaloFutureInterfaces/CaloFutureInterfaces/ICaloFutureShowerOverlapTool.h
@@ -34,8 +34,8 @@ struct ICaloFutureShowerOverlapTool : extend_interfaces<IAlgTool>
   // Return the interface ID
   DeclareInterfaceID(ICaloFutureShowerOverlapTool, 2, 0 );
 
-  virtual void process(const LHCb::CaloCluster* c1,const LHCb::CaloCluster* c2,
-                       int niter=5, bool propagateInitialWeights=false) const = 0;
+  virtual void process(LHCb::CaloCluster& c1, LHCb::CaloCluster& c2,
+                       const int niter=5, bool propagateInitialWeights=false) const = 0;
   
 };
 #endif // ICALOFUTURESHOWEROVERLAPTOOL_H
