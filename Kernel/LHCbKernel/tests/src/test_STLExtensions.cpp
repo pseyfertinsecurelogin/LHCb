29cc5542558d40ab5609849f15d9442875ac9601
Gerhard Raven <gerhard.raven@nikhef.nl>
2018-12-11 14:01:16 +0100

    Add LHCb::range::single<T>
    * class which can be used as a range of T, but which is actually a
      single element -- i.e. this looks like a vector which always has
      size 1.
    * Useful if an interface is written in terms of a range, but one wants
      to calls it with only a single element...

--- Kernel/LHCbKernel/tests/src/test_STLExtensions.cpp
+++ Kernel/LHCbKernel/tests/src/test_STLExtensions.cpp
@@ -32,3 +32,58 @@ BOOST_AUTO_TEST_CASE( span )
     BOOST_CHECK( sum( LHCb::make_span(vi) ) == 30 );
   }
 }
+
+BOOST_AUTO_TEST_CASE( range_single )
+{
+  {
+      auto single = LHCb::range::single{ 4.0 };
+      auto span = LHCb::make_span(single);
+      static_assert( span.extent == gsl::dynamic_extent ); //FIXME -- annoying that this isn't 1
+      BOOST_CHECK( sum( span ) == single.value() );
+      BOOST_CHECK( single.size() == 1 );
+  }
+
+  {
+      // check conversion single<T> -> span<T>
+      auto single = LHCb::range::single{ 4.0 };
+      LHCb::span<double> d = single;
+      static_assert( d.extent == gsl::dynamic_extent ); //FIXME annoying, this has dynamic extent...
+      LHCb::span<double,1> e = single;
+      static_assert( e.extent == 1 );
+      auto f = static_cast<LHCb::span<double,1>>(single);
+      auto g = static_cast<LHCb::span<double>>(single);
+  }
+
+  // check two ways of moving out of single...
+
+  {
+      auto single_uptr = LHCb::range::single{ std::make_unique<int>(99) };
+      BOOST_CHECK( static_cast<bool>(single_uptr.value()) );
+
+      auto uptr = std::move(single_uptr.value());
+
+      BOOST_CHECK( !static_cast<bool>(single_uptr.value()) );
+      BOOST_CHECK( *uptr == 99 );
+  }
+
+  {
+      auto single_uptr = LHCb::range::single{ std::make_unique<int>(99) };
+      BOOST_CHECK( static_cast<bool>(single_uptr.value()) );
+
+      auto uptr = std::move(single_uptr).value();
+
+      BOOST_CHECK( !static_cast<bool>(single_uptr.value()) );
+      BOOST_CHECK( *uptr == 99 );
+  }
+
+  // check in place construction
+  {
+      struct S { constexpr S(double,int,const char*) {} };
+      constexpr auto s1 = LHCb::range::single<S>{std::in_place,2.5,3,"HelloGoodbye" };
+      static_assert( s1.size() == 1 );
+      static_assert( !s1.empty() );
+  }
+
+
+
+}
