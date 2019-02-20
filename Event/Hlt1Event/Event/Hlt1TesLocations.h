20a17477b804c99dd520bb63ef26b6ccacb44b2f
Peter Kardos <peter.kardos@cern.ch>
2019-02-20 19:06:45 +0100

    added tes locations

--- Event/Hlt1Event/Event/Hlt1TesLocations.h
+++ Event/Hlt1Event/Event/Hlt1TesLocations.h
@@ -20,8 +20,14 @@ namespace LHCb::HLT1::TESLocations {
     [[maybe_unused]] inline const char* PrPixelEndStates = "Rec/HLT1/PrPixelEndStates";
     [[maybe_unused]] inline const char* PrPixelVeloTracks = "Rec/HLT1/PrPixelVeloTracks";
     [[maybe_unused]] inline const char* PrPixelChiSquares = "Rec/HLT1/PrPixelChiSquares";
+
     [[maybe_unused]] inline const char* PrPixelHitsSelection = "Rec/HLT1/PrPixelHits_Selection";
     [[maybe_unused]] inline const char* PrPixelBeamStatesSelection = "Rec/HLT1/PrPixelBeamStates_Selection";
     [[maybe_unused]] inline const char* PrPixelEndStatesSelection = "Rec/HLT1/PrPixelEndStates_Selection";
     [[maybe_unused]] inline const char* PrPixelVeloTracksSelection = "Rec/HLT1/PrPixelVeloTracks_Selection";
+
+    [[maybe_unused]] inline const char* PrVeloUtHits = "Rec/HLT1/PrVeloUtHits";
+    [[maybe_unused]] inline const char* PrVeloUtVeloStates = "Rec/HLT1/PrVeloUtVeloStates";
+    [[maybe_unused]] inline const char* PrVeloUtUtStates = "Rec/HLT1/PrVeloUtUtStates";
+    [[maybe_unused]] inline const char* PrVeloUtTracks = "Rec/HLT1/PrVeloUtTracks";
 } // namespace LHCb::HL1::TESLocations
