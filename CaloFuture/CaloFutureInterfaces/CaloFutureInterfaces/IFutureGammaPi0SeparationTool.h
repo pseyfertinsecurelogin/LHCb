dbf8d3946e1f9dac1c6db8d96ec848d941b54da9
calvom <calvom@lxplus069.cern.ch>
2019-03-28 17:41:09 +0100

    update of IsPhoton for Upgrade

--- CaloFuture/CaloFutureInterfaces/CaloFutureInterfaces/IFutureGammaPi0SeparationTool.h
+++ CaloFuture/CaloFutureInterfaces/CaloFutureInterfaces/IFutureGammaPi0SeparationTool.h
@@ -23,7 +23,7 @@
  *
  *
  *  @author Miriam Calvo Gomez --
- *  @date   2010-03-29
+ *  @date   2019-03-28
  */
 struct IFutureGammaPi0SeparationTool : extend_interfaces<IAlgTool> {
 
@@ -34,12 +34,8 @@ struct IFutureGammaPi0SeparationTool : extend_interfaces<IAlgTool> {
   virtual double isPhoton( const double* v )                                                = 0;
   virtual bool ClusterVariables( const LHCb::CaloHypo* hypo, double& fr2, double& fasym, double& fkappa, double& fr2r4,
                                  double& etot, double& Eseed, double& E2, int& area )       = 0;
-  virtual bool PrsVariables( const LHCb::CaloHypo* hypo, double& r2PS, double& asymPS, double& kappaPS, double& r2r4PS,
-                             double& eSumPS, double& ePrs, double& eMaxPS, double& e2ndPS, double& ecornerPS,
-                             int& multiPS, int& multiPS15, int& multiPS30, int& multiPS45 ) = 0;
 
   virtual double                        inputData( std::string data ) = 0;
   virtual std::map<std::string, double> inputDataMap()                = 0;
-  virtual std::map<std::string, double> inputPrsDataMap()             = 0;
 };
 #endif // IFUTUREGAMMAPI0SEPARATIONTOOL_H
