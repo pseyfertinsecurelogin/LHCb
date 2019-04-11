d5681943a15571d7c1d2cfa6ad9f21b62d35aef0
Gerhard Raven <gerhard.raven@nikhef.nl>
2019-04-11 16:52:09 +0200

    prefer inline constexp over static const variables

--- FT/FTDAQ/src/FTRawBankParams.h
+++ FT/FTDAQ/src/FTRawBankParams.h
@@ -20,11 +20,11 @@ namespace FTRawBank {
     sizeShift     = 0,
   };
 
-  static constexpr uint16_t nbClusMaximum   = 31;  // 5 bits
-  static constexpr uint16_t nbClusFFMaximum = 10;  //
-  static constexpr uint16_t fractionMaximum = 1;   // 1 bits allocted
-  static constexpr uint16_t cellMaximum     = 127; // 0 to 127; coded on 7 bits
-  static constexpr uint16_t sizeMaximum     = 1;   // 1 bits allocated
+  inline constexpr uint16_t nbClusMaximum   = 31;  // 5 bits
+  inline constexpr uint16_t nbClusFFMaximum = 10;  //
+  inline constexpr uint16_t fractionMaximum = 1;   // 1 bits allocted
+  inline constexpr uint16_t cellMaximum     = 127; // 0 to 127; coded on 7 bits
+  inline constexpr uint16_t sizeMaximum     = 1;   // 1 bits allocated
 
   enum BankProperties { NbBanks = 240, NbLinksPerBank = 24 };
 
--- Kernel/LHCbKernel/Kernel/MuonBase.h
+++ Kernel/LHCbKernel/Kernel/MuonBase.h
@@ -23,51 +23,51 @@ namespace MuonBase {
 
   typedef unsigned int ContentType;
   //
-  static const unsigned int BitsX       = 7;
-  static const unsigned int BitsY       = 4;
-  static const unsigned int BitsQuarter = 2;
-  static const unsigned int BitsRegion  = 2;
-  static const unsigned int BitsReadout = 0;
-  static const unsigned int BitsLayer   = 0;
-  static const unsigned int BitsStation = 3;
-  static const unsigned int BitsLayoutX = 6;
-  static const unsigned int BitsLayoutY = 4;
+  inline const unsigned int BitsX       = 7;
+  inline const unsigned int BitsY       = 4;
+  inline const unsigned int BitsQuarter = 2;
+  inline const unsigned int BitsRegion  = 2;
+  inline const unsigned int BitsReadout = 0;
+  inline const unsigned int BitsLayer   = 0;
+  inline const unsigned int BitsStation = 3;
+  inline const unsigned int BitsLayoutX = 6;
+  inline const unsigned int BitsLayoutY = 4;
 
   //
-  static const unsigned int BitsIndex = BitsX + BitsY + BitsRegion + BitsQuarter;
-  static const unsigned int BitsKey   = BitsIndex + BitsReadout + BitsLayer + BitsStation;
+  inline const unsigned int BitsIndex = BitsX + BitsY + BitsRegion + BitsQuarter;
+  inline const unsigned int BitsKey   = BitsIndex + BitsReadout + BitsLayer + BitsStation;
   //
-  static const unsigned int ShiftY       = 0;
-  static const unsigned int ShiftX       = ShiftY + BitsY;
-  static const unsigned int ShiftQuarter = ShiftX + BitsX;
-  static const unsigned int ShiftRegion  = ShiftQuarter + BitsQuarter;
-  static const unsigned int ShiftReadout = ShiftRegion + BitsRegion;
-  static const unsigned int ShiftLayer   = ShiftReadout + BitsReadout;
-  static const unsigned int ShiftStation = ShiftLayer + BitsLayer;
-  static const unsigned int ShiftLayoutX = ShiftStation + BitsStation;
-  static const unsigned int ShiftLayoutY = ShiftLayoutX + BitsLayoutX;
+  inline const unsigned int ShiftY       = 0;
+  inline const unsigned int ShiftX       = ShiftY + BitsY;
+  inline const unsigned int ShiftQuarter = ShiftX + BitsX;
+  inline const unsigned int ShiftRegion  = ShiftQuarter + BitsQuarter;
+  inline const unsigned int ShiftReadout = ShiftRegion + BitsRegion;
+  inline const unsigned int ShiftLayer   = ShiftReadout + BitsReadout;
+  inline const unsigned int ShiftStation = ShiftLayer + BitsLayer;
+  inline const unsigned int ShiftLayoutX = ShiftStation + BitsStation;
+  inline const unsigned int ShiftLayoutY = ShiftLayoutX + BitsLayoutX;
   //
-  static const unsigned int ShiftIndex = 0;
-  static const unsigned int ShiftKey   = 0;
+  inline const unsigned int ShiftIndex = 0;
+  inline const unsigned int ShiftKey   = 0;
   //
-  static const ContentType MaskX       = ( ( ( (ContentType)1 ) << BitsX ) - 1 ) << ShiftX;
-  static const ContentType MaskY       = ( ( ( (ContentType)1 ) << BitsY ) - 1 ) << ShiftY;
-  static const ContentType MaskRegion  = ( ( ( (ContentType)1 ) << BitsRegion ) - 1 ) << ShiftRegion;
-  static const ContentType MaskQuarter = ( ( ( (ContentType)1 ) << BitsQuarter ) - 1 ) << ShiftQuarter;
-  static const ContentType MaskLayoutX = ( ( ( (ContentType)1 ) << BitsLayoutX ) - 1 ) << ShiftLayoutX;
-  static const ContentType MaskLayoutY = ( ( ( (ContentType)1 ) << BitsLayoutY ) - 1 ) << ShiftLayoutY;
-  static const ContentType MaskReadout = ( ( ( (ContentType)1 ) << BitsReadout ) - 1 ) << ShiftReadout;
-  static const ContentType MaskLayer   = ( ( ( (ContentType)1 ) << BitsLayer ) - 1 ) << ShiftLayer;
-  static const ContentType MaskStation = ( ( ( (ContentType)1 ) << BitsStation ) - 1 ) << ShiftStation;
+  inline const ContentType MaskX       = ( ( ( (ContentType)1 ) << BitsX ) - 1 ) << ShiftX;
+  inline const ContentType MaskY       = ( ( ( (ContentType)1 ) << BitsY ) - 1 ) << ShiftY;
+  inline const ContentType MaskRegion  = ( ( ( (ContentType)1 ) << BitsRegion ) - 1 ) << ShiftRegion;
+  inline const ContentType MaskQuarter = ( ( ( (ContentType)1 ) << BitsQuarter ) - 1 ) << ShiftQuarter;
+  inline const ContentType MaskLayoutX = ( ( ( (ContentType)1 ) << BitsLayoutX ) - 1 ) << ShiftLayoutX;
+  inline const ContentType MaskLayoutY = ( ( ( (ContentType)1 ) << BitsLayoutY ) - 1 ) << ShiftLayoutY;
+  inline const ContentType MaskReadout = ( ( ( (ContentType)1 ) << BitsReadout ) - 1 ) << ShiftReadout;
+  inline const ContentType MaskLayer   = ( ( ( (ContentType)1 ) << BitsLayer ) - 1 ) << ShiftLayer;
+  inline const ContentType MaskStation = ( ( ( (ContentType)1 ) << BitsStation ) - 1 ) << ShiftStation;
   //
-  static const ContentType MaskIndex = ( ( ( (ContentType)1 ) << BitsIndex ) - 1 ) << ShiftIndex;
-  static const ContentType MaskKey   = ( ( ( (ContentType)1 ) << BitsKey ) - 1 ) << ShiftKey;
+  inline const ContentType MaskIndex = ( ( ( (ContentType)1 ) << BitsIndex ) - 1 ) << ShiftIndex;
+  inline const ContentType MaskKey   = ( ( ( (ContentType)1 ) << BitsKey ) - 1 ) << ShiftKey;
 
-  static const int CENTER = 0;
-  static const int UP     = 1;
-  static const int DOWN   = -1;
-  static const int RIGHT  = 1;
-  static const int LEFT   = -1;
+  inline const int CENTER = 0;
+  inline const int UP     = 1;
+  inline const int DOWN   = -1;
+  inline const int RIGHT  = 1;
+  inline const int LEFT   = -1;
 
 } // namespace MuonBase
 
