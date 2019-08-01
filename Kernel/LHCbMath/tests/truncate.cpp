415154cec4bbf0d8e4f7f270e41d5f6b4fb10a48
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2019-08-01 20:59:40 +0100

    TestTruncate - test scalar double and a number of different precisions

--- Kernel/LHCbMath/tests/truncate.cpp
+++ Kernel/LHCbMath/tests/truncate.cpp
@@ -10,6 +10,7 @@
 \*****************************************************************************/
 
 // STD & STL
+#include <array>
 #include <iostream>
 
 // LHCbMath
@@ -17,32 +18,55 @@
 
 template <typename TYPE>
 inline void print( const TYPE x, const TYPE x_abs, const TYPE x_rel ) {
-  std::cout << std::defaultfloat << std::setw( 10 ) << x << ' '        //
-            << std::defaultfloat << std::setw( 10 ) << x_abs << ' '    //
-            << std::defaultfloat << std::setw( 10 ) << x_rel << "    " //
-            << std::hexfloat << std::setw( 18 ) << x << ' '            //
-            << std::hexfloat << std::setw( 18 ) << x_abs << ' '        //
-            << std::hexfloat << std::setw( 18 ) << x_rel << std::endl;
+  std::cout << std::setprecision( 8 )                        //
+            << std::fixed << std::setw( 16 ) << x << " "     //
+            << std::fixed << std::setw( 16 ) << x_abs << " " //
+            << std::fixed << std::setw( 16 ) << x_rel << "    | ";
+  std::cout << std::hexfloat << std::setw( 24 ) << x << "   "     //
+            << std::hexfloat << std::setw( 24 ) << x_abs << "   " //
+            << std::hexfloat << std::setw( 24 ) << x_rel << std::endl;
 }
 
-int main() {
-
-  constexpr std::size_t PREC = 1u;
-
+template <std::size_t PREC_ABS, std::size_t PREC_REL>
+void test() {
   using namespace LHCb::Math;
 
-  for ( float x = 0; x < 1; x += 0.01 ) {
+  std::cout << "test float PREC(ABS,REL) = (" << PREC_ABS << "," << PREC_REL << ")" << std::endl;
+  for ( float x = 0.00876543; x < 1; x += 0.01 ) {
     // test scalar
-    const auto x_abs = truncate<PrecisionMode::Absolute, PREC>( x );
-    const auto x_rel = truncate<PrecisionMode::Relative, PREC>( x );
+    const auto x_abs = truncate<PrecisionMode::Absolute, PREC_ABS>( x );
+    const auto x_rel = truncate<PrecisionMode::Relative, PREC_REL>( x );
     print( x, x_abs, x_rel );
     // test SIMD
     x += 0.01;
     const auto y     = LHCb::SIMD::FPF( x );
-    const auto y_abs = truncate<PrecisionMode::Absolute, PREC>( y );
-    const auto y_rel = truncate<PrecisionMode::Relative, PREC>( y );
+    const auto y_abs = truncate<PrecisionMode::Absolute, PREC_ABS>( y );
+    const auto y_rel = truncate<PrecisionMode::Relative, PREC_REL>( y );
     print( y[0], y_abs[0], y_rel[0] );
   }
 
+  std::cout << "test double PREC(ABS,REL) = (" << PREC_ABS << "," << PREC_REL << ")" << std::endl;
+  for ( double x = 0.00876543; x < 1; x += 0.01 ) {
+    // test scalar
+    const auto x_abs = truncate<PrecisionMode::Absolute, PREC_ABS>( x );
+    const auto x_rel = truncate<PrecisionMode::Relative, PREC_REL>( x );
+    print( x, x_abs, x_rel );
+    // test SIMD - Not yet supported by Vc
+    // x += 0.01;
+    // const auto y     = LHCb::SIMD::FPD( x );
+    // const auto y_abs = truncate<PrecisionMode::Absolute, PREC_ABS>( y );
+    // const auto y_rel = truncate<PrecisionMode::Relative, PREC_REL>( y );
+    // print( y[0], y_abs[0], y_rel[0] );
+  }
+}
+
+int main() {
+  // test a number of different precisions
+  test<1, 1>();
+  test<2, 2>();
+  test<3, 3>();
+  test<4, 6>();
+  test<4, 16>();
+  // return
   exit( 0 );
 }
