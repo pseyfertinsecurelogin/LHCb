d1b9177bcfffc783aa220a02c61a541f0f96afb6
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2019-08-01 20:25:59 +0100

    TestTruncate - Exercise both scalar and SIMD truncation

--- Kernel/LHCbMath/tests/truncate.cpp
+++ Kernel/LHCbMath/tests/truncate.cpp
@@ -15,19 +15,33 @@
 // LHCbMath
 #include "LHCbMath/Truncate.h"
 
+template <typename TYPE>
+inline void print( const TYPE x, const TYPE x_abs, const TYPE x_rel ) {
+  std::cout << std::defaultfloat << std::setw( 10 ) << x << ' '        //
+            << std::defaultfloat << std::setw( 10 ) << x_abs << ' '    //
+            << std::defaultfloat << std::setw( 10 ) << x_rel << "    " //
+            << std::hexfloat << std::setw( 18 ) << x << ' '            //
+            << std::hexfloat << std::setw( 18 ) << x_abs << ' '        //
+            << std::hexfloat << std::setw( 18 ) << x_rel << std::endl;
+}
+
 int main() {
 
+  constexpr std::size_t PREC = 1u;
+
+  using namespace LHCb::Math;
+
   for ( float x = 0; x < 1; x += 0.01 ) {
-
-    const auto x_abs = LHCb::Math::truncate<LHCb::Math::PrecisionMode::Absolute, 1>( x );
-    const auto x_rel = LHCb::Math::truncate<LHCb::Math::PrecisionMode::Relative, 1>( x );
-
-    std::cout << std::defaultfloat << std::setw( 10 ) << x << ' '        //
-              << std::defaultfloat << std::setw( 10 ) << x_abs << ' '    //
-              << std::defaultfloat << std::setw( 10 ) << x_rel << "    " //
-              << std::hexfloat << std::setw( 18 ) << x << ' '            //
-              << std::hexfloat << std::setw( 18 ) << x_abs << ' '        //
-              << std::hexfloat << std::setw( 18 ) << x_rel << std::endl;
+    // test scalar
+    const auto x_abs = truncate<PrecisionMode::Absolute, PREC>( x );
+    const auto x_rel = truncate<PrecisionMode::Relative, PREC>( x );
+    print( x, x_abs, x_rel );
+    // test SIMD
+    x += 0.01;
+    const auto y     = LHCb::SIMD::FPF( x );
+    const auto y_abs = truncate<PrecisionMode::Absolute, PREC>( y );
+    const auto y_rel = truncate<PrecisionMode::Relative, PREC>( y );
+    print( y[0], y_abs[0], y_rel[0] );
   }
 
   exit( 0 );
