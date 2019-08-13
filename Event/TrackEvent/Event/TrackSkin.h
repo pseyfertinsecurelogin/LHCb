7d67f2658badfb404fb2fd99a0a1d1a37df7a2d5
Sascha Stahl <sascha.stahl@cern.ch>
2019-08-13 20:49:11 +0000

    Add chi2perdof function to track skin

--- Event/TrackEvent/Event/TrackSkin.h
+++ Event/TrackEvent/Event/TrackSkin.h
@@ -21,6 +21,7 @@ namespace LHCb::Event::v2 {
   SOAFIELD( TrackField, Track, SOAFIELD_ACCESSORS( track )
             auto pt() const { return track().pt(); }
             auto pseudoRapidity() const { return track().pseudoRapidity(); }
+            auto chi2PerDoF() const {return track().chi2PerDoF() ;}
             template <typename... Args>
             auto stateAt( Args&&... args ) const {
               return track().stateAt( std::forward<Args>( args )... );
