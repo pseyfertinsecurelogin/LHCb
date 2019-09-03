13e20bfd4a43c28c429454269bf44327e899d026
Olli Lupton <oliver.lupton@cern.ch>
2019-09-03 12:57:42 +0200

    Fix compilation error hidden behind an ifdef.

--- Kernel/LHCbMath/LHCbMath/SIMDWrapper.h
+++ Kernel/LHCbMath/LHCbMath/SIMDWrapper.h
@@ -975,7 +975,7 @@ namespace SIMDWrapper {
       static mask_v mask_false() { return 0x0000; }
       static int_v indices() { return _mm512_setr_epi32( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ); }
       static int_v indices( int start ) { return indices() + start; }
-      static int popcount( mask_v const& mask ) { avx512::popcount( mask ); }
+      static int popcount( mask_v const& mask ) { return avx512::popcount( mask ); }
       static mask_v loop_mask( int i, int n ) { return ( ( i + 16 ) > n ) ? ~( 0xFFFF << ( n & 15 ) ) : 0xFFFF; }
     };
   } // namespace avx512
