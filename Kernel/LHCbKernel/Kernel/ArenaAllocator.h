7be44777558305704adfb9daeb4dca78d23238bb
Gerhard Raven <gerhard.raven@nikhef.nl>
2019-10-28 20:44:13 +0100

    Add a 'dynamic' allocator which is 'movable'

--- Kernel/LHCbKernel/Kernel/ArenaAllocator.h
+++ Kernel/LHCbKernel/Kernel/ArenaAllocator.h
@@ -27,68 +27,145 @@
 #include <new>
 #include <utility>
 
+#include "STLExtensions.h"
+
 namespace LHCb::Allocators {
 
-  template <std::size_t N, std::size_t alignment = alignof( std::max_align_t )>
-  class StaticArena {
-    std::byte* m_ptr = nullptr;
-    alignas( alignment ) std::byte m_buf[N];
+  namespace details {
+    template <std::size_t Alignment>
+    constexpr std::size_t align_up( std::size_t n ) {
+      return ( n + ( Alignment - 1 ) ) & ~( Alignment - 1 );
+    }
+  } // namespace details
 
-    static std::size_t align_up( std::size_t n ) noexcept { return ( n + ( alignment - 1 ) ) & ~( alignment - 1 ); }
-    bool               is_inside( std::byte* p ) const noexcept { return m_buf <= p && p <= m_buf + size(); }
+  template <std::size_t Alignment>
+  class DynamicArena {
+
+    std::byte*      m_current;
+    span<std::byte> m_buffer;
+
+    bool is_inside( std::byte* p ) const noexcept { return m_buffer.data() <= p && p <= m_buffer.data() + size(); }
+
+    DynamicArena( span<std::byte> buffer ) noexcept : m_current{buffer.data()}, m_buffer{buffer} {
+      assert( this + details::align_up<Alignment>( sizeof( DynamicArena ) ) == buffer.data() );
+    }
+
+    struct DynamicArenaDeleter {
+      void operator()( DynamicArena<Alignment>* p ) {
+        p->~DynamicArena<Alignment>();
+        delete[] reinterpret_cast<std::byte*>( p );
+      }
+    };
 
   public:
-    ~StaticArena() { m_ptr = nullptr; }
-    StaticArena() noexcept : m_ptr{m_buf} {}
+    using Handle = std::unique_ptr<DynamicArena<Alignment>, DynamicArenaDeleter>;
+
+    // create a dynamic arena with (at least) N bytes of storage...
+    static auto create( size_t N ) {
+      constexpr auto daSize = details::align_up<Alignment>( sizeof( DynamicArena<Alignment> ) );
+      auto           M      = details::align_up<Alignment>( N );
+      auto           buf    = new std::byte[M + daSize];
+      return Handle( new ( buf )
+                         DynamicArena<Alignment>( {buf + daSize, static_cast<span<std::byte>::index_type>( M )} ) );
+    }
+
+    template <typename T>
+    static constexpr std::size_t align_up( std::size_t N ) {
+      return details::align_up<Alignment>( N * sizeof( T ) );
+    }
+
+    DynamicArena( const DynamicArena& ) = delete;
+    DynamicArena& operator=( const DynamicArena& ) = delete;
+    DynamicArena( DynamicArena&& rhs )             = delete;
+    DynamicArena& operator=( DynamicArena&& rhs ) = delete;
+
+    static constexpr std::size_t alignment = Alignment;
+    std::size_t                  size() const noexcept { return m_buffer.size(); }
+
+    // TODO: use CRTP to share the code below...
+    std::size_t used() const noexcept { return static_cast<std::size_t>( m_current - m_buffer.data() ); }
+
+    template <std::size_t ReqAlign>
+    std::byte* allocate( std::size_t n ) {
+      static_assert( ReqAlign <= alignment, "alignment is too small for this arena" );
+      auto const aligned_n = details::align_up<Alignment>( n );
+      if ( static_cast<decltype( aligned_n )>( m_buffer.data() + size() - m_current ) < aligned_n )
+        throw std::bad_alloc{};
+      return std::exchange( m_current, m_current + aligned_n );
+    }
+
+    void deallocate( std::byte* p, std::size_t n ) noexcept {
+      assert( is_inside( m_current ) );
+      assert( is_inside( p ) );
+      if ( p + details::align_up<Alignment>( n ) == m_current ) m_current = p;
+    }
+
+    void reset() noexcept { m_current = m_buffer; } // FIXME
+  };
+
+  template <std::size_t N, std::size_t Alignment = alignof( std::max_align_t )>
+  class StaticArena {
+    std::byte* m_current = nullptr;
+    alignas( Alignment ) std::byte m_buffer[N];
+
+    bool is_inside( std::byte* p ) const noexcept { return m_buffer <= p && p <= m_buffer + size(); }
+
+  public:
+    ~StaticArena() { m_current = nullptr; }
+    StaticArena() noexcept : m_current{m_buffer} {}
     StaticArena( const StaticArena& ) = delete;
     StaticArena& operator=( const StaticArena& ) = delete;
     StaticArena( StaticArena&& )                 = delete;
     StaticArena& operator=( StaticArena&& ) = delete;
 
+    static constexpr std::size_t alignment = Alignment;
     static constexpr std::size_t size() noexcept { return N; }
-    std::size_t                  used() const noexcept { return static_cast<std::size_t>( m_ptr - m_buf ); }
+
+    // TODO: use CRTP to share the code below...
+    std::size_t used() const noexcept { return static_cast<std::size_t>( m_current - m_buffer ); }
 
     template <std::size_t ReqAlign>
     std::byte* allocate( std::size_t n ) {
       static_assert( ReqAlign <= alignment, "alignment is too small for this arena" );
-      auto const aligned_n = align_up( n );
-      if ( static_cast<decltype( aligned_n )>( m_buf + size() - m_ptr ) < aligned_n ) throw std::bad_alloc{};
-      return std::exchange( m_ptr, m_ptr + aligned_n );
+      auto const aligned_n = details::align_up<Alignment>( n );
+      if ( static_cast<decltype( aligned_n )>( m_buffer + size() - m_current ) < aligned_n ) throw std::bad_alloc{};
+      return std::exchange( m_current, m_current + aligned_n );
     }
 
     void deallocate( std::byte* p, std::size_t n ) noexcept {
-      assert( is_inside( m_ptr ) );
+      assert( is_inside( m_current ) );
       assert( is_inside( p ) );
-      if ( p + align_up( n ) == m_ptr ) m_ptr = p;
+      if ( p + details::align_up<Alignment>( n ) == m_current ) m_current = p;
     }
 
-    void reset() noexcept { m_ptr = m_buf; }
+    void reset() noexcept { m_current = m_buffer; }
   };
 
-  template <class T, std::size_t N, std::size_t Align = alignof( std::max_align_t )>
-  class ArenaAllocator {
+  template <class T, typename Arena>
+  class AllocatorWrapper {
   public:
     using value_type                = T;
-    static auto constexpr alignment = Align;
-    static auto constexpr size      = N;
-    using arena_t                   = StaticArena<size, alignment>;
+    static auto constexpr alignment = Arena::alignment;
+    using arena_t                   = Arena;
 
   private:
     arena_t* m_arena;
 
   public:
-    ArenaAllocator( const ArenaAllocator& ) = default;
-    ArenaAllocator& operator=( const ArenaAllocator& ) = delete;
+    AllocatorWrapper( const AllocatorWrapper& ) = default;
+    AllocatorWrapper& operator=( const AllocatorWrapper& ) = delete;
 
-    ArenaAllocator( arena_t* a ) noexcept : m_arena( a ) {
-      static_assert( size % alignment == 0, "size N needs to be a multiple of alignment Align" );
+    AllocatorWrapper( arena_t* a ) : m_arena( a ) {
+      if ( a->size() % alignment != 0 )
+        throw std::runtime_error( "AllocatorWrapper: size must be a multiple of alignment" );
     }
 
     template <class U>
-    ArenaAllocator( const ArenaAllocator<U, N, alignment>& a ) noexcept : m_arena{a.m_arena} {}
-    template <class _Up>
+    AllocatorWrapper( const AllocatorWrapper<U, arena_t>& a ) noexcept : m_arena{a.m_arena} {}
+
+    template <class U>
     struct rebind {
-      using other = ArenaAllocator<_Up, N, alignment>;
+      using other = AllocatorWrapper<U, arena_t>;
     };
 
     T* allocate( std::size_t n ) {
@@ -98,18 +175,24 @@ namespace LHCb::Allocators {
       m_arena->deallocate( reinterpret_cast<std::byte*>( p ), n * sizeof( T ) );
     }
 
-    template <class U, std::size_t M, std::size_t A>
-    friend class ArenaAllocator;
+    template <typename, typename>
+    friend class AllocatorWrapper;
   };
 
-  template <class T, std::size_t N, std::size_t A1, class U, std::size_t M, std::size_t A2>
-  inline bool operator==( const ArenaAllocator<T, N, A1>& x, const ArenaAllocator<U, M, A2>& y ) noexcept {
-    return N == M && A1 == A2 && &x.m_arena == &y.m_arena;
+  template <class T, typename Arena1, class U, typename Arena2>
+  inline bool operator==( const AllocatorWrapper<T, Arena1>& x, const AllocatorWrapper<U, Arena2>& y ) noexcept {
+    return std::is_same_v<Arena1, Arena2> && x.m_arena == y.m_arena;
   }
 
-  template <class T, std::size_t N, std::size_t A1, class U, std::size_t M, std::size_t A2>
-  inline bool operator!=( const ArenaAllocator<T, N, A1>& x, const ArenaAllocator<U, M, A2>& y ) noexcept {
+  template <class T, typename Arena1, class U, typename Arena2>
+  inline bool operator!=( const AllocatorWrapper<T, Arena1>& x, const AllocatorWrapper<U, Arena2>& y ) noexcept {
     return !( x == y );
   }
+
+  template <class T, std::size_t N, std::size_t Align = alignof( std::max_align_t )>
+  using ArenaAllocator = AllocatorWrapper<T, StaticArena<N, Align>>;
+
+  template <class T, std::size_t Align = alignof( std::max_align_t )>
+  using DynamicArenaAllocator = AllocatorWrapper<T, DynamicArena<Align>>;
 
 } // namespace LHCb::Allocators
--- Kernel/LHCbKernel/tests/src/test_arenaAllocator.cpp
+++ Kernel/LHCbKernel/tests/src/test_arenaAllocator.cpp
@@ -13,13 +13,15 @@
 
 #include "Kernel/ArenaAllocator.h"
 #include <new>
+#include <numeric>
 #include <vector>
 
 // Replace new and delete just for the purpose of demonstrating that
 // they are not called.
 
-std::size_t memory = 0;
-std::size_t alloc  = 0;
+std::size_t memory  = 0;
+std::size_t alloc   = 0;
+std::size_t dealloc = 0;
 
 void* operator new( std::size_t s ) {
   memory += s;
@@ -27,14 +29,14 @@ void* operator new( std::size_t s ) {
   return malloc( s );
 }
 void operator delete( void* p ) {
-  --alloc;
+  ++dealloc;
+  free( p );
+}
+void operator delete( void* p, size_t ) {
+  ++dealloc;
   free( p );
 }
 
-void operator delete( void* p, size_t ) {
-  --alloc;
-  free( p );
-}
 //   Note for vector it is possible to reduce the alignment requirements
 //   down to alignof(T) because vector doesn't allocate anything but T's.
 //   And if we're wrong about that guess, it is a compile-time error, not
@@ -143,20 +145,19 @@ BOOST_AUTO_TEST_CASE( test_SmallList ) {
 }
 
 template <size_t MaxEntries>
-class Container {
-
+class StaticContainer {
   constexpr static std::size_t c_size = MaxEntries * ( sizeof( int ) + sizeof( double ) + sizeof( char ) );
   using Arena_t                       = LHCb::Allocators::StaticArena<c_size>;
   template <typename T>
   using Allocator = LHCb::Allocators::ArenaAllocator<T, c_size>;
 
-  std::unique_ptr<Arena_t>               m_buffer = std::make_unique<Arena_t>();
-  std::vector<int, Allocator<int>>       is{m_buffer.get()};
-  std::vector<double, Allocator<double>> ds{m_buffer.get()};
-  std::vector<char, Allocator<char>>     cs{m_buffer.get()};
+  Arena_t                                m_buffer;
+  std::vector<int, Allocator<int>>       is{&m_buffer};
+  std::vector<double, Allocator<double>> ds{&m_buffer};
+  std::vector<char, Allocator<char>>     cs{&m_buffer};
 
 public:
-  Container() {
+  StaticContainer() {
     cs.reserve( MaxEntries );
     is.reserve( MaxEntries );
     ds.reserve( MaxEntries );
@@ -183,26 +184,182 @@ public:
   bool   empty() const { return cs.empty(); }
 };
 
-BOOST_AUTO_TEST_CASE( test_Container ) {
+BOOST_AUTO_TEST_CASE( test_StaticContainer ) {
 
   memory = 0;
   alloc  = 0;
 
-  Container<32> c;
+  StaticContainer<32> c;
   BOOST_CHECK( c.empty() );
   c.emplace_back( 1, 1.5, 'a' );
   c.emplace_back( 2, 4.5, 'b' );
   BOOST_CHECK( c.size() == 2 );
-  auto ex_1 = Container<32>::Data{1, 1.5, 'a'};
+  auto ex_1 = StaticContainer<32>::Data{1, 1.5, 'a'};
   BOOST_CHECK( c[0] == ex_1 );
 
+  BOOST_CHECK( alloc == 0 );
+  BOOST_CHECK( memory == 0 );
+}
+
+namespace details {
+  template <std::size_t N, typename T>
+  std::array<T, N> distribute( T t ) {
+    std::array<T, N> a;
+    a.fill( t );
+    return a;
+  }
+
+  template <typename F, typename T1, typename T2, std::size_t... I>
+  constexpr void apply_binary_impl( F f, T1&& t1, T2&& t2, std::index_sequence<I...> ) {
+    ( std::invoke( f, std::get<I>( std::forward<T1>( t1 ) ), std::get<I>( std::forward<T2>( t2 ) ) ), ... );
+  }
+  template <typename F, typename T1, typename T2>
+  constexpr void apply_binary( F&& f, T1&& t1, T2&& t2 ) {
+    static_assert( std::tuple_size_v<std::remove_reference_t<T1>> == std::tuple_size_v<std::remove_reference_t<T2>> );
+    apply_binary_impl( std::forward<F>( f ), std::forward<T1>( t1 ), std::forward<T2>( t2 ),
+                       std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<T1>>>{} );
+  }
+} // namespace details
+
+template <typename... Args>
+class Container {
+  constexpr static auto N = sizeof...( Args );
+  static_assert( N > 1 );
+  constexpr static auto alignment = std::max( {sizeof( Args )...} );
+  // constexpr static auto alignment = 64;
+  template <typename T>
+  using Allocator = LHCb::Allocators::DynamicArenaAllocator<T, alignment>;
+  using Arena     = LHCb::Allocators::DynamicArena<alignment>;
+  using Data      = std::tuple<std::vector<Args, Allocator<Args>>...>;
+
+  static constexpr std::size_t n_bytes( std::size_t n ) { return ( Arena::template align_up<Args>( n ) + ... ); }
+
+  static constexpr std::size_t n_bytes( std::array<std::size_t, N> n ) {
+    auto sizes = std::array{Arena::template align_up<Args>( 1 )...};
+    return std::inner_product( sizes.begin(), sizes.end(), n.begin(), 0 );
+  }
+
+  typename Arena::Handle m_buffer;
+  Data                   m_data = std::make_from_tuple<Data>( details::distribute<N>( m_buffer.get() ) );
+
+public:
+  Container( std::size_t maxElements ) : m_buffer{Arena::create( n_bytes( maxElements ) )} {
+    std::apply( [maxElements]( auto&... d ) { ( d.reserve( maxElements ), ... ); }, m_data );
+  }
+
+  Container( std::array<std::size_t, N> maxElements ) : m_buffer{Arena::create( n_bytes( maxElements ) )} {
+    details::apply_binary( []( auto& c, auto n ) { c.reserve( n ); }, m_data, maxElements );
+  }
+
+  auto operator[]( int i ) const {
+    return std::apply( [i]( const auto&... d ) { return std::make_tuple( d[i]... ); }, m_data );
+  }
+
+  template <typename T>
+  const auto& column() const {
+    return std::get<std::vector<T, Allocator<T>>>( m_data );
+  }
+  template <typename T>
+  auto& column() {
+    return std::get<std::vector<T, Allocator<T>>>( m_data );
+  }
+
+  template <typename T>
+  const auto& column( int i ) const {
+    return std::get<std::vector<T, Allocator<T>>>( m_data )[i];
+  }
+
+  template <size_t I>
+  const auto& column() const {
+    return std::get<I>( m_data );
+  }
+  template <size_t I>
+  auto& column() {
+    return std::get<I>( m_data );
+  }
+
+  template <size_t I>
+  const auto& column( int i ) const {
+    return std::get<I>( m_data )[i];
+  }
+
+  template <typename... U>
+  void emplace_back( U&&... arg ) {
+    // C++20: std::apply( [..arg=std::forward<U>(arg)](auto&... d) { (d.push_back(std::forward<U>(arg)),...); }, m_data
+    // );
+    std::apply( [&]( auto&... d ) { ( d.push_back( std::forward<U>( arg ) ), ... ); }, m_data );
+  }
+
+  size_t size() const { return std::get<0>( m_data ).size(); }
+  bool   empty() const { return std::get<0>( m_data ).empty(); }
+};
+
+BOOST_AUTO_TEST_CASE( test_Container1 ) {
+  memory = 0;
+  alloc  = 0;
+
+  std::size_t                  N = 64;
+  Container<int, double, char> c{64};
+
   BOOST_CHECK( alloc == 1 );
-  // std::cout << memory << '\n';
-  // BOOST_CHECK(memory==32*(4+8+1));
+
+  c.emplace_back( 1, 2.5, 'a' );
+  BOOST_CHECK( alloc == 1 );
+
+  BOOST_CHECK( c[0] == std::make_tuple( 1, 2.5, 'a' ) );
+  BOOST_CHECK( c.column<0>( 0 ) == 1 );
+  BOOST_CHECK( c.column<char>().at( 0 ) == 'a' );
+  BOOST_CHECK( c.column<double>( 0 ) == 2.5 );
+
+  c.emplace_back( 2, 4.5, 'b' );
+  for ( int i = 2; i < static_cast<int>( N ); ++i ) {
+    c.emplace_back( i * 3, i * 1.5, 'c' );
+    BOOST_CHECK( alloc == 1 );
+  }
+  BOOST_CHECK( c.size() == N );
 
   auto c2 = std::move( c );
-  BOOST_CHECK( alloc == 1 );
   BOOST_CHECK( c.empty() );
-  BOOST_CHECK( c2.size() == 2 );
-  BOOST_CHECK( c2[0] == ex_1 );
+  BOOST_CHECK( c2.size() == N );
+  BOOST_CHECK( c2[0] == std::make_tuple( 1, 2.5, 'a' ) );
+
+  BOOST_CHECK( alloc == 1 );
+}
+
+BOOST_AUTO_TEST_CASE( test_Container2 ) {
+  memory = 0;
+  alloc  = 0;
+  Container<LHCb::span<int>, int> c{{4, 100}}; // allocates, using a _single_ allocation,  memory for 4 spans, and
+                                               // 100 doubles...
+  auto& ints  = c.column<int>();               // get the column of doubles
+  auto& spans = c.column<LHCb::span<int>>();   // get the column of spans
+
+  // intersprerse writing to ints and span,
+  // and demonstrate that the spans remain valid
+  for ( int i = 0; i < 5; ++i ) ints.push_back( i );
+  spans.emplace_back( LHCb::make_span( ints ).subspan( 0, 5 ) );
+
+  for ( int i = 5; i < 25; ++i ) ints.push_back( i );
+  spans.emplace_back( LHCb::make_span( ints ).subspan( 5, 20 ) );
+
+  for ( int i = 25; i < 80; ++i ) ints.push_back( i );
+  spans.emplace_back( LHCb::make_span( ints ).subspan( 25, 55 ) );
+
+  for ( int i = 80; i < 100; ++i ) ints.push_back( i );
+  spans.emplace_back( LHCb::make_span( ints ).subspan( 80, 20 ) );
+
+  BOOST_CHECK( alloc == 1 );
+
+  auto c2 = std::move( c );  // move the container
+  BOOST_CHECK( alloc == 1 ); // check that this doesn't allocate memory
+  BOOST_CHECK( c.empty() );
+
+  // and verify that the spans inside the 'moved' container are still valid
+  std::vector<int> sums;
+  for ( auto s : c2.column<LHCb::span<int>>() ) { sums.push_back( std::accumulate( s.begin(), s.end(), 0 ) ); }
+  auto euler = []( int n ) { return n * ( n + 1 ) / 2; };
+  BOOST_CHECK( sums.at( 0 ) == euler( 4 ) );
+  BOOST_CHECK( sums.at( 1 ) == euler( 24 ) - euler( 4 ) );
+  BOOST_CHECK( sums.at( 2 ) == euler( 79 ) - euler( 24 ) );
+  BOOST_CHECK( sums.at( 3 ) == euler( 99 ) - euler( 79 ) );
 }
