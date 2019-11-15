0d18d718d48a57b90380454bc8c9dbd23cabbca4
Sebastien Ponce <sebastien.ponce@cern.ch>
2019-11-15 17:35:26 +0100

    Allow functional algorithms using conditions to have non default base class

--- Det/DetCond/examples/FunctionalConditionAccessor.cpp
+++ Det/DetCond/examples/FunctionalConditionAccessor.cpp
@@ -28,16 +28,15 @@ namespace DetCond::Examples::Functional {
     }
   };
 
-  // Example of algorithm that requires caching of condition derivations.
 
   /// Example of a derived condition that does not inherit from Condition base class.
   struct MyData {
     double p1, p2, v;
   };
 
+  /// Example of algorithm that requires caching of condition derivations.
   struct CondAccessExampleWithDerivation
       : Gaudi::Functional::Consumer<void( const MyData& ), LHCb::DetDesc::usesConditions<MyData>> {
-    // costructor
     CondAccessExampleWithDerivation( const std::string& name, ISvcLocator* loc )
         : Consumer{name, loc, {KeyValue{"Target", "DerivedCondition"}}} {}
 
@@ -61,7 +60,43 @@ namespace DetCond::Examples::Functional {
     }
   };
 
+  /// Example of algorithm that requires caching of condition derivations and inherits from non default base class
+  struct MyAlgorithm : Gaudi::Algorithm {
+    using Algorithm::Algorithm;
+    static constexpr double m_pi{3.1415};
+    double                  pi() { return m_pi; };
+  };
+  struct CondAccessExampleWithDerivationAndBase
+      : Gaudi::Functional::Consumer<void( const MyData& ),
+                                    LHCb::DetDesc::usesBaseAndConditions<MyAlgorithm, MyData>> {
+    CondAccessExampleWithDerivationAndBase( const std::string& name, ISvcLocator* loc )
+        : Consumer{name, loc, {KeyValue{"Target", "DerivedCondition"}}} {}
+
+    /// Property used to change the path of the input condition, if needed
+    Gaudi::Property<std::string> m_srcPath{this, "Source", "TestCondition"};
+
+    StatusCode initialize() override {
+      const auto sc = Consumer::initialize();
+      if ( !sc ) return sc;
+
+      addConditionDerivation( m_srcPath.value(), inputLocation<0>(),
+                              /// Callable which converts raw information into a derived condition.
+                              [this]( const ParamValidDataObject& obj ) -> MyData {
+                                const auto& [p1, p2] = obj.params<double, double>( "par1", "par2" );
+                                return {p1, p2, std::sqrt( p1 ) + pi() * p2 * p2};
+                              } );
+
+      return sc;
+    }
+
+    void operator()( const MyData& cond ) const override {
+      info() << "condition value: {\n  p1: " << cond.p1 << "\n  p2: " << cond.p2 << "\n  v:  " << cond.v << "\n}"
+             << endmsg;
+    }
+  };
+
 } // namespace DetCond::Examples::Functional
 
 DECLARE_COMPONENT( DetCond::Examples::Functional::CondAccessExample )
 DECLARE_COMPONENT( DetCond::Examples::Functional::CondAccessExampleWithDerivation )
+DECLARE_COMPONENT( DetCond::Examples::Functional::CondAccessExampleWithDerivationAndBase )
