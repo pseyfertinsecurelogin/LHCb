44d0c1d985bb4837f31046567ac5a4e367d089e3
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2019-11-29 17:44:46 +0000

    Add new standalone refractive index utility

---
+++ Rich/RichFutureUtils/RichFutureUtils/RichTabulatedRefIndex.h
@@ -0,0 +1,164 @@
+/*****************************************************************************\
+* (c) Copyright 2000-2020 CERN for the benefit of the LHCb Collaboration      *
+*                                                                             *
+* This software is distributed under the terms of the GNU General Public      *
+* Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   *
+*                                                                             *
+* In applying this licence, CERN does not waive the privileges and immunities *
+* granted to it by virtue of its status as an Intergovernmental Organization  *
+* or submit itself to any jurisdiction.                                       *
+\*****************************************************************************/
+
+#pragma once
+
+// STL
+#include <array>
+#include <memory>
+
+// RichDet
+#include "RichDet/DeRich1.h"
+#include "RichDet/DeRich2.h"
+#include "RichDet/DeRichRadiator.h"
+#include "RichDet/Rich1DTabProperty.h"
+
+// Det Desc
+#include "DetDesc/ConditionKey.h"
+#include "DetDesc/IConditionDerivationMgr.h"
+
+namespace Rich::Utils {
+
+  //-----------------------------------------------------------------------------
+  /** @class TabulatedRefIndex RichTabulatedRefIndex.h
+   *
+   *  Tool to calculate the effective refractive index for
+   *  a given radiator. An implementation that uses the tabulated
+   *  information from the XML.
+   *
+   *  @author Chris Jones
+   *  @date   2017-01-19
+   */
+  //-----------------------------------------------------------------------------
+
+  class TabulatedRefIndex final {
+
+  public:
+    /// Disallow default constructor
+    TabulatedRefIndex() = delete;
+
+    /// Constructor from detector info
+    TabulatedRefIndex( const DeRich1&              rich1, //
+                       const DeRich2&              rich2, //
+                       const DeRichRadiator&       r1gas, //
+                       const DeRichRadiator&       r2gas,
+                       const RadiatorArray<float>& minPhotEn, //
+                       const RadiatorArray<float>& maxPhotEn )
+        : m_riches{&rich1, &rich2}
+        , m_radiators{nullptr, &r1gas, &r2gas}
+        , m_minPhotEn( minPhotEn )
+        , m_maxPhotEn( maxPhotEn ) {}
+
+  public:
+    // access methods
+
+    // Calculates the refractive index for a given radiator type at a
+    // given energy
+    float refractiveIndex( const Rich::RadiatorType rad, //
+                           const float              energy ) const {
+      return deRad( rad )->refractiveIndex( energy, m_hltMode );
+    }
+
+    // Calculates the average refractive index for a given radiator type
+    // for a given range of photon energies.
+    float refractiveIndex( const Rich::RadiatorType rad,       //
+                           const float              energyBot, //
+                           const float              energyTop ) const {
+      const auto rich = ( rad == Rich::Rich2Gas ? Rich::Rich2 : Rich::Rich1 );
+      return refractiveIndex( rad,
+                              m_riches[rich]->nominalPDQuantumEff()->meanX( energyBot, energyTop ) / Gaudi::Units::eV );
+    }
+
+    // Calculates the average refractive index for a given radiator type
+    // for a all visable photon energies.
+    float refractiveIndex( const Rich::RadiatorType rad ) const {
+      return refractiveIndex( rad, meanPhotonEnergy( rad ) );
+    }
+
+    // Calculates the average refractive index for a given set of radiator intersections
+    // for all visable photon energies.
+    float refractiveIndex( const Rich::RadIntersection::Vector& intersections, const float energy ) const;
+
+    // Calculates the average refractive index for a given set of radiator intersections
+    // for all visable photon energies
+    float refractiveIndex( const Rich::RadIntersection::Vector& intersections ) const;
+
+    // Calculates the refractive index R.M.S. for a given set of radiator intersections
+    // for all visable photon energies.
+    float refractiveIndexRMS( const Rich::RadIntersection::Vector& intersections ) const;
+
+    // Calculates the refractive index S.D. for a given set of radiator intersections
+    // for all visable photon energies.
+    float refractiveIndexSD( const Rich::RadIntersection::Vector& intersections ) const;
+
+  private:
+    // methods
+
+    /// get the average mean photon energy for given radiator
+    inline float meanPhotonEnergy( const Rich::RadiatorType rad ) const noexcept {
+      return 0.5f * ( m_maxPhotEn[rad] + m_minPhotEn[rad] );
+    }
+
+    /// Access on demand the Rich radiator detector elements
+    inline const DeRichRadiator* deRad( const Rich::RadiatorType rad ) const { return m_radiators[rad]; }
+
+  private:
+    // data
+
+    /// Pointers to RICHes
+    Rich::DetectorArray<const DeRich*> m_riches = {{}};
+
+    /// Pointers to RICH radiator detector elements
+    Rich::RadiatorArray<const DeRichRadiator*> m_radiators = {{}};
+
+    /// The minimum photon energies
+    RadiatorArray<float> m_minPhotEn{1.75, 1.75, 1.75};
+
+    /// The maximum photon energies
+    RadiatorArray<float> m_maxPhotEn{4.0, 7.0, 7.0};
+
+    /// Flag to say if we are in HLT mode or not ...
+    /// Need to decide what to do with this ...
+    const bool m_hltMode{true};
+
+  public:
+    /// Creates a condition derivation for the given key
+    template <typename PARENT>
+    static inline LHCb::DetDesc::IConditionDerivationMgr::DerivationId //
+    addConditionDerivation( PARENT*                     parent,        //
+                            LHCb::DetDesc::ConditionKey key,           //
+                            const RadiatorArray<float>& minPhotEn,     //
+                            const RadiatorArray<float>& maxPhotEn ) {
+      if ( parent->msgLevel( MSG::DEBUG ) ) {
+        parent->debug() << "TabulatedRefIndex::addConditionDerivation : Key=" << key << endmsg;
+      }
+      using SA = std::array<std::string, 4>;
+      return LHCb::DetDesc:: //
+          addConditionDerivation( parent->conditionDerivationMgr(),
+                                  SA{DeRichLocations::Rich1,                            // inputs
+                                     DeRichLocations::Rich2,                            //
+                                     DeRichLocations::Rich1Gas,                         //
+                                     DeRichLocations::Rich2Gas},                        //
+                                  std::move( key ),                                     // output
+                                  [minPhotEn = minPhotEn,                               //
+                                   maxPhotEn = maxPhotEn]( const DeRich1&        rich1, //
+                                                           const DeRich2&        rich2, //
+                                                           const DeRichRadiator& r1gas, //
+                                                           const DeRichRadiator& r2gas ) {
+                                    return Utils::TabulatedRefIndex{rich1, rich2, r1gas, r2gas, minPhotEn, maxPhotEn};
+                                  } );
+    }
+
+    /// Default conditions name
+    static constexpr const char* DefaultConditionKey = "RichRefIndex-Handler";
+  };
+
+} // namespace Rich::Utils
---
+++ Rich/RichFutureUtils/src/RichTabulatedRefIndex.cpp
@@ -0,0 +1,66 @@
+/*****************************************************************************\
+* (c) Copyright 2000-2020 CERN for the benefit of the LHCb Collaboration      *
+*                                                                             *
+* This software is distributed under the terms of the GNU General Public      *
+* Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   *
+*                                                                             *
+* In applying this licence, CERN does not waive the privileges and immunities *
+* granted to it by virtue of its status as an Intergovernmental Organization  *
+* or submit itself to any jurisdiction.                                       *
+\*****************************************************************************/
+
+#include "RichFutureUtils/RichTabulatedRefIndex.h"
+
+using namespace Rich::Utils;
+
+float TabulatedRefIndex::refractiveIndex( const Rich::RadIntersection::Vector& intersections,
+                                          const float                          energy ) const {
+  // loop over all radiator intersections and calculate the weighted average
+  // according to the path length in each radiator
+  float refIndex( 0 ), totPathL( 0 );
+  for ( const auto& R : intersections ) {
+    const auto pLength = R.pathLength();
+    refIndex += pLength * R.radiator()->refractiveIndex( energy, m_hltMode );
+    totPathL += pLength;
+  }
+  return ( totPathL > 0 ? refIndex / totPathL : refIndex );
+}
+
+float TabulatedRefIndex::refractiveIndex( const Rich::RadIntersection::Vector& intersections ) const {
+  // loop over all radiator intersections and calculate the weighted average
+  // according to the path length in each radiator
+  float refIndex( 0 ), totPathL( 0 );
+  for ( const auto& R : intersections ) {
+    const auto energy  = meanPhotonEnergy( R.radiator()->radiatorID() );
+    const auto pLength = R.pathLength();
+    refIndex += pLength * R.radiator()->refractiveIndex( energy, m_hltMode );
+    totPathL += pLength;
+  }
+  return ( totPathL > 0 ? refIndex / totPathL : refIndex );
+}
+
+float TabulatedRefIndex::refractiveIndexRMS( const Rich::RadIntersection::Vector& intersections ) const {
+  // loop over all radiator intersections and calculate the weighted average
+  // according to the path length in each radiator
+  float refIndexRMS( 0 ), totPathL( 0 );
+  for ( const auto& R : intersections ) {
+    const auto  pLength = R.pathLength();
+    const auto* index   = R.radiator()->refIndex( m_hltMode );
+    refIndexRMS += pLength * index->rms( index->minX(), index->maxX(), 100 );
+    totPathL += pLength;
+  }
+  return ( totPathL > 0 ? refIndexRMS / totPathL : refIndexRMS );
+}
+
+float TabulatedRefIndex::refractiveIndexSD( const Rich::RadIntersection::Vector& intersections ) const {
+  // loop over all radiator intersections and calculate the weighted average
+  // according to the path length in each radiator
+  float refIndexSD( 0 ), totPathL( 0 );
+  for ( const auto& R : intersections ) {
+    const auto  pLength = R.pathLength();
+    const auto* index   = R.radiator()->refIndex( m_hltMode );
+    refIndexSD += pLength * index->standardDeviation( index->minX(), index->maxX(), 100 );
+    totPathL += pLength;
+  }
+  return ( totPathL > 0 ? refIndexSD / totPathL : refIndexSD );
+}
