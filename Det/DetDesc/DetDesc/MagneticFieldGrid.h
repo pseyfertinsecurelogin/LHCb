eb9e08d8d8ecaa807b2981759b7fd6ebd1888c83
Sebastien Ponce <sebastien.ponce@cern.ch>
2020-02-04 16:05:37 +0100

    Dropped unused code after the drop of VectorFitter

--- Det/DetDesc/DetDesc/MagneticFieldGrid.h
+++ Det/DetDesc/DetDesc/MagneticFieldGrid.h
@@ -8,8 +8,7 @@
 * granted to it by virtue of its status as an Intergovernmental Organization  *
 * or submit itself to any jurisdiction.                                       *
 \*****************************************************************************/
-#ifndef MAGFIELDGRID_H
-#define MAGFIELDGRID_H
+#pragma once
 
 // STD
 #include <array>
@@ -87,147 +86,6 @@ namespace LHCb {
       return fetchVectorQ_helper( indices, std::make_index_sequence<width>{} );
     }
 
-  public:
-    template <class vectype, size_t width>
-    std::array<vectype, 3> horizontallyVectorizedFieldVectorClosestPoint( const std::array<vectype, 3>& point ) const {
-      std::array<vectype, 3> bf{vectype{0.}, vectype{0.}, vectype{0.}};
-
-      const std::array<vectype, 3> Nxyz{vectype{(double)m_Nxyz_V[0]}, vectype{(double)m_Nxyz_V[1]},
-                                        vectype{(double)m_Nxyz_V[2]}};
-      const std::array<vectype, 3> inverted_Dxyz{vectype{m_invDxyz_V[0]}, vectype{m_invDxyz_V[1]},
-                                                 vectype{m_invDxyz_V[2]}};
-      const vectype                x = ( point[0] - vectype{m_min_FL_V[0]} ) * inverted_Dxyz[0];
-      const vectype                y = ( point[1] - vectype{m_min_FL_V[1]} ) * inverted_Dxyz[1];
-      const vectype                z = ( point[2] - vectype{m_min_FL_V[2]} ) * inverted_Dxyz[2];
-      const vectype                i = floor( x );
-      const vectype                j = floor( y );
-      const vectype                k = floor( z );
-
-      // if ( _i < m_Nxyz_V[0]-1 &&
-      //      _j < m_Nxyz_V[1]-1 &&
-      //      _k < m_Nxyz_V[2]-1 ) {
-      const auto condition{i < Nxyz[0] - 1. && j < Nxyz[1] - 1. && k < Nxyz[2] - 1.};
-
-      if ( horizontal_or( condition ) ) {
-        const vectype cond_multiplier = select( condition, vectype{1.}, vectype{0.} );
-        const vectype scaleFactor{m_scaleFactor};
-
-        const vectype                index = cond_multiplier * ( Nxyz[0] * ( Nxyz[1] * k + j ) + i );
-        const std::array<vectype, 3> q     = fetchVectorQ<vectype, width>( index );
-
-        bf[0] = cond_multiplier * scaleFactor * q[0];
-        bf[1] = cond_multiplier * scaleFactor * q[1];
-        bf[2] = cond_multiplier * scaleFactor * q[2];
-      }
-
-      return bf;
-    }
-
-    template <class vectype, size_t width>
-    std::array<vectype, 3> horizontallyVectorizedFieldVector( const std::array<vectype, 3>& point ) const {
-      std::array<vectype, 3> bf{vectype{0.}, vectype{0.}, vectype{0.}};
-
-      // Load variables
-      // TODO: This could be done more efficiently with
-      //       templating magic and permute or so
-      const std::array<vectype, 3> inverted_Dxyz{vectype{m_invDxyz_V[0]}, vectype{m_invDxyz_V[1]},
-                                                 vectype{m_invDxyz_V[2]}};
-      const std::array<vectype, 3> Nxyz{vectype{(double)m_Nxyz_V[0]}, vectype{(double)m_Nxyz_V[1]},
-                                        vectype{(double)m_Nxyz_V[2]}};
-      const vectype                scaleFactor{m_scaleFactor};
-
-      // double x = r.x() - m_min_FL[0] ; unsigned i =  int(x/m_Dxyz[0]) ;
-      // double y = r.y() - m_min_FL[1] ; unsigned j =  int(y/m_Dxyz[1]) ;
-      // double z = r.z() - m_min_FL[2] ; unsigned k =  int(z/m_Dxyz[2]) ;
-      const vectype x = ( point[0] - vectype{m_min_FL_V[0]} ) * inverted_Dxyz[0];
-      const vectype y = ( point[1] - vectype{m_min_FL_V[1]} ) * inverted_Dxyz[1];
-      const vectype z = ( point[2] - vectype{m_min_FL_V[2]} ) * inverted_Dxyz[2];
-
-      const vectype i = floor( x );
-      const vectype j = floor( y );
-      const vectype k = floor( z );
-
-      // if ( _i < m_Nxyz_V[0]-1 &&
-      //      _j < m_Nxyz_V[1]-1 &&
-      //      _k < m_Nxyz_V[2]-1 ) {
-      const auto condition{i < Nxyz[0] - 1. && j < Nxyz[1] - 1. && k < Nxyz[2] - 1.};
-
-      if ( horizontal_or( condition ) ) {
-        std::array<vectype, 3> q1;
-        std::array<vectype, 3> q2;
-
-        // Use a conditional multiplier to avoid more selects
-        const vectype cond_multiplier = select( condition, vectype{1.}, vectype{0.} );
-
-        const vectype ijk000 = cond_multiplier * ( Nxyz[0] * ( Nxyz[1] * k + j ) + i );
-        const vectype ijk001 = cond_multiplier * ( Nxyz[0] * ( Nxyz[1] * ( k + 1 ) + j ) + i );
-        const vectype ijk010 = cond_multiplier * ( Nxyz[0] * ( Nxyz[1] * k + j + 1 ) + i );
-        const vectype ijk011 = cond_multiplier * ( Nxyz[0] * ( Nxyz[1] * ( k + 1 ) + j + 1 ) + i );
-
-        // float hx1 = ( x-i*m_Dxyz[0] )/m_Dxyz[0]; auto hx0 = 1.0f-hx1;
-        // float hy1 = ( y-j*m_Dxyz[1] )/m_Dxyz[1]; auto hy0 = 1.0f-hy1;
-        // float hz1 = ( z-k*m_Dxyz[2] )/m_Dxyz[2]; auto hz0 = 1.0f-hz1;
-        const vectype hx1 = ( x - i );
-        const vectype hy1 = ( y - j );
-        const vectype hz1 = ( z - k );
-
-        const vectype hx0 = vectype{1.} - hx1;
-        const vectype hy0 = vectype{1.} - hy1;
-        const vectype hz0 = vectype{1.} - hz1;
-
-        // auto h000 = hx0*hy0*hz0; auto h100 = hx1*hy0*hz0;
-        // auto ijk000 = ( m_Nxyz[0]*( m_Nxyz[1]*k     + j )     + i );
-        // bf  = h000*m_Q[ijk000]+h100*m_Q[ijk000+1] ;
-        const vectype h000 = hx0 * hy0 * hz0;
-        const vectype h100 = hx1 * hy0 * hz0;
-        q1                 = fetchVectorQ<vectype, width>( ijk000 );
-        q2                 = fetchVectorQ<vectype, width>( ijk000 + 1 );
-        bf[0]              = h000 * q1[0] + h100 * q2[0];
-        bf[1]              = h000 * q1[1] + h100 * q2[1];
-        bf[2]              = h000 * q1[2] + h100 * q2[2];
-
-        // auto h001 = hx0*hy0*hz1; auto h101 = hx1*hy0*hz1;
-        // auto ijk001 = ( m_Nxyz[0]*( m_Nxyz[1]*(k+1) + j )     + i );
-        // bf += h001*m_Q[ijk001]+h101*m_Q[ijk001+1] ;
-        const vectype h001 = hx0 * hy0 * hz1;
-        const vectype h101 = hx1 * hy0 * hz1;
-        q1                 = fetchVectorQ<vectype, width>( ijk001 );
-        q2                 = fetchVectorQ<vectype, width>( ijk001 + 1 );
-        bf[0] += h001 * q1[0] + h101 * q2[0];
-        bf[1] += h001 * q1[1] + h101 * q2[1];
-        bf[2] += h001 * q1[2] + h101 * q2[2];
-
-        // auto h010 = hx0*hy1*hz0; auto h110 = hx1*hy1*hz0;
-        // auto ijk010 = ( m_Nxyz[0]*( m_Nxyz[1]*k     + j + 1 ) + i );
-        // bf += h010*m_Q[ijk010]+h110*m_Q[ijk010+1] ;
-        const vectype h010 = hx0 * hy1 * hz0;
-        const vectype h110 = hx1 * hy1 * hz0;
-        q1                 = fetchVectorQ<vectype, width>( ijk010 );
-        q2                 = fetchVectorQ<vectype, width>( ijk010 + 1 );
-        bf[0] += h010 * q1[0] + h110 * q2[0];
-        bf[1] += h010 * q1[1] + h110 * q2[1];
-        bf[2] += h010 * q1[2] + h110 * q2[2];
-
-        // auto h011 = hx0*hy1*hz1; auto h111 = hx1*hy1*hz1;
-        // auto ijk011 = ( m_Nxyz[0]*( m_Nxyz[1]*(k+1) + j + 1)  + i );
-        // bf += h011*m_Q[ijk011]+h111*m_Q[ijk011+1] ;
-        const vectype h011 = hx0 * hy1 * hz1;
-        const vectype h111 = hx1 * hy1 * hz1;
-        q1                 = fetchVectorQ<vectype, width>( ijk011 );
-        q2                 = fetchVectorQ<vectype, width>( ijk011 + 1 );
-        bf[0] += h011 * q1[0] + h111 * q2[0];
-        bf[1] += h011 * q1[1] + h111 * q2[1];
-        bf[2] += h011 * q1[2] + h111 * q2[2];
-
-        bf[0] *= scaleFactor * cond_multiplier;
-        bf[1] *= scaleFactor * cond_multiplier;
-        bf[2] *= scaleFactor * cond_multiplier;
-      }
-
-      return bf;
-    }
-
-  private:
     float m_scaleFactor = 1; ///< The scale factor
 
     std::array<float, 4> m_scaleFactor_V = {1, 1, 1, 0}; ///< Vector version of the scale factor, padded to 4 floats
@@ -240,19 +98,3 @@ namespace LHCb {
   };
 
 } // namespace LHCb
-
-// Instantiate horizontally vectorized entrypoints
-#if defined( __SSE__ )
-template std::array<Vec2d, 3>
-LHCb::MagneticFieldGrid::horizontallyVectorizedFieldVector<Vec2d, 2>( const std::array<Vec2d, 3>& ) const;
-#endif
-#if defined( __AVX__ )
-template std::array<Vec4d, 3>
-LHCb::MagneticFieldGrid::horizontallyVectorizedFieldVector<Vec4d, 4>( const std::array<Vec4d, 3>& ) const;
-#endif
-#if defined( __AVX512F__ )
-template std::array<Vec8d, 3>
-LHCb::MagneticFieldGrid::horizontallyVectorizedFieldVector<Vec8d, 8>( const std::array<Vec8d, 3>& ) const;
-#endif
-
-#endif // MAGFIELDGRID_H
